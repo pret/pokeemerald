63dd0ea397ff01787753f182541ed8324f2d498b
PikalaxALT <pikalaxalt@gmail.com>
2017-09-06 08:35:16 -0400

    Spawn source files for Field Map Obj

---
+++ include/field_map_obj.h
@@ -0,0 +1,10 @@
+#ifndef POKEEMERALD_FIELD_MAP_OBJ_H
+#define POKEEMERALD_FIELD_MAP_OBJ_H
+
+// Exported struct declarations
+
+// Exported RAM declarations
+
+// Exported ROM declarations
+
+#endif //POKEEMERALD_FIELD_MAP_OBJ_H
---
+++ src/field_map_obj.c
@@ -0,0 +1,16 @@
+// Includes
+
+#include "global.h"
+#include "field_map_obj.h"
+
+// Static struct declarations
+
+// Static RAM declarations
+
+// Static ROM declarations
+
+// ROM data
+
+// Code
+
+
