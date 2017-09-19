e033c721219f4412a0cb2cbbdcbec583ec60c87a
PikalaxALT <pikalaxalt@gmail.com>
2017-09-18 20:00:46 -0400

    load_pokemon_image_TODO

--- src/battle_dome_cards.c
+++ src/battle_dome_cards.c
@@ -1,6 +1,15 @@
 
 // Includes
 #include "global.h"
+#include "sprite.h"
+#include "species.h"
+#include "pokemon.h"
+#include "decompress.h"
+
+extern const struct CompressedSpriteSheet gMonFrontPicTable[NUM_SPECIES];
+extern const struct CompressedSpriteSheet gMonBackPicTable[NUM_SPECIES];
+extern const struct CompressedSpriteSheet gTrainerFrontPicTable[];
+extern const struct CompressedSpriteSheet gTrainerBackPicTable[];
 
 // Static type declarations
 
@@ -20,7 +29,7 @@ extern const struct BattleDomeCard gUnknown_0860B058;
 
 // .text
 
-void nullsub_122(void)
+void nullsub_122(struct Sprite *sprite)
 {
 
 }
@@ -32,6 +41,47 @@ bool8 dp13_810BB8C(void)
     for (i = 0; i < 8; i ++)
     {
         gUnknown_0203CD04[i] = gUnknown_0860B058;
+    }
+    return FALSE;
+}
+
+bool8 load_pokemon_image_TODO(u16 species, u32 personality, bool8 isFrontPic, void *dest, bool8 isTrainer, bool8 ignoreDeoxys)
+{
+    if (!isTrainer)
+    {
+        if (isFrontPic)
+        {
+            if (!ignoreDeoxys)
+            {
+                LoadSpecialPokePic(&gMonFrontPicTable[species], dest, species, personality, isFrontPic);
+            }
+            else
+            {
+                LoadSpecialPokePic_DontHandleDeoxys(&gMonFrontPicTable[species], dest, species, personality, isFrontPic);
+            }
+        }
+        else
+        {
+            if (!ignoreDeoxys)
+            {
+                LoadSpecialPokePic(&gMonBackPicTable[species], dest, species, personality, isFrontPic);
+            }
+            else
+            {
+                LoadSpecialPokePic_DontHandleDeoxys(&gMonBackPicTable[species], dest, species, personality, isFrontPic);
+            }
+        }
+    }
+    else
+    {
+        if (isFrontPic)
+        {
+            DecompressPicFromTable(&gTrainerFrontPicTable[species], dest, species);
+        }
+        else
+        {
+            DecompressPicFromTable(&gTrainerBackPicTable[species], dest, species);
+        }
     }
     return FALSE;
 }
