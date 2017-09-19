99a7fcfdfa723390ecb25ec6819359b3521c8f34
scnorton <scnorton@biociphers.org>
2017-09-19 16:21:50 -0400

    sub_818DA78, SetLilycoveLadyRandomly

--- src/lilycove_lady.c
+++ src/lilycove_lady.c
@@ -4,11 +4,15 @@
 
 #include "global.h"
 #include "event_data.h"
+#include "rng.h"
 #include "lilycove_lady.h"
 
 void SetLilycoveQuizLady(void);
 void SetLilycoveFavourLady(void);
 void SetLilycoveContestLady(void);
+void sub_818E004(void);
+void sub_818DBC4(void);
+void sub_818E674(void);
 
 extern const u16 gUnknown_0860B074[];
 extern const u16 gUnknown_0860B07E[];
@@ -42,6 +46,41 @@ void SetLilycoveLady(void)
     id = ((gSaveBlock2Ptr->playerTrainerId[1] << 8) | gSaveBlock2Ptr->playerTrainerId[0]);
     id %= 6;
     id >>= 1;
+    switch (id)
+    {
+        case LILYCOVE_LADY_QUIZ:
+            SetLilycoveQuizLady();
+            break;
+        case LILYCOVE_LADY_FAVOUR:
+            SetLilycoveFavourLady();
+            break;
+        case LILYCOVE_LADY_CONTEST:
+            SetLilycoveContestLady();
+            break;
+    }
+}
+
+void sub_818DA78(void)
+{
+    switch (GetLilycoveLadyId())
+    {
+        case LILYCOVE_LADY_QUIZ:
+            sub_818E004();
+            break;
+        case LILYCOVE_LADY_FAVOUR:
+            sub_818DBC4();
+            break;
+        case LILYCOVE_LADY_CONTEST:
+            sub_818E674();
+            break;
+    }
+}
+
+void SetLilycoveLadyRandomly(void)
+{
+    u8 id;
+
+    id = Random() % 3;
     switch (id)
     {
         case LILYCOVE_LADY_QUIZ:
