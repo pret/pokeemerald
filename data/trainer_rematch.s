4e1c9920b2431459f12ceaa787c59654034e60ad
PikalaxALT <pikalax1@gmail.com>
2017-09-21 13:51:32 -0400

    Split data/data_emerald2.s

---
+++ data/trainer_rematch.s
@@ -0,0 +1,10 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+
+gUnknown_0862AD34:: @ 862AD34
+	.incbin "baserom.gba", 0x62ad34, 0x10
+
+gUnknown_0862AD44:: @ 862AD44
+	.incbin "baserom.gba", 0x62ad44, 0x10
---
+++ data/unk_transition.s
@@ -0,0 +1,28 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+
+gUnknown_0862B770:: @ 862B770
+	.incbin "baserom.gba", 0x62b770, 0x14
+
+gUnknown_0862B784:: @ 862B784
+	.incbin "baserom.gba", 0x62b784, 0x14
+
+gUnknown_0862B798:: @ 862B798
+	.incbin "baserom.gba", 0x62b798, 0x14
+
+gUnknown_0862B7AC:: @ 862B7AC
+	.incbin "baserom.gba", 0x62b7ac, 0x14
+
+gUnknown_0862B7C0:: @ 862B7C0
+	.incbin "baserom.gba", 0x62b7c0, 0x14
+
+gUnknown_0862B7D4:: @ 862B7D4
+	.incbin "baserom.gba", 0x62b7d4, 0x14
+
+gUnknown_0862B7E8:: @ 862B7E8
+	.incbin "baserom.gba", 0x62b7e8, 0x14
+
+gUnknown_0862B7FC:: @ 862B7FC
+	.incbin "baserom.gba", 0x62b7fc, 0x14
