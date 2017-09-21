ab6a7d2d8c5851462cf768010405cb1dbdfbaf03
PikalaxALT <pikalax1@gmail.com>
2017-09-21 13:28:02 -0400

    Split data/data_emerald.s

---
+++ data/battle_tent.s
@@ -0,0 +1,22 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+
+gUnknown_086160B4:: @ 86160B4
+	.incbin "baserom.gba", 0x6160b4, 0x20
+
+gUnknown_086160D4:: @ 86160D4
+	.incbin "baserom.gba", 0x6160d4, 0x4
+
+gUnknown_086160D8:: @ 86160D8
+	.incbin "baserom.gba", 0x6160d8, 0x1c
+
+gUnknown_086160F4:: @ 86160F4
+	.incbin "baserom.gba", 0x6160f4, 0x4
+
+gUnknown_086160F8:: @ 86160F8
+	.incbin "baserom.gba", 0x6160f8, 0x28
+
+gUnknown_08616120:: @ 8616120
+	.incbin "baserom.gba", 0x616120, 0x4
