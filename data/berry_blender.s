a8a4a500abf23b8ab3ea206614b94e0f16d9af7d
scnorton <scnorton@biociphers.org>
2017-09-25 15:27:54 -0400

    split data3_a0_A

---
+++ data/berry_blender.s
@@ -0,0 +1,167 @@
+    .include "asm/macros.inc"
+    .include "constants/constants.inc"
+
+    .section .rodata
+    .align 2, 0
+
+gUnknown_083390D4:: @ 83390D4
+	.incbin "baserom.gba", 0x3390d4, 0x20
+
+gUnknown_083390F4:: @ 83390F4
+	.incbin "baserom.gba", 0x3390f4, 0x400
+
+gUnknown_083394F4:: @ 83394F4
+	.incbin "baserom.gba", 0x3394f4, 0x246
+
+gUnknown_0833973A:: @ 833973A
+	.incbin "baserom.gba", 0x33973a, 0x61
+
+gUnknown_0833979B:: @ 833979B
+	.incbin "baserom.gba", 0x33979b, 0x2
+
+gUnknown_0833979D:: @ 833979D
+	.incbin "baserom.gba", 0x33979d, 0x33
+
+gUnknown_083397D0:: @ 83397D0
+	.incbin "baserom.gba", 0x3397d0, 0x4a
+
+gUnknown_0833981A:: @ 833981A
+	.incbin "baserom.gba", 0x33981a, 0x17
+
+gUnknown_08339831:: @ 8339831
+	.incbin "baserom.gba", 0x339831, 0x27
+
+gUnknown_08339858:: @ 8339858
+	.incbin "baserom.gba", 0x339858, 0x3e
+
+gUnknown_08339896:: @ 8339896
+	.incbin "baserom.gba", 0x339896, 0x1a
+
+gUnknown_083398B0:: @ 83398B0
+	.incbin "baserom.gba", 0x3398b0, 0x2d
+
+gUnknown_083398DD:: @ 83398DD
+	.incbin "baserom.gba", 0x3398dd, 0x18
+
+gUnknown_083398F5:: @ 83398F5
+	.incbin "baserom.gba", 0x3398f5, 0x1f
+
+gUnknown_08339914:: @ 8339914
+	.incbin "baserom.gba", 0x339914, 0x7
+
+gUnknown_0833991B:: @ 833991B
+	.incbin "baserom.gba", 0x33991b, 0x6
+
+gUnknown_08339921:: @ 8339921
+	.incbin "baserom.gba", 0x339921, 0x7
+
+gUnknown_08339928:: @ 8339928
+	.incbin "baserom.gba", 0x339928, 0x6
+
+gUnknown_0833992E:: @ 833992E
+	.incbin "baserom.gba", 0x33992e, 0xe
+
+gUnknown_0833993C:: @ 833993C
+	.incbin "baserom.gba", 0x33993c, 0x5
+
+gUnknown_08339941:: @ 8339941
+	.incbin "baserom.gba", 0x339941, 0x2
+
+gUnknown_08339943:: @ 8339943
+	.incbin "baserom.gba", 0x339943, 0x4
+
+gUnknown_08339947:: @ 8339947
+	.incbin "baserom.gba", 0x339947, 0x8
+
+gUnknown_0833994F:: @ 833994F
+	.incbin "baserom.gba", 0x33994f, 0xe
+
+gUnknown_0833995D:: @ 833995D
+	.incbin "baserom.gba", 0x33995d, 0x13
+
+gUnknown_08339970:: @ 8339970
+	.incbin "baserom.gba", 0x339970, 0x4
+
+gUnknown_08339974:: @ 8339974
+	.incbin "baserom.gba", 0x339974, 0xc
+
+gUnknown_08339980:: @ 8339980
+	.incbin "baserom.gba", 0x339980, 0x38
+
+gUnknown_083399B8:: @ 83399B8
+	.incbin "baserom.gba", 0x3399b8, 0x8
+
+gUnknown_083399C0:: @ 83399C0
+	.incbin "baserom.gba", 0x3399c0, 0x8
+
+gUnknown_083399C8:: @ 83399C8
+	.incbin "baserom.gba", 0x3399c8, 0x8
+
+gUnknown_083399D0:: @ 83399D0
+	.incbin "baserom.gba", 0x3399d0, 0xc
+
+gUnknown_083399DC:: @ 83399DC
+	.incbin "baserom.gba", 0x3399dc, 0x8
+
+gUnknown_083399E4:: @ 83399E4
+	.incbin "baserom.gba", 0x3399e4, 0x3
+
+gUnknown_083399E7:: @ 83399E7
+	.incbin "baserom.gba", 0x3399e7, 0x5
+
+gUnknown_083399EC:: @ 83399EC
+	.incbin "baserom.gba", 0x3399ec, 0xd4
+
+gUnknown_08339AC0:: @ 8339AC0
+	.incbin "baserom.gba", 0x339ac0, 0x8
+
+gUnknown_08339AC8:: @ 8339AC8
+	.incbin "baserom.gba", 0x339ac8, 0x8
+
+gUnknown_08339AD0:: @ 8339AD0
+	.incbin "baserom.gba", 0x339ad0, 0x8
+
+gUnknown_08339AD8:: @ 8339AD8
+	.incbin "baserom.gba", 0x339ad8, 0x60
+
+gUnknown_08339B38:: @ 8339B38
+	.incbin "baserom.gba", 0x339b38, 0x8
+
+gUnknown_08339B40:: @ 8339B40
+	.incbin "baserom.gba", 0x339b40, 0x98
+
+gUnknown_08339BD8:: @ 8339BD8
+	.incbin "baserom.gba", 0x339bd8, 0x8
+
+gUnknown_08339BE0:: @ 8339BE0
+	.incbin "baserom.gba", 0x339be0, 0x44
+
+gUnknown_08339C24:: @ 8339C24
+	.incbin "baserom.gba", 0x339c24, 0x8
+
+gUnknown_08339C2C:: @ 8339C2C
+	.incbin "baserom.gba", 0x339c2c, 0x2c
+
+gUnknown_08339C58:: @ 8339C58
+	.incbin "baserom.gba", 0x339c58, 0x8
+
+gUnknown_08339C60:: @ 8339C60
+	.incbin "baserom.gba", 0x339c60, 0x18
+
+gUnknown_08339C78:: @ 8339C78
+	.incbin "baserom.gba", 0x339c78, 0x28
+
+gUnknown_08339CA0:: @ 8339CA0
+	.incbin "baserom.gba", 0x339ca0, 0x1e
+
+gUnknown_08339CBE:: @ 8339CBE
+	.incbin "baserom.gba", 0x339cbe, 0x5
+
+gUnknown_08339CC3:: @ 8339CC3
+	.incbin "baserom.gba", 0x339cc3, 0x5
+
+gUnknown_08339CC8:: @ 8339CC8
+	.incbin "baserom.gba", 0x339cc8, 0x4c
+
+gUnknown_08339D14:: @ 8339D14
+	.incbin "baserom.gba", 0x339d14, 0x8
