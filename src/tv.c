ba631663f06eba85233a174e5d89333a06fb5a65
PikalaxALT <pikalaxalt@gmail.com>
2017-09-25 20:22:31 -0400

    special_0x44

--- src/tv.c
+++ src/tv.c
@@ -1,6 +1,7 @@
 
 // Includes
 #include "global.h"
+#include "rng.h"
 #include "tv.h"
 
 // Static type declarations
@@ -10,6 +11,7 @@
 // Static ROM declarations
 
 void sub_80EEE5C(void);
+u8 sub_80EFFE0(u8);
 
 // .rodata
 
@@ -30,4 +32,49 @@ void ClearTVShowData(void)
         }
     }
     sub_80EEE5C();
+}
+
+u8 special_0x44(void)
+{
+    u8 i;
+    u8 j;
+    u8 selIdx;
+    struct TVShowMassOutbreak *massOutbreak;
+
+    for (i = 5; i < ARRAY_COUNT(gSaveBlock1Ptr->tvShows) - 1; i ++)
+    {
+        if (gSaveBlock1Ptr->tvShows[i].common.var00 == 0)
+        {
+            break;
+        }
+    }
+    j = Random() % i;
+    selIdx = j;
+    do
+    {
+        if (sub_80EFFE0(gSaveBlock1Ptr->tvShows[j].common.var00) != 4)
+        {
+            if (gSaveBlock1Ptr->tvShows[j].common.var01 == TRUE)
+            {
+                return j;
+            }
+        }
+        else
+        {
+            massOutbreak = &gSaveBlock1Ptr->tvShows[j].massOutbreak;
+            if (massOutbreak->var16 == 0 && massOutbreak->var01 == TRUE)
+            {
+                return j;
+            }
+        }
+        if (j == 0)
+        {
+            j = ARRAY_COUNT(gSaveBlock1Ptr->tvShows) - 2;
+        }
+        else
+        {
+            j --;
+        }
+    } while (j != selIdx);
+    return 0xFF;
 }
