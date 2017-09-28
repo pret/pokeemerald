f1fe0c217d5366123c057da85b28b5c10ac9798d
DizzyEggg <jajkodizzy@wp.pl>
2017-09-28 20:52:36 +0200

    decompile roamer

--- include/roamer.h
+++ include/roamer.h
@@ -3,9 +3,15 @@
 
 void ClearRoamerData(void);
 void ClearRoamerLocationData(void);
+void InitRoamer(void);
 void UpdateLocationHistoryForRoamer(void);
 void RoamerMoveToOtherLocationSet(void);
-void RoamerMove();
+void RoamerMove(void);
+bool8 IsRoamerAt(u8 mapGroup, u8 mapNum);
+void CreateRoamerMonInstance(void);
 u8 TryStartRoamerEncounter(void);
+void UpdateRoamerHPStatus(struct Pokemon *mon);
+void SetRoamerInactive(void);
+void GetRoamerLocation(u8 *mapGroup, u8 *mapNum);
 
 #endif // GUARD_ROAMER_H
