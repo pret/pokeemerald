64631bac9365cf8df3798d9d3be8746d4d641685
PikalaxALT <pikalaxalt@gmail.com>
2017-09-30 13:08:28 -0400

    BSS: rom4.o, field_camera.o

--- src/berry_blender.c
+++ src/berry_blender.c
@@ -5,7 +5,7 @@
 // Static type declarations
 
 // Static RAM declarations
-IWRAM_DATA u32 berry_blender_c_unused_03000de4;
+IWRAM_DATA void *berry_blender_c_unused_03000de4;
 IWRAM_DATA s16 gUnknown_03000DE8[8];
 IWRAM_DATA s16 gUnknown_03000DF8[6];
 IWRAM_DATA s16 gUnknown_03000E04;
