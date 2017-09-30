5d9d9c14235c93ac35f8833cdaca8f0d0b9b9c36
DizzyEggg <jajkodizzy@wp.pl>
2017-09-30 09:11:48 +0200

    split, rename for real this time

--- data/battle_anim_815A0D4.s
+++ data/battle_anim_815A0D4.s
@@ -64,20 +64,3 @@ gUnknown_085CE87C:: @ 85CE87C
 gUnknown_085CE9C8:: @ 85CE9C8
 	.incbin "baserom.gba", 0x5ce9c8, 0x1e8
 
-gUnknown_085CEBB0:: @ 85CEBB0
-	.incbin "baserom.gba", 0x5cebb0, 0x8
-
-gUnknown_085CEBB8:: @ 85CEBB8
-	.incbin "baserom.gba", 0x5cebb8, 0x8
-
-gUnknown_085CEBC0:: @ 85CEBC0
-	.incbin "baserom.gba", 0x5cebc0, 0x10
-
-gUnknown_085CEBD0:: @ 85CEBD0
-	.incbin "baserom.gba", 0x5cebd0, 0x40
-
-gUnknown_085CEC10:: @ 85CEC10
-	.incbin "baserom.gba", 0x5cec10, 0x18
-
-gUnknown_085CEC28:: @ 85CEC28
-	.incbin "baserom.gba", 0x5cec28, 0x8
