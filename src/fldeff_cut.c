572648ba2d82dbdc8a05ba3d38908e1a1c326e0e
PikalaxALT <pikalaxalt@gmail.com>
2017-10-02 21:51:22 -0400

    BSS: field_screen, field_effect, pokemon_storage_system, fldeff_cut, script_menu

---
+++ src/fldeff_cut.c
@@ -0,0 +1,19 @@
+
+// Includes
+#include "global.h"
+
+// Static type declarations
+
+// Static RAM declarations
+
+IWRAM_DATA u8 gUnknown_03001100;
+IWRAM_DATA u8 gUnknown_03001101;
+IWRAM_DATA u8 gUnknown_03001102;
+IWRAM_DATA u32 fldeff_cut_unused_03001104;
+IWRAM_DATA u8 gUnknown_03001108[25];
+
+// Static ROM declarations
+
+// .rodata
+
+// .text
