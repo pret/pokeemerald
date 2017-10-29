6fcde15bb671db34dd1f17406ee20f319595b6a5
PikalaxALT <pikalaxalt@gmail.com>
2017-10-28 20:43:33 -0400

    Decompile heal location data

---
+++ include/heal_location.h
@@ -0,0 +1,18 @@
+#ifndef GUARD_HEAL_LOCATION_H
+#define GUARD_HEAL_LOCATION_H
+
+// Exported type declarations
+
+struct HealLocation
+{
+    s8 group;
+    s8 map;
+    u16 x;
+    u16 y;
+};
+
+// Exported RAM declarations
+
+// Exported ROM declarations
+
+#endif //GUARD_HEAL_LOCATION_H
