fd80cc40c0e669111766d82497989d26821c7b55
itskobold <itskobold@users.noreply.github.com>
2017-11-07 23:23:09 +0000

    daycare.s
    a few pointers

--- data/daycare.s
+++ data/daycare.s
@@ -18,7 +18,12 @@ gUnknown_0832B6E0:: @ 832B6E0
 	.incbin "baserom.gba", 0x32b6e0, 0x18
 
 gUnknown_0832B6F8:: @ 832B6F8
-	.incbin "baserom.gba", 0x32b6f8, 0x10
+@ replacing .incbin "baserom.gba", 0x0032b6f8, 0x10
+.4byte 0x85ef7da
+.4byte 0x85ef7ff
+.4byte 0x85ef81a
+.4byte 0x85ef846
 
 gEggName:: @ 832B708
 	.string "タマゴ$" @ "tamago" ("egg" in Japanese)
+
