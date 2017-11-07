dedae4a1d0f265b32bbdc89d147875192b52ca91
itskobold <itskobold@users.noreply.github.com>
2017-11-07 14:51:51 +0000

    battle_frontier_1.s, unk_transition.s dumproms
    still a fair bit to dump for battle_frontier_1.s

--- data/unk_transition.s
+++ data/unk_transition.s
@@ -4,25 +4,66 @@
 	.section .rodata
 
 gUnknown_0862B770:: @ 862B770
-	.incbin "baserom.gba", 0x62b770, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b770, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DAC14
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DAC80
 
 gUnknown_0862B784:: @ 862B784
-	.incbin "baserom.gba", 0x62b784, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b784, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DACEC
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DAD58
 
 gUnknown_0862B798:: @ 862B798
-	.incbin "baserom.gba", 0x62b798, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b798, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DADC4
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DAE44
 
 gUnknown_0862B7AC:: @ 862B7AC
-	.incbin "baserom.gba", 0x62b7ac, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b7ac, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DAEB0
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DAF34
 
 gUnknown_0862B7C0:: @ 862B7C0
-	.incbin "baserom.gba", 0x62b7c0, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b7c0, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DAFA0
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DB02C
 
 gUnknown_0862B7D4:: @ 862B7D4
-	.incbin "baserom.gba", 0x62b7d4, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b7d4, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DB098
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DB124
 
 gUnknown_0862B7E8:: @ 862B7E8
-	.incbin "baserom.gba", 0x62b7e8, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b7e8, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DB190
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DB224
 
 gUnknown_0862B7FC:: @ 862B7FC
-	.incbin "baserom.gba", 0x62b7fc, 0x14
+@ replacing .incbin "baserom.gba", 0x0062b7fc, 0x14
+.4byte sub_81DAACC
+.4byte sub_81DB290
+.4byte sub_81DABBC
+.4byte sub_81DAB4C
+.4byte sub_81DB328
+
