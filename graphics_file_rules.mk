5c44c640509f9a80d62ecb9cc5bccee20ca5b2b5
DizzyEggg <jajkodizzy@wp.pl>
2017-11-13 21:55:38 +0100

    add hof graphics

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -288,5 +288,5 @@ graphics/title_screen/pokemon_logo.gbapal: graphics/title_screen/pokemon_logo.pa
 $(INTROGFXDIR)/copyright.4bpp: $(INTROGFXDIR)/copyright.png
 	$(GFX) $< $@ -num_tiles 39
 
-$(interfacedir)/pkmnjump_bg.4bpp: $(interfacedir)/pkmnjump_bg.png
-	$(GFX) $< $@ -num_tiles 63
+graphics/misc/japanese_hof.4bpp: graphics/misc/japanese_hof.png
+	$(GFX) $< $@ -num_tiles 29
