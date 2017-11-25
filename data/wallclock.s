2746967bef9e82df0655391176283c064997cf7b
PikalaxALT <pikalaxalt@gmail.com>
2017-11-24 20:46:29 -0500

    through sub_8134CE8

--- data/wallclock.s
+++
@@ -1,9 +0,0 @@
-@ the fourth big chunk of data
-
-	.include "asm/macros.inc"
-	.include "constants/constants.inc"
-
-	.section .rodata
-
-gUnknown_085B22D0:: @ 85B22D0
-	.incbin "baserom.gba", 0x5b22d0, 0x2d0
