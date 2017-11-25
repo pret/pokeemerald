5f631dc2234a572b977226715f547fc78b366b70
PikalaxALT <pikalaxalt@gmail.com>
2017-11-25 14:09:54 -0500

    through sub_81C7388

--- include/pokemon_storage_system.h
+++ include/pokemon_storage_system.h
@@ -9,5 +9,6 @@ struct BoxPokemon *GetBoxedMonPtr(u8, u8);
 void SetBoxMonNickFromAnyBox(u8, u8, u8 *);
 void CompactPartySlots(void);
 u32 GetBoxMonDataFromAnyBox(u8 boxId, u8 monPosition, u32 request);
+bool32 sub_80D2270(int boxId, int monPosition);
 
 #endif // GUARD_POKEMON_STORAGE_SYSTEM_H
--- src/pokenav.c
+++ src/pokenav.c
@@ -5,25 +5,30 @@
 #include "palette.h"
 #include "overworld.h"
 #include "field_weather.h"
+#include "pokemon_storage_system.h"
 #include "pokenav.h"
 
 // static types
 
 struct PokenavStruct_203CF40 {
-    u8 filler_00[8];
+    int unk_00;
+    int unk_04;
     bool16 unk_08;
-    u8 filler_0a[0x52];
+    bool32 anyMonsHaveRibbons;
+    void *unk_10[19];
 }; // size=0x5c
 
 // static declarations
 
 EWRAM_DATA struct PokenavStruct_203CF40 *gUnknown_0203CF40 = NULL;
 
-void sub_81C72BC(void);
-void sub_81C7360(void);
+static void sub_81C72BC(void);
+static void sub_81C7360(struct PokenavStruct_203CF40 *data);
+static bool32 sub_81C7388(void);
 void sub_81C7400(void);
 void sub_81C7418(void);
 void sub_81C742C(u8 taskId);
+void sub_81C7650(int i);
 
 // rodata
 
@@ -38,7 +43,7 @@ void CB2_PokeNav(void)
     }
     else
     {
-        sub_81C7360();
+        sub_81C7360(gUnknown_0203CF40);
         ResetTasks();
         SetVBlankCallback(NULL);
         CreateTask(sub_81C742C, 0);
@@ -53,7 +58,7 @@ void sub_81C72A4(void)
     fade_screen(1, 0);
 }
 
-void sub_81C72BC(void)
+static void sub_81C72BC(void)
 {
     UpdatePaletteFade();
     if (!gPaletteFade.active)
@@ -65,7 +70,7 @@ void sub_81C72BC(void)
         }
         else
         {
-            sub_81C7360();
+            sub_81C7360(gUnknown_0203CF40);
             gUnknown_0203CF40->unk_08 = TRUE;
             ResetTasks();
             ResetSpriteData();
@@ -76,4 +81,56 @@ void sub_81C72BC(void)
             SetVBlankCallback(sub_81C7418);
         }
     }
+}
+
+void sub_81C7334(void)
+{
+    int i;
+
+    for (i = 0; i < 19; i++)
+    {
+        sub_81C7650(i);
+    }
+    free(gUnknown_0203CF40);
+    gUnknown_0203CF40 = NULL;
+    InitKeys();
+}
+
+static void sub_81C7360(struct PokenavStruct_203CF40 *data)
+{
+    int i;
+
+    for (i = 0; i < 19; i++)
+    {
+        data->unk_10[i] = NULL;
+    }
+    data->unk_08 = FALSE;
+    data->unk_04 = 0;
+    data->anyMonsHaveRibbons = sub_81C7388();
+    data->unk_00 = 0;
+}
+
+static bool32 sub_81C7388(void)
+{
+    int i;
+    int j;
+
+    for (i = 0; i < PARTY_SIZE; i++)
+    {
+        if (GetMonData(&gPlayerParty[i], MON_DATA_SANITY_BIT2) && !GetMonData(&gPlayerParty[i], MON_DATA_SANITY_BIT3) && GetMonData(&gPlayerParty[i], MON_DATA_RIBBON_COUNT) != 0)
+        {
+            return TRUE;
+        }
+    }
+    for (j = 0; j < 14; j++)
+    {
+        for (i = 0; i < 30; i++)
+        {
+            if (sub_80D2270(j, i) && GetBoxMonDataFromAnyBox(j, i, MON_DATA_RIBBON_COUNT) != 0)
+            {
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
 }
