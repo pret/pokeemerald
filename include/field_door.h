a11c65ff1ac380cd213b26d0bb7a2787936b33f9
Diegoisawesome <diego@domoreaweso.me>
2018-01-07 17:50:13 -0600

    Port field_door

--- include/field_door.h
+++ include/field_door.h
@@ -1,11 +1,27 @@
 #ifndef GUARD_FIELDDOOR_H
 #define GUARD_FIELDDOOR_H
 
+struct DoorGraphics
+{
+    u16 metatileNum;
+    u8 sound;
+    u8 size;
+    const void *tiles;
+    const void *palette;
+};
+
+struct DoorAnimFrame
+{
+    u8 time;
+    u16 offset;
+};
+
 void FieldSetDoorOpened(u32, u32);
 void FieldSetDoorClosed(u32, u32);
 s8 FieldAnimateDoorClose(u32, u32);
 s8 FieldAnimateDoorOpen(u32, u32);
 bool8 FieldIsDoorAnimationRunning(void);
 u32 GetDoorSoundEffect(u32 x, u32 y);
+bool8 sub_808A964(void);
 
 #endif
