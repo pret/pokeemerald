efe19e95c846a658c5e9bd9908de548db00dcab5
Diegoisawesome <diego@domoreaweso.me>
2018-01-07 01:29:26 -0600

    Extract door animations

--- data/field_door.s
+++ data/field_door.s
@@ -1,17 +1,614 @@
-    .include "asm/macros.inc"
-    .include "constants/constants.inc"
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
 
-    .section .rodata
-    .align 2, 0
+	.section .rodata
+    .align 2
 
+DoorAnimTiles_04::
+    .incbin "graphics/door_anims/04/0.4bpp"
+    .incbin "graphics/door_anims/04/1.4bpp"
+    .incbin "graphics/door_anims/04/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_05::
+    .incbin "graphics/door_anims/05/0.4bpp"
+    .incbin "graphics/door_anims/05/1.4bpp"
+    .incbin "graphics/door_anims/05/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_08::
+    .incbin "graphics/door_anims/08/0.4bpp"
+    .incbin "graphics/door_anims/08/1.4bpp"
+    .incbin "graphics/door_anims/08/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_15::
+    .incbin "graphics/door_anims/15/0.4bpp"
+    .incbin "graphics/door_anims/15/1.4bpp"
+    .incbin "graphics/door_anims/15/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_16::
+    .incbin "graphics/door_anims/16/0.4bpp"
+    .incbin "graphics/door_anims/16/1.4bpp"
+    .incbin "graphics/door_anims/16/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_00::
+    .incbin "graphics/door_anims/00/0.4bpp"
+    .incbin "graphics/door_anims/00/1.4bpp"
+    .incbin "graphics/door_anims/00/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_01::
+    .incbin "graphics/door_anims/01/0.4bpp"
+    .incbin "graphics/door_anims/01/1.4bpp"
+    .incbin "graphics/door_anims/01/2.4bpp"
+
+DoorAnimTiles_02::
+    .incbin "graphics/door_anims/02/0.4bpp"
+    .incbin "graphics/door_anims/02/1.4bpp"
+    .incbin "graphics/door_anims/02/2.4bpp"
+
+DoorAnimTiles_03::
+    .incbin "graphics/door_anims/03/0.4bpp"
+    .incbin "graphics/door_anims/03/1.4bpp"
+    .incbin "graphics/door_anims/03/2.4bpp"
+
+DoorAnimTiles_06::
+    .incbin "graphics/door_anims/06/0.4bpp"
+    .incbin "graphics/door_anims/06/1.4bpp"
+    .incbin "graphics/door_anims/06/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_07::
+    .incbin "graphics/door_anims/07/0.4bpp"
+    .incbin "graphics/door_anims/07/1.4bpp"
+    .incbin "graphics/door_anims/07/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_09::
+    .incbin "graphics/door_anims/09/0.4bpp"
+    .incbin "graphics/door_anims/09/1.4bpp"
+    .incbin "graphics/door_anims/09/2.4bpp"
+
+	.space 0x5900
+
+	.incbin "graphics/door_anims/unused_848EDEC/0_top.4bpp"
+	.incbin "graphics/door_anims/unused_848EDEC/1_top.4bpp"
+	.incbin "graphics/door_anims/unused_848EDEC/2_top.4bpp"
+
+	.space 0x140
+
+	.incbin "graphics/door_anims/unused_848EDEC/0_bottom.4bpp"
+	.incbin "graphics/door_anims/unused_848EDEC/1_bottom.4bpp"
+	.incbin "graphics/door_anims/unused_848EDEC/2_bottom.4bpp"
+
+	.space 32
+
+DoorAnimTiles_10::
+    .incbin "graphics/door_anims/10/0.4bpp"
+    .incbin "graphics/door_anims/10/1.4bpp"
+    .incbin "graphics/door_anims/10/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_11::
+    .incbin "graphics/door_anims/11/0.4bpp"
+    .incbin "graphics/door_anims/11/1.4bpp"
+    .incbin "graphics/door_anims/11/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_12::
+    .incbin "graphics/door_anims/12/0.4bpp"
+    .incbin "graphics/door_anims/12/1.4bpp"
+    .incbin "graphics/door_anims/12/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_13::
+    .incbin "graphics/door_anims/13/0.4bpp"
+    .incbin "graphics/door_anims/13/1.4bpp"
+    .incbin "graphics/door_anims/13/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_14::
+    .incbin "graphics/door_anims/14/0.4bpp"
+    .incbin "graphics/door_anims/14/1.4bpp"
+    .incbin "graphics/door_anims/14/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_17::
+    .incbin "graphics/door_anims/17/0.4bpp"
+    .incbin "graphics/door_anims/17/1.4bpp"
+    .incbin "graphics/door_anims/17/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_18::
+    .incbin "graphics/door_anims/18/0.4bpp"
+    .incbin "graphics/door_anims/18/1.4bpp"
+    .incbin "graphics/door_anims/18/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_19::
+    .incbin "graphics/door_anims/19/0.4bpp"
+    .incbin "graphics/door_anims/19/1.4bpp"
+    .incbin "graphics/door_anims/19/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_20::
+    .incbin "graphics/door_anims/20/0.4bpp"
+    .incbin "graphics/door_anims/20/1.4bpp"
+    .incbin "graphics/door_anims/20/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_21::
+    .incbin "graphics/door_anims/21/0.4bpp"
+    .incbin "graphics/door_anims/21/1.4bpp"
+    .incbin "graphics/door_anims/21/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_22::
+    .incbin "graphics/door_anims/22/0.4bpp"
+    .incbin "graphics/door_anims/22/1.4bpp"
+    .incbin "graphics/door_anims/22/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_23::
+    .incbin "graphics/door_anims/23/0.4bpp"
+    .incbin "graphics/door_anims/23/1.4bpp"
+    .incbin "graphics/door_anims/23/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_24::
+    .incbin "graphics/door_anims/24/0.4bpp"
+    .incbin "graphics/door_anims/24/1.4bpp"
+    .incbin "graphics/door_anims/24/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_25::
+    .incbin "graphics/door_anims/25/0.4bpp"
+    .incbin "graphics/door_anims/25/1.4bpp"
+    .incbin "graphics/door_anims/25/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_26::
+    .incbin "graphics/door_anims/26/0.4bpp"
+    .incbin "graphics/door_anims/26/1.4bpp"
+    .incbin "graphics/door_anims/26/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_27::
+    .incbin "graphics/door_anims/27/0.4bpp"
+    .incbin "graphics/door_anims/27/1.4bpp"
+    .incbin "graphics/door_anims/27/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_28::
+    .incbin "graphics/door_anims/28/0.4bpp"
+    .incbin "graphics/door_anims/28/1.4bpp"
+    .incbin "graphics/door_anims/28/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_29::
+    .incbin "graphics/door_anims/29/0.4bpp"
+    .incbin "graphics/door_anims/29/1.4bpp"
+    .incbin "graphics/door_anims/29/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_30::
+    .incbin "graphics/door_anims/30/0.4bpp"
+    .incbin "graphics/door_anims/30/1.4bpp"
+    .incbin "graphics/door_anims/30/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_31::
+    .incbin "graphics/door_anims/31/0.4bpp"
+    .incbin "graphics/door_anims/31/1.4bpp"
+    .incbin "graphics/door_anims/31/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_32::
+    .incbin "graphics/door_anims/32/0.4bpp"
+    .incbin "graphics/door_anims/32/1.4bpp"
+    .incbin "graphics/door_anims/32/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_33::
+    .incbin "graphics/door_anims/33/0.4bpp"
+    .incbin "graphics/door_anims/33/1.4bpp"
+    .incbin "graphics/door_anims/33/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_34::
+    .incbin "graphics/door_anims/34/0.4bpp"
+    .incbin "graphics/door_anims/34/1.4bpp"
+    .incbin "graphics/door_anims/34/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_35::
+    .incbin "graphics/door_anims/35/0.4bpp"
+    .incbin "graphics/door_anims/35/1.4bpp"
+    .incbin "graphics/door_anims/35/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_36::
+    .incbin "graphics/door_anims/36/0.4bpp"
+    .incbin "graphics/door_anims/36/1.4bpp"
+    .incbin "graphics/door_anims/36/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_37::
+    .incbin "graphics/door_anims/37/0.4bpp"
+    .incbin "graphics/door_anims/37/1.4bpp"
+    .incbin "graphics/door_anims/37/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_38::
+    .incbin "graphics/door_anims/38/0.4bpp"
+    .incbin "graphics/door_anims/38/1.4bpp"
+    .incbin "graphics/door_anims/38/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_39::
+    .incbin "graphics/door_anims/39/0.4bpp"
+    .incbin "graphics/door_anims/39/1.4bpp"
+    .incbin "graphics/door_anims/39/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_40::
+    .incbin "graphics/door_anims/40/0.4bpp"
+    .incbin "graphics/door_anims/40/1.4bpp"
+    .incbin "graphics/door_anims/40/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_41::
+    .incbin "graphics/door_anims/41/0.4bpp"
+    .incbin "graphics/door_anims/41/1.4bpp"
+    .incbin "graphics/door_anims/41/2.4bpp"
+
+DoorAnimTiles_42::
+    .incbin "graphics/door_anims/42/0.4bpp"
+    .incbin "graphics/door_anims/42/1.4bpp"
+    .incbin "graphics/door_anims/42/2.4bpp"
+    .incbin "graphics/door_anims/42/3.4bpp"
+
+	.space 32
+
+DoorAnimTiles_43::
+    .incbin "graphics/door_anims/43/0.4bpp"
+    .incbin "graphics/door_anims/43/1.4bpp"
+    .incbin "graphics/door_anims/43/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_44::
+    .incbin "graphics/door_anims/44/0_left.4bpp"
+    .incbin "graphics/door_anims/44/0_right.4bpp"
+    .incbin "graphics/door_anims/44/1_left.4bpp"
+    .incbin "graphics/door_anims/44/1_right.4bpp"
+    .incbin "graphics/door_anims/44/2_left.4bpp"
+    .incbin "graphics/door_anims/44/2_right.4bpp"
+
+	.space 32
+
+DoorAnimTiles_45::
+    .incbin "graphics/door_anims/45/0.4bpp"
+    .incbin "graphics/door_anims/45/1.4bpp"
+    .incbin "graphics/door_anims/45/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_46::
+    .incbin "graphics/door_anims/46/0.4bpp"
+    .incbin "graphics/door_anims/46/1.4bpp"
+    .incbin "graphics/door_anims/46/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_47::
+    .incbin "graphics/door_anims/47/0.4bpp"
+    .incbin "graphics/door_anims/47/1.4bpp"
+    .incbin "graphics/door_anims/47/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_48::
+    .incbin "graphics/door_anims/48/0.4bpp"
+    .incbin "graphics/door_anims/48/1.4bpp"
+    .incbin "graphics/door_anims/48/2.4bpp"
+    .incbin "graphics/door_anims/48/3.4bpp"
+
+	.space 32
+
+DoorAnimTiles_49::
+    .incbin "graphics/door_anims/49/0.4bpp"
+    .incbin "graphics/door_anims/49/1.4bpp"
+    .incbin "graphics/door_anims/49/2.4bpp"
+
+	.space 32
+
+DoorAnimTiles_50::
+    .incbin "graphics/door_anims/50/0.4bpp"
+    .incbin "graphics/door_anims/50/1.4bpp"
+    .incbin "graphics/door_anims/50/2.4bpp"
+
+	.space 32
+
+	.align 2
 gUnknown_08496F8C:: @ 8496F8C
-	.incbin "baserom.gba", 0x496f8c, 0x14
+	door_anim_frame 4, -1
+	door_anim_frame 4, 0
+	door_anim_frame 4, 0x100
+	door_anim_frame 4, 0x200
+	door_anim_frame 0, 0
 
+	.align 2
 gUnknown_08496FA0:: @ 8496FA0
-	.incbin "baserom.gba", 0x496fa0, 0x14
+	door_anim_frame 4, 0x200
+	door_anim_frame 4, 0x100
+	door_anim_frame 4, 0
+	door_anim_frame 4, -1
+	door_anim_frame 0, 0
 
+	.align 2
 gUnknown_08496FB4:: @ 8496FB4
-	.incbin "baserom.gba", 0x496fb4, 0x1c0
+	door_anim_frame 4, -1
+	door_anim_frame 4, 0
+	door_anim_frame 4, 0x200
+	door_anim_frame 4, 0x400
+	door_anim_frame 0, 0
 
+	.align 2
+gUnknown_08496FC8:: @ 8496FB4
+	door_anim_frame 4, 0x400
+	door_anim_frame 4, 0x200
+	door_anim_frame 4, 0
+	door_anim_frame 4, -1
+	door_anim_frame 0, 0
+
+DoorAnimPalettes_8496FDC:: @ 8496FDC
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_8496FE4:: @ 8496FE4
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_8496FEC:: @ 8496FEC
+	.byte 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05
+
+DoorAnimPalettes_8496FF4:: @ 8496FF4
+	.byte 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_8496FFC:: @ 8496FFC
+	.byte 0x0a, 0x0a, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06
+
+DoorAnimPalettes_8497004:: @ 8497004
+	.byte 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08
+
+DoorAnimPalettes_849700C:: @ 849700C
+	.byte 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b
+
+DoorAnimPalettes_8497014:: @ 8497014
+	.byte 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a
+
+DoorAnimPalettes_849701C:: @ 849701C
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_8497024:: @ 8497024
+	.byte 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08
+
+DoorAnimPalettes_849702C:: @ 849702C
+	.byte 0x0a, 0x0a, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_8497034:: @ 8497034
+	.byte 0x09, 0x09, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_849703C:: @ 849703C
+	.byte 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08
+
+DoorAnimPalettes_8497044:: @ 8497044
+	.byte 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_849704C:: @ 849704C
+	.byte 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06
+
+DoorAnimPalettes_8497054:: @ 8497054
+	.byte 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06
+
+DoorAnimPalettes_849705C:: @ 849705C
+	.byte 0x00, 0x00, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05
+
+DoorAnimPalettes_8497064:: @ 8497064
+	.byte 0x06, 0x06, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_849706C:: @ 849706C
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_8497074:: @ 8497074
+	.byte 0x06, 0x06, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05
+
+DoorAnimPalettes_849707C:: @ 849707C
+	.byte 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05
+
+DoorAnimPalettes_8497084:: @ 8497084
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_849708C:: @ 849708C
+	.byte 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06
+
+DoorAnimPalettes_8497094:: @ 8497094
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_849709C:: @ 849709C
+	.byte 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05
+
+DoorAnimPalettes_84970A4:: @ 84970A4
+	.byte 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_84970AC:: @ 84970AC
+	.byte 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08
+
+DoorAnimPalettes_84970B4:: @ 84970B4
+	.byte 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06
+
+DoorAnimPalettes_84970BC:: @ 84970BC
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_84970C4:: @ 84970C4
+	.byte 0x0b, 0x0b, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_84970CC:: @ 84970CC
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_84970D4:: @ 84970D4
+	.byte 0x06, 0x06, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_84970DC:: @ 84970DC
+	.byte 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_84970E4:: @ 84970E4
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_84970EC:: @ 84970EC
+	.byte 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_84970F4:: @ 84970F4
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_84970FC:: @ 84970FC
+	.byte 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_8497104:: @ 8497104
+	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+DoorAnimPalettes_849710C:: @ 849710C
+	.byte 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05
+
+DoorAnimPalettes_8497114:: @ 8497114
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_849711C:: @ 849711C
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_8497124:: @ 8497124
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_849712C:: @ 849712C
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_8497134:: @ 8497134
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_849713C:: @ 849713C
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_8497144:: @ 8497144
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_849714C:: @ 849714C
+	.byte 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+DoorAnimPalettes_8497154:: @ 8497154
+	.byte 0x09, 0x09, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_849715C:: @ 849715C
+	.byte 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09, 0x09
+
+DoorAnimPalettes_8497164:: @ 8497164
+	.byte 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+DoorAnimPalettes_849716C:: @ 849716C
+	.byte 0x09, 0x09, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07
+
+	.align 2
 gUnknown_08497174:: @ 8497174
-	.incbin "baserom.gba", 0x497174, 0x288
+	door_anim_gfx 0x0021, 0x00, 0x01, DoorAnimTiles_00, DoorAnimPalettes_8496FDC
+	door_anim_gfx 0x0061, 0x01, 0x01, DoorAnimTiles_01, DoorAnimPalettes_8496FE4
+	door_anim_gfx 0x01cd, 0x01, 0x01, DoorAnimTiles_02, DoorAnimPalettes_8496FEC
+	door_anim_gfx 0x0041, 0x01, 0x01, DoorAnimTiles_03, DoorAnimPalettes_8496FF4
+	door_anim_gfx 0x0248, 0x00, 0x01, DoorAnimTiles_04, DoorAnimPalettes_8496FFC
+	door_anim_gfx 0x0249, 0x00, 0x01, DoorAnimTiles_05, DoorAnimPalettes_8497004
+	door_anim_gfx 0x022f, 0x00, 0x01, DoorAnimTiles_06, DoorAnimPalettes_849700C
+	door_anim_gfx 0x021f, 0x00, 0x01, DoorAnimTiles_07, DoorAnimPalettes_8497014
+	door_anim_gfx 0x02a5, 0x00, 0x01, DoorAnimTiles_08, DoorAnimPalettes_849701C
+	door_anim_gfx 0x0287, 0x00, 0x01, DoorAnimTiles_09, DoorAnimPalettes_849702C
+	door_anim_gfx 0x02ac, 0x00, 0x01, DoorAnimTiles_10, DoorAnimPalettes_849706C
+	door_anim_gfx 0x03a1, 0x00, 0x01, DoorAnimTiles_11, DoorAnimPalettes_8497074
+	door_anim_gfx 0x02dc, 0x00, 0x01, DoorAnimTiles_12, DoorAnimPalettes_8497064
+	door_anim_gfx 0x0225, 0x00, 0x01, DoorAnimTiles_13, DoorAnimPalettes_849705C
+	door_anim_gfx 0x01db, 0x01, 0x01, DoorAnimTiles_14, DoorAnimPalettes_8497084
+	door_anim_gfx 0x0246, 0x00, 0x01, DoorAnimTiles_15, DoorAnimPalettes_8497024
+	door_anim_gfx 0x028e, 0x00, 0x01, DoorAnimTiles_16, DoorAnimPalettes_849707C
+	door_anim_gfx 0x02a1, 0x00, 0x01, DoorAnimTiles_17, DoorAnimPalettes_8497034
+	door_anim_gfx 0x021c, 0x00, 0x01, DoorAnimTiles_18, DoorAnimPalettes_849704C
+	door_anim_gfx 0x021e, 0x00, 0x01, DoorAnimTiles_19, DoorAnimPalettes_8497054
+	door_anim_gfx 0x021d, 0x01, 0x01, DoorAnimTiles_20, DoorAnimPalettes_849703C
+	door_anim_gfx 0x021a, 0x00, 0x01, DoorAnimTiles_21, DoorAnimPalettes_8497044
+	door_anim_gfx 0x0224, 0x00, 0x01, DoorAnimTiles_22, DoorAnimPalettes_849708C
+	door_anim_gfx 0x0289, 0x00, 0x01, DoorAnimTiles_23, DoorAnimPalettes_8497094
+	door_anim_gfx 0x030c, 0x01, 0x01, DoorAnimTiles_24, DoorAnimPalettes_849709C
+	door_anim_gfx 0x032d, 0x01, 0x01, DoorAnimTiles_25, DoorAnimPalettes_84970A4
+	door_anim_gfx 0x02ed, 0x01, 0x01, DoorAnimTiles_26, DoorAnimPalettes_84970AC
+	door_anim_gfx 0x0264, 0x01, 0x01, DoorAnimTiles_27, DoorAnimPalettes_84970B4
+	door_anim_gfx 0x022b, 0x00, 0x01, DoorAnimTiles_28, DoorAnimPalettes_84970BC
+	door_anim_gfx 0x02f7, 0x00, 0x01, DoorAnimTiles_29, DoorAnimPalettes_84970C4
+	door_anim_gfx 0x0297, 0x00, 0x01, DoorAnimTiles_30, DoorAnimPalettes_84970CC
+	door_anim_gfx 0x0285, 0x01, 0x01, DoorAnimTiles_31, DoorAnimPalettes_84970D4
+	door_anim_gfx 0x025d, 0x01, 0x01, DoorAnimTiles_32, DoorAnimPalettes_84970DC
+	door_anim_gfx 0x020e, 0x01, 0x01, DoorAnimTiles_33, DoorAnimPalettes_84970E4
+	door_anim_gfx 0x03b0, 0x01, 0x01, DoorAnimTiles_34, DoorAnimPalettes_84970EC
+	door_anim_gfx 0x028a, 0x01, 0x01, DoorAnimTiles_35, DoorAnimPalettes_84970F4
+	door_anim_gfx 0x0263, 0x01, 0x01, DoorAnimTiles_36, DoorAnimPalettes_84970FC
+	door_anim_gfx 0x0329, 0x01, 0x01, DoorAnimTiles_37, DoorAnimPalettes_8497104
+	door_anim_gfx 0x0291, 0x00, 0x01, DoorAnimTiles_38, DoorAnimPalettes_849710C
+	door_anim_gfx 0x021b, 0x02, 0x01, DoorAnimTiles_39, DoorAnimPalettes_8497114
+	door_anim_gfx 0x0209, 0x01, 0x01, DoorAnimTiles_40, DoorAnimPalettes_849711C
+	door_anim_gfx 0x0219, 0x00, 0x01, DoorAnimTiles_41, DoorAnimPalettes_8497124
+	door_anim_gfx 0x0393, 0x01, 0x01, DoorAnimTiles_42, DoorAnimPalettes_849712C
+	door_anim_gfx 0x03d4, 0x01, 0x01, DoorAnimTiles_42, DoorAnimPalettes_849712C
+	door_anim_gfx 0x036c, 0x01, 0x01, DoorAnimTiles_42, DoorAnimPalettes_849712C
+	door_anim_gfx 0x025e, 0x01, 0x01, DoorAnimTiles_43, DoorAnimPalettes_8497134
+	door_anim_gfx 0x02ad, 0x01, 0x02, DoorAnimTiles_44, DoorAnimPalettes_849713C
+	door_anim_gfx 0x03fc, 0x00, 0x01, DoorAnimTiles_45, DoorAnimPalettes_849714C
+	door_anim_gfx 0x0396, 0x01, 0x01, DoorAnimTiles_46, DoorAnimPalettes_849714C
+	door_anim_gfx 0x020a, 0x01, 0x01, DoorAnimTiles_47, DoorAnimPalettes_8497154
+	door_anim_gfx 0x026b, 0x01, 0x01, DoorAnimTiles_48, DoorAnimPalettes_849715C
+	door_anim_gfx 0x032c, 0x01, 0x01, DoorAnimTiles_49, DoorAnimPalettes_8497164
+	door_anim_gfx 0x0383, 0x01, 0x01, DoorAnimTiles_50, DoorAnimPalettes_849716C
+	door_anim_gfx 0x0000, 0x00, 0x00, NULL, NULL
+
