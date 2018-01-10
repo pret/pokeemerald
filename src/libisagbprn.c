e1801885a0619724860cb322ba1cb8dba2b2c0f3
ProjectRevoTPP <projectrevotpp@hotmail.com>
2018-01-09 19:39:51 -0500

    both print defines for nocashgba

--- src/libisagbprn.c
+++ src/libisagbprn.c
@@ -11,7 +11,8 @@
 // originally for auto no$gba support, the string "no$gba" should be at this address,
 // the user needs to read this string out as the memory viewer won't show it.
 #define NOCASHGBAIDADDR 0x4FFFA00
-#define NOCASHGBAPRINTADDR 0x4FFFA14
+#define NOCASHGBAPRINTADDR1 0x4FFFA10 // automatically adds a newline after the string has finished
+#define NOCASHGBAPRINTADDR2 0x4FFFA14 // does not automatically add the newline. by default, NOCASHGBAPRINTADDR2 is used. this is used to keep strings consistent between no$gba and VBA-RR, but a user can choose to forgo this.
 
 struct AGBPrintStruct
 {
@@ -159,7 +160,7 @@ void AGBAssert(const char *pFile, int nLine, const char *pExpression, int nStopP
 /*
 void NoCashGBAPrint(const char *pBuf)
 {
-    *(volatile u32*)NOCASHGBAPRINTADDR = (u32)pBuf;
+    *(volatile u32*)NOCASHGBAPRINTADDR2 = (u32)pBuf;
 }
 
 void NoCashGBAPrintf(const char *pBuf, ...)
