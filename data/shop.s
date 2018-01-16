29d84dabe61051238c7b7e6dd805fc2de447ba72
golem galvanize <golemgalvanize@github.com>
2018-01-16 01:01:14 -0500

    spacing fix

--- data/shop.s
+++ data/shop.s
@@ -25,31 +25,31 @@ gUnknown_08589A28:: @ 8589A28
 
 gUnknown_08589A38:: @ 8589A38
 	window_template 0, 2, 1, 9, 6, 15, 8
-    window_template 0, 2, 1, 9, 4, 15, 8
+	window_template 0, 2, 1, 9, 4, 15, 8
 
 gUnknown_08589A48:: @ 8589A48
 	.byte 0, 0, 0, 0
-    .4byte BuyMenuPrintItemDescriptionAndShowItemIcon
-    .4byte BuyMenuPrintPriceInList 
-    .byte 0, 0, 0, 0, 1, 0, 8, 0, 33, 48, 0, 7
+	.4byte BuyMenuPrintItemDescriptionAndShowItemIcon
+	.4byte BuyMenuPrintPriceInList 
+	.byte 0, 0, 0, 0, 1, 0, 8, 0, 33, 48, 0, 7
 
 gUnknown_08589A60:: @ 8589A60
 	.4byte 0x1F8
-    .4byte 0x11E1
-    .4byte 0x21D2
-    .4byte 0x31C3
+	.4byte 0x11E1
+	.4byte 0x21D2
+	.4byte 0x31C3
 
 gUnknown_08589A70:: @ 8589A70
 	window_template 0, 1, 1, 10, 2, 15, 30
-    window_template 0, 14, 2, 15, 16, 15, 50
-    window_template 0, 0, 13, 14, 6, 15, 0x122
-    window_template 0, 1, 11, 12, 2, 15, 0x176
-    window_template 0, 18, 11, 10, 2, 15, 0x18E
-    window_template 0, 2, 15, 27, 4, 15, 0x1A2
-    window_template_terminator
+	window_template 0, 14, 2, 15, 16, 15, 50
+	window_template 0, 0, 13, 14, 6, 15, 0x122
+	window_template 0, 1, 11, 12, 2, 15, 0x176
+	window_template 0, 18, 11, 10, 2, 15, 0x18E
+	window_template 0, 2, 15, 27, 4, 15, 0x1A2
+	window_template_terminator
 
 gUnknown_08589AA8:: @ 8589AA8
-    window_template 0, 21, 9, 5, 4, 15, 0x20E
+	window_template 0, 21, 9, 5, 4, 15, 0x20E
 
 gUnknown_08589AB0:: @ 8589AB0
 	.byte 1, 2, 3
