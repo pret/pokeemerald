d66c1c642c60191887fc4dda6d33bb5b8e9ff69c
Diegoisawesome <diego@domoreaweso.me>
2018-05-09 05:07:56 -0500

    Port/decompile fieldmap.c

--- include/secret_base.h
+++ include/secret_base.h
@@ -10,5 +10,7 @@ void sub_80E9578(void);
 void sub_80E980C(void);
 u8 *GetSecretBaseMapName(u8 *dest);
 const u8 *GetSecretBaseTrainerLoseText(void);
+void sub_80E8EE0(struct MapEvents const *events);
+void sub_80E9238(u8 flagIn);
 
 #endif //GUARD_SECRET_BASE_H
