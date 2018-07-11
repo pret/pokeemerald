7d375972ba0e5a8a17fecbd223dd6de6e87b6090
Marcus Huderle <huderlem@gmail.com>
2018-07-11 11:12:26 -0500

    Sync bg_event macros with pokeruby

--- data/map_events.s
+++ data/map_events.s
@@ -1,9 +1,12 @@
+#include "constants/bg_event_constants.h"
 #include "constants/event_object_movement_constants.h"
 #include "constants/event_objects.h"
 #include "constants/flags.h"
 #include "constants/items.h"
 #include "constants/maps.h"
+#include "constants/secret_bases.h"
 #include "constants/vars.h"
+#include "constants/weather.h"
 	.include "asm/macros.inc"
 	.include "constants/constants.inc"
 
