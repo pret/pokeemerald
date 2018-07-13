d53e8137cd1e14c7fd5435562a979bb6bd83d20e
Marcus Huderle <huderlem@gmail.com>
2018-07-12 22:15:35 -0500

    Use weather constants in map headers

--- data/maps.s
+++ data/maps.s
@@ -1,6 +1,7 @@
 #include "constants/maps.h"
 #include "constants/region_map_sections.h"
 #include "constants/songs.h"
+#include "constants/weather.h"
 	.include "asm/macros.inc"
 	.include "constants/constants.inc"
 
