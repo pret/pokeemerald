aceb85e11e76005691519e897df9c2f023b11ba6
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-23 23:16:58 -0400

    Match mus_ayasii.mid

--- songs.mk
+++ songs.mk
@@ -12,5 +12,8 @@ $(MID_SUBDIR)/mus_aqa_0.s: %.s: %.mid
 $(MID_SUBDIR)/mus_ashroad.s: %.s: %.mid
 	$(MID) $< $@ -G055 -R$(STD_REVERB)
 
+$(MID_SUBDIR)/mus_ayasii.s: %.s: %.mid
+	$(MID) $< $@ -G069 -R$(STD_REVERB) -V078
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
