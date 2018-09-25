51805c02afa18fd4116c3c0101284fdf2f2cd7ab
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-25 16:32:48 -0400

    Match mus_battle27.mid

--- songs.mk
+++ songs.mk
@@ -42,6 +42,9 @@ $(MID_SUBDIR)/mus_b_tube.s: %.s: %.mid
 $(MID_SUBDIR)/mus_battle20.s: %.s: %.mid
 	$(MID) $< $@ -G119 -R$(STD_REVERB) -P1
 
+$(MID_SUBDIR)/mus_battle27.s: %.s: %.mid
+	$(MID) $< $@ -G117 -R$(STD_REVERB) -P1
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
