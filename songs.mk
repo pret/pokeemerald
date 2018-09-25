1c4da06bfaf337f50bea2a585f36c01c8a1587a3
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-25 16:59:56 -0400

    Match mus_bd_time.mid

--- songs.mk
+++ songs.mk
@@ -69,6 +69,9 @@ $(MID_SUBDIR)/mus_battle36.s: %.s: %.mid
 $(MID_SUBDIR)/mus_battle38.s: %.s: %.mid
 	$(MID) $< $@ -G125 -R$(STD_REVERB) -P1
 
+$(MID_SUBDIR)/mus_bd_time.s: %.s: %.mid
+	$(MID) $< $@ -G038 -R$(STD_REVERB)
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
