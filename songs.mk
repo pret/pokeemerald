1768c542dbe7a0262fa4ffeaaead5a691497ffab
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-25 17:14:25 -0400

    Match mus_c_road.mid

--- songs.mk
+++ songs.mk
@@ -81,6 +81,9 @@ $(MID_SUBDIR)/mus_boy_sup.s: %.s: %.mid
 $(MID_SUBDIR)/mus_boyeye.s: %.s: %.mid
 	$(MID) $< $@ -G028 -R$(STD_REVERB)
 
+$(MID_SUBDIR)/mus_c_road.s: %.s: %.mid
+	$(MID) $< $@ -G075 -R$(STD_REVERB)
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
