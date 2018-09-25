d396871781e9423126ea6eaae9418eeeff5d5159
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-25 14:32:56 -0400

    Match mus_b_tube.mid

--- songs.mk
+++ songs.mk
@@ -36,6 +36,9 @@ $(MID_SUBDIR)/mus_b_palace.s: %.s: %.mid
 $(MID_SUBDIR)/mus_b_tower.s: %.s: %.mid
 	$(MID) $< $@ -G035 -R$(STD_REVERB)
 
+$(MID_SUBDIR)/mus_b_tube.s: %.s: %.mid
+	$(MID) $< $@ -G112 -R$(STD_REVERB)
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
