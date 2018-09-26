c597ef4f735133cea7cdbb6617581e3d0b967a67
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-26 18:46:33 -0400

    Match mus_fanfa5.mid

--- songs.mk
+++ songs.mk
@@ -153,6 +153,9 @@ $(MID_SUBDIR)/mus_fanfa1.s: %.s: %.mid
 $(MID_SUBDIR)/mus_fanfa4.s: %.s: %.mid
 	$(MID) $< $@ -G012 -R$(STD_REVERB) -P5
 
+$(MID_SUBDIR)/mus_fanfa5.s: %.s: %.mid
+	$(MID) $< $@ -G012 -R$(STD_REVERB) -P5
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
