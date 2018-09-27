26b8b367177818ff389a64e20537e6c8eda3aa80
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-26 20:22:23 -0400

    Match mus_kachi4.mid

--- songs.mk
+++ songs.mk
@@ -207,6 +207,9 @@ $(MID_SUBDIR)/mus_kachi1.s: %.s: %.mid
 $(MID_SUBDIR)/mus_kachi2.s: %.s: %.mid
 	$(MID) $< $@ -G025 -R$(STD_REVERB)
 
+$(MID_SUBDIR)/mus_kachi4.s: %.s: %.mid
+	$(MID) $< $@ -G070 -R$(STD_REVERB)
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
