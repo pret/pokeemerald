585f11d0253c2dc41ed573300115c5e9fa9c89f9
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-28 22:43:35 -0400

    Match mus_me_wasure.mid

--- songs.mk
+++ songs.mk
@@ -279,6 +279,9 @@ $(MID_SUBDIR)/mus_me_tama.s: %.s: %.mid
 $(MID_SUBDIR)/mus_me_tore_eye.s: %.s: %.mid
 	$(MID) $< $@ -G105 -R$(STD_REVERB) -V090 -P5
 
+$(MID_SUBDIR)/mus_me_wasure.s: %.s: %.mid
+	$(MID) $< $@ -G012 -R$(STD_REVERB) -V090 -P5
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
