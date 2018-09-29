1903d37f1c28e4bb825774768ebd6ddc28aeddf8
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-09-28 22:21:33 -0400

    Match mus_me_symbolget.mid

--- songs.mk
+++ songs.mk
@@ -270,6 +270,9 @@ $(MID_SUBDIR)/mus_me_pointget.s: %.s: %.mid
 $(MID_SUBDIR)/mus_me_shinka.s: %.s: %.mid
 	$(MID) $< $@ -G026 -R$(STD_REVERB) -V080
 
+$(MID_SUBDIR)/mus_me_symbolget.s: %.s: %.mid
+	$(MID) $< $@ -G103 -R$(STD_REVERB) -V100 -P5
+
 $(MID_SUBDIR)/mus_naminori.s: %.s: %.mid
 	$(MID) $< $@ -G017 -R$(STD_REVERB)
 
