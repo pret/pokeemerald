602ca92b3b26278eae1f5553d1a46f44750067bb
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-01 16:05:57 -0400

    Match mus_rg_cycling.mid

--- songs.mk
+++ songs.mk
@@ -345,6 +345,9 @@ $(MID_SUBDIR)/mus_rg_annai.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_champ_r.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G154 -V090
 
+$(MID_SUBDIR)/mus_rg_cycling.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G141 -V090
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
