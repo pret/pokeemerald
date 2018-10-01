e38a3e532f834726e7fbea0a43447f1ec0a54e86
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-01 15:58:31 -0400

    Match mus_rg_annai.mid

--- songs.mk
+++ songs.mk
@@ -339,6 +339,9 @@ $(MID_SUBDIR)/mus_rekkuu_kourin.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_ajito.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G133 -V090
 
+$(MID_SUBDIR)/mus_rg_annai.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G131 -V068
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
