865398e7010716de870e17a7ec5c2812b39e39e3
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-02 01:19:11 -0400

    Match mus_rg_kuchiba.mid

--- songs.mk
+++ songs.mk
@@ -396,6 +396,9 @@ $(MID_SUBDIR)/mus_rg_kenkyu.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_kinomikui.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G132 -V090
 
+$(MID_SUBDIR)/mus_rg_kuchiba.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G172 -V090
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
