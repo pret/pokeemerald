0922e3b2052fb275d85cace99d53b1aee05c2bea
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-02 14:42:51 -0400

    Match mus_rg_nana123.mid

--- songs.mk
+++ songs.mk
@@ -420,6 +420,9 @@ $(MID_SUBDIR)/mus_rg_nana45.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_nana67.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G189 -V084
 
+$(MID_SUBDIR)/mus_rg_nana123.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G173 -V084
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
