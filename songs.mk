b1aa0c5b7886f1fd10ef2a6661a39b75d20945d7
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-02 21:42:10 -0400

    Match mus_rg_t_mori.mid

--- songs.mk
+++ songs.mk
@@ -507,6 +507,9 @@ $(MID_SUBDIR)/mus_rg_slowmasara.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_sousa.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G182 -V085
 
+$(MID_SUBDIR)/mus_rg_t_mori.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G146 -V090
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
