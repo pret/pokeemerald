2066705756ecce10da4c2e03014317de895f1234
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-02 23:51:42 -0400

    Match mus_rg_tamamusi.mid

--- songs.mk
+++ songs.mk
@@ -513,6 +513,9 @@ $(MID_SUBDIR)/mus_rg_t_mori.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_t_tower.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G134 -V090
 
+$(MID_SUBDIR)/mus_rg_tamamusi.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G168 -V070
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
