2a3cd99fed8703a5134a83272301db5b8a8049ed
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-02 21:29:13 -0400

    Match mus_rg_shounen.mid

--- songs.mk
+++ songs.mk
@@ -495,6 +495,9 @@ $(MID_SUBDIR)/mus_rg_shiruhu.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_shoujo.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G143 -V051
 
+$(MID_SUBDIR)/mus_rg_shounen.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G144 -V090
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
