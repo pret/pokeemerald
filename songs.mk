adcd05f1764ee6ff2cfc3015475101a46469e6f1
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-02 21:16:29 -0400

    Match mus_rg_seibetu.mid

--- songs.mk
+++ songs.mk
@@ -480,6 +480,9 @@ $(MID_SUBDIR)/mus_rg_rocket.s: %.s: %.mid
 $(MID_SUBDIR)/mus_rg_santoan.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G163 -V090
 
+$(MID_SUBDIR)/mus_rg_seibetu.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G182 -V088
+
 $(MID_SUBDIR)/mus_ropeway.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G071 -V078
 
