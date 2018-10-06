76a9cf0d54ff8163fbe5598d04b131407377df87
Deokishisu <edsaletto@gmail.com>
2018-10-06 01:56:03 -0400

    Fix captitalization of HGSS in comment and match FRLG formatting in enum

--- include/global.h
+++ include/global.h
@@ -89,7 +89,7 @@ extern u8 gStringVar4[];
 // Invalid Versions show as "----------" in Gen 4 and Gen 5's summary screen.
 // In Gen 4 only, migrated Pokemon with Diamond, Pearl, or Platinum's ID show as "----------".
 // Gen 5 and up read Diamond, Pearl, or Platinum's ID as "Sinnoh".
-// In Gen 4, migrated Pokemon with Heartgold or Soulsilver's ID show the unused "Johto" string.
+// In Gen 4, migrated Pokemon with HeartGold or SoulSilver's ID show the unused "Johto" string.
 enum
 {
     VERSION_INVALID_0 = 0,
@@ -99,8 +99,8 @@ enum
     VERSION_FIRE_RED = 4,
     VERSION_LEAF_GREEN = 5,
     VERSION_INVALID_6 = 6,
-    VERSION_HEARTGOLD = 7,
-    VERSION_SOULSILVER = 8,
+    VERSION_HEART_GOLD = 7,
+    VERSION_SOUL_SILVER = 8,
     VERSION_INVALID_9 = 9,
     VERSION_DIAMOND = 10,
     VERSION_PEARL = 11,
