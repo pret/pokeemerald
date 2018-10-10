94e491357e1b01969e8f711dd5a588f2e5958065
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 15:04:57 -0400

    Match se_curtain.mid

--- songs.mk
+++ songs.mk
@@ -831,5 +831,8 @@ $(MID_SUBDIR)/se_c_syu.s: %.s: %.mid
 $(MID_SUBDIR)/se_card.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V100 -P4
 
+$(MID_SUBDIR)/se_curtain.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G129 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
