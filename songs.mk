3b929049daf1ab24be43d45b62764115087fddae
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 15:17:05 -0400

    Match se_daugi.mid

--- songs.mk
+++ songs.mk
@@ -840,5 +840,8 @@ $(MID_SUBDIR)/se_curtain1.s: %.s: %.mid
 $(MID_SUBDIR)/se_dansa.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V100 -P4
 
+$(MID_SUBDIR)/se_daugi.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V090 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
