c98f5c25d674227b8d73273dab2b7980224f9d44
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 14:56:12 -0400

    Match se_c_pikon.mid

--- songs.mk
+++ songs.mk
@@ -822,5 +822,8 @@ $(MID_SUBDIR)/se_c_maku_u.s: %.s: %.mid
 $(MID_SUBDIR)/se_c_pasi.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P5
 
+$(MID_SUBDIR)/se_c_pikon.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V090 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
