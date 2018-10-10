acb7b7eb5c0dbd6337e508749d578a07f36cf389
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 14:50:48 -0400

    Match se_c_maku_u.mid

--- songs.mk
+++ songs.mk
@@ -816,5 +816,8 @@ $(MID_SUBDIR)/se_c_gaji.s: %.s: %.mid
 $(MID_SUBDIR)/se_c_maku_d.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V070 -P5
 
+$(MID_SUBDIR)/se_c_maku_u.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V070 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
