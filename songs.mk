e66fb45f242b110ebdec8a38d66a160a40f1446b
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 15:42:53 -0400

    Match se_expmax.mid

--- songs.mk
+++ songs.mk
@@ -864,5 +864,8 @@ $(MID_SUBDIR)/se_esuka.s: %.s: %.mid
 $(MID_SUBDIR)/se_exp.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V080 -P5
 
+$(MID_SUBDIR)/se_expmax.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V094 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
