2f10c5c24c306ea9bf1c212190375f9f05b170db
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 16:20:54 -0400

    Match se_jite_pyoko.mid

--- songs.mk
+++ songs.mk
@@ -912,5 +912,8 @@ $(MID_SUBDIR)/se_jido_doa.s: %.s: %.mid
 $(MID_SUBDIR)/se_jihanki.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
 
+$(MID_SUBDIR)/se_jite_pyoko.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V090 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
