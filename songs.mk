ab5c24daa6a5190f9caf470ea6043c0a35c92ce6
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 16:11:23 -0400

    Match se_hinsi.mid

--- songs.mk
+++ songs.mk
@@ -900,5 +900,8 @@ $(MID_SUBDIR)/se_hazure.s: %.s: %.mid
 $(MID_SUBDIR)/se_hi_turun.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V090 -P4
 
+$(MID_SUBDIR)/se_hinsi.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V100 -P3
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
