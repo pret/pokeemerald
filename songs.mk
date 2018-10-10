1d241702f225b5be32dcba80cefcefa617320985
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 15:30:17 -0400

    Match se_elebeta.mid

--- songs.mk
+++ songs.mk
@@ -855,5 +855,8 @@ $(MID_SUBDIR)/se_door.s: %.s: %.mid
 $(MID_SUBDIR)/se_e.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V120 -P4
 
+$(MID_SUBDIR)/se_elebeta.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V100 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
