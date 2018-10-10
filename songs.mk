a9bdd34f9ea12868176375d535cd88d7f21e7668
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 16:39:11 -0400

    Match se_kon.mid

--- songs.mk
+++ songs.mk
@@ -930,5 +930,8 @@ $(MID_SUBDIR)/se_kaifuku.s: %.s: %.mid
 $(MID_SUBDIR)/se_ki_gasyan.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V100 -P4
 
+$(MID_SUBDIR)/se_kon.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V100 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
