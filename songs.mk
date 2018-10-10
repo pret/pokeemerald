b9989eaa562fab3e6c5da7c693417135497f440f
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-10 14:53:47 -0400

    Match se_c_pasi.mid

--- songs.mk
+++ songs.mk
@@ -819,5 +819,8 @@ $(MID_SUBDIR)/se_c_maku_d.s: %.s: %.mid
 $(MID_SUBDIR)/se_c_maku_u.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V070 -P5
 
+$(MID_SUBDIR)/se_c_pasi.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
