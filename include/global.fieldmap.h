dd3689fdbf98b0a60190bf10e3bed3158692818e
Diegoisawesome <diego@domoreaweso.me>
2018-10-19 00:05:54 -0500

    Finish porting/decompiling field_player_avatar

--- include/global.fieldmap.h
+++ include/global.fieldmap.h
@@ -232,7 +232,7 @@ struct EventObjectGraphicsInfo
 #define PLAYER_AVATAR_FLAG_MACH_BIKE (1 << 1)
 #define PLAYER_AVATAR_FLAG_ACRO_BIKE (1 << 2)
 #define PLAYER_AVATAR_FLAG_SURFING   (1 << 3)
-#define PLAYER_AVATAR_FLAG_4         (1 << 4)
+#define PLAYER_AVATAR_FLAG_UNDERWATER (1 << 4)
 #define PLAYER_AVATAR_FLAG_5         (1 << 5)
 #define PLAYER_AVATAR_FLAG_6         (1 << 6)
 #define PLAYER_AVATAR_FLAG_DASH      (1 << 7)
