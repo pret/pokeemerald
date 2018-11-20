80ea58a198603820e887572341bb56b3b1ba0c4e
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-11-20 17:44:09 -0500

    Match se_ru_gashin.mid

--- songs.mk
+++ songs.mk
@@ -1056,5 +1056,8 @@ $(MID_SUBDIR)/se_rg_w_door.s: %.s: %.mid
 $(MID_SUBDIR)/se_ru_bari.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V100 -P4
 
+$(MID_SUBDIR)/se_ru_gashin.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V090 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
