9c46276911cae9b39ff3ba414b416d0e5ad2c690
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-11-20 15:13:22 -0500

    Match se_nageru.mid

--- songs.mk
+++ songs.mk
@@ -963,5 +963,8 @@ $(MID_SUBDIR)/se_mu_pachi.s: %.s: %.mid
 $(MID_SUBDIR)/se_n.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -P4
 
+$(MID_SUBDIR)/se_nageru.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V120 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
