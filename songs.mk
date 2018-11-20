a00079e63b31815dc86cc31120f5c0b19dcd2f66
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-11-20 17:55:18 -0500

    Match se_suikomu.mid

--- songs.mk
+++ songs.mk
@@ -1074,5 +1074,8 @@ $(MID_SUBDIR)/se_seikai.s: %.s: %.mid
 $(MID_SUBDIR)/se_select.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V080 -P5
 
+$(MID_SUBDIR)/se_suikomu.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V100 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
