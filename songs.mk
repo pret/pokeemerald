83c843474a8a890b2e379657d081067cc0be910e
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-11-20 17:40:34 -0500

    Match se_rg_w_door.mid

--- songs.mk
+++ songs.mk
@@ -1050,5 +1050,8 @@ $(MID_SUBDIR)/se_rg_nawamiss.s: %.s: %.mid
 $(MID_SUBDIR)/se_rg_shop.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G129 -V080 -P5
 
+$(MID_SUBDIR)/se_rg_w_door.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G129 -V100 -P5
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
