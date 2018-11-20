27d46fa3a8542615de455bcc9068480ed1b5a44d
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-11-20 18:10:25 -0500

    Match se_t_ooame_e.mid

--- songs.mk
+++ songs.mk
@@ -1098,5 +1098,8 @@ $(MID_SUBDIR)/se_t_koame_e.s: %.s: %.mid
 $(MID_SUBDIR)/se_t_ooame.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V100 -P2
 
+$(MID_SUBDIR)/se_t_ooame_e.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V100 -P2
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
