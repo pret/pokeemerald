9d93817319777fb07ddab67000451a305f208220
LOuroboros <lunosouroboros@gmail.com>
2018-11-21 18:27:35 -0300

    Update battle_anim_scripts.s

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -12303,31 +12303,31 @@ General_WishHeal:
 	end
 
 General_MegaEvolution:
-        loadspritegfx ANIM_TAG_ORBS
-        loadspritegfx ANIM_TAG_CIRCLE_OF_LIGHT
-        loadspritegfx ANIM_TAG_METEOR
-        loadspritegfx ANIM_TAG_FLAT_ROCK
-        monbg ANIM_ATK_PARTNER
-        setalpha 12, 8
-        createvisualtask sub_8115A04, 2, 2, 1, 4, 0, 11, RGB(31, 31, 11)
-        playsewithpan SE_W025, -64
-        call SolarBeamSetUp1
-        waitforvisualfinish
-        createsprite gUnknown_08595FEC, 130, 0
-        playsewithpan SE_W025, -64
-        delay 15
-        createsprite gBattleAnimSpriteTemplate_85972D8, 2, 4, 1, 180, 1
-        createvisualtask sub_8159244, 5, 234, 0
-        delay 20
-        createvisualtask sub_815B7D0, 2, 0
-        delay 4
-        createvisualtask sub_8117494, 50
-        waitforvisualfinish
-        createvisualtask sub_8117494, 2
-        playsewithpan SE_W043, -64
-        blendoff
-        clearmonbg ANIM_ATTACKER
-        end
+    loadspritegfx ANIM_TAG_ORBS
+    loadspritegfx ANIM_TAG_CIRCLE_OF_LIGHT
+    loadspritegfx ANIM_TAG_METEOR
+    loadspritegfx ANIM_TAG_FLAT_ROCK
+    monbg ANIM_ATK_PARTNER
+    setalpha 12, 8
+    createvisualtask sub_8115A04, 2, 2, 1, 4, 0, 11, RGB(31, 31, 11)
+    playsewithpan SE_W025, -64
+    call SolarBeamSetUp1
+    waitforvisualfinish
+    createsprite gUnknown_08595FEC, ANIM_TARGET, 2, 0
+    playsewithpan SE_W025, -64
+    delay 15
+    createsprite gBattleAnimSpriteTemplate_85972D8, ANIM_ATTACKER, 2, 4, 1, 180, 1
+    createvisualtask sub_8159244, 5, 234, 0
+    delay 20
+    createvisualtask sub_815B7D0, 2, 0
+    delay 4
+    createvisualtask sub_8117494, 50
+    waitforvisualfinish
+    createvisualtask sub_8117494, 2
+    playsewithpan SE_W043, -64
+    blendoff
+    clearmonbg ANIM_ATTACKER
+    end
 
 AnimScript_82D85A3:
 	createvisualtask sub_8172E9C, 2
