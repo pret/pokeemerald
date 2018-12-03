2779421ad479312eba5a0fc5bb993473e83465ee
Melody <melody@pallet.town>
2018-12-03 08:01:52 -0500

    fix brendan/may/wally consistency issues

--- src/pokemon.c
+++ src/pokemon.c
@@ -7113,9 +7113,9 @@ u16 FacilityClassToPicIndex(u16 facilityClass)
 u16 PlayerGenderToFrontTrainerPicId(u8 playerGender)
 {
     if (playerGender != MALE)
-        return FacilityClassToPicIndex(FACILITY_CLASS_PKMN_TRAINER_MAY);
+        return FacilityClassToPicIndex(FACILITY_CLASS_MAY);
     else
-        return FacilityClassToPicIndex(FACILITY_CLASS_PKMN_TRAINER_BRENDAN);
+        return FacilityClassToPicIndex(FACILITY_CLASS_BRENDAN);
 }
 
 void HandleSetPokedexFlag(u16 nationalNum, u8 caseId, u32 personality)
