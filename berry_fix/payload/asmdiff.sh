25cef5327f490b277968ec50dc014c0da6c04be9
PikalaxALT <pikalaxalt@gmail.com>
2018-12-17 09:53:16 -0500

    Remove unneded scripts

--- berry_fix/payload/asmdiff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OBJDUMP="$DEVKITARM/bin/arm-none-eabi-objdump -D -bbinary -marmv4t -Mforce-thumb"
-OPTIONS="--start-address=$(($1)) --stop-address=$(($1 + $2))"
-$OBJDUMP $OPTIONS baserom.gba > baserom.dump
-$OBJDUMP $OPTIONS payload.gba > payload.dump
-diff baserom.dump payload.dump
