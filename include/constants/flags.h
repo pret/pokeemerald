97ebeef1dbe8b4954d234fda067651f63d7e6360
Tetrable <atifhuss386@gmail.com>
2019-01-03 18:13:16 +0000

    Relabel TEMP_FLAG_20 into FLAG_UNUSED_0x020

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -36,8 +36,8 @@
 #define FLAG_TEMP_1D    0x1D
 #define FLAG_TEMP_1E    0x1E
 #define FLAG_TEMP_1F    0x1F
-#define FLAG_TEMP_20    0x20 // Unused Flag
 
+#define FLAG_UNUSED_0x020    0x20 // Unused Flag
 #define FLAG_UNUSED_0x021    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
