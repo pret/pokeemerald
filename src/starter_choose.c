2997966a128b97256e1290f9bcca5b35594ce231
tipsypastels <dakotasankey@gmail.com>
2019-01-26 00:49:14 -0800

    setup

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -125,9 +125,9 @@ static const u8 gStarterChoose_LabelCoords[][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_ZIGZAGOON,
+    SPECIES_BULBASAUR,
+    SPECIES_PSYDUCK,
 };
 
 static const struct BgTemplate gUnknown_085B1E00[3] =
--- tools/gbagfx/rl.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016 YamaArashi
-
-#ifndef RL_H
-#define RL_H
-
-unsigned char *RLDecompress(unsigned char *src, int srcSize, int *uncompressedSize);
-unsigned char *RLCompress(unsigned char *src, int srcSize, int *compressedSize);
-
-#endif // RL_H
