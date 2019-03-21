768a68d3e8d09b0f244047c2b75cf47a3a713ff9
Marcus Huderle <huderlem@gmail.com>
2019-03-21 18:15:47 -0500

    Decompile ereader.c up to faraway island code

---
+++ include/ereader.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_EREADER_H
+#define GUARD_EREADER_H
+
+bool32 sub_81D3B34(void);
+u32 sub_81D427C(void);
+bool8 sub_81D4A58(struct EventObject*);
+bool8 EventObjectIsFarawayIslandMew(struct EventObject *);
+
+#endif // GUARD_EREADER_H
