7118462b9f58e4b9756c41fe1d464be70e9ac2b4
Marcus Huderle <huderlem@gmail.com>
2019-03-21 18:15:47 -0500

    Decompile ereader.c up to faraway island code

--- include/pokenav.h
+++ include/pokenav.h
@@ -3,9 +3,6 @@
 #include "list_menu.h"
 
 bool8 sub_81D312C(void *arg0);
-bool8 sub_81D4A58(struct EventObject*);
-bool8 EventObjectIsFarawayIslandMew(struct EventObject *);
-u32 sub_81D427C(void);
 void sub_81D4998(void);
 void CB2_InitPokeNav(void);
 
