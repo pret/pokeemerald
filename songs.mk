c0e40ec311dbc3655ba2250e8c6dc6fe2a31ef6d
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-24 16:19:00 -0400

    Match se_toy_c.mid

--- songs.mk
+++ songs.mk
@@ -1143,5 +1143,8 @@ $(MID_SUBDIR)/se_toy_a.s: %.s: %.mid
 $(MID_SUBDIR)/se_toy_b.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
 
+$(MID_SUBDIR)/se_toy_c.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
