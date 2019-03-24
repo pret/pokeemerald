1d08ff3a60d55fb2a91720e8e625c241c70e8596
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-24 16:30:05 -0400

    Match se_track_haiki.mid

--- songs.mk
+++ songs.mk
@@ -1170,5 +1170,8 @@ $(MID_SUBDIR)/se_toy_kabe.s: %.s: %.mid
 $(MID_SUBDIR)/se_track_door.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
 
+$(MID_SUBDIR)/se_track_haiki.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
