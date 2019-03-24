94a15d4182c1d5f0d85b131f3db6df79aea84a4c
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-24 16:59:08 -0400

    Match se_w016.mid

--- songs.mk
+++ songs.mk
@@ -1218,5 +1218,8 @@ $(MID_SUBDIR)/se_w014.s: %.s: %.mid
 $(MID_SUBDIR)/se_w015.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V120 -P4
 
+$(MID_SUBDIR)/se_w016.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
