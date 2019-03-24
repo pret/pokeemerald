7d9cda59ba7346874c570172f62aeea6c45a1416
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-24 16:28:26 -0400

    Match se_track_door.mid

--- songs.mk
+++ songs.mk
@@ -1167,5 +1167,8 @@ $(MID_SUBDIR)/se_toy_g.s: %.s: %.mid
 $(MID_SUBDIR)/se_toy_kabe.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
 
+$(MID_SUBDIR)/se_track_door.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
