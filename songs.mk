e0df5a49fd823fa7792d3bc37e8bce430fd20ec4
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-24 16:34:00 -0400

    Match se_tu_saa.mid

--- songs.mk
+++ songs.mk
@@ -1179,5 +1179,8 @@ $(MID_SUBDIR)/se_track_move.s: %.s: %.mid
 $(MID_SUBDIR)/se_track_stop.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -P4
 
+$(MID_SUBDIR)/se_tu_saa.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -V090 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
