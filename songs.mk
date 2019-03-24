e2779ad0e1502a7daf19e90f5963d268b491ac88
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-24 16:27:06 -0400

    Match se_toy_kabe.mid

--- songs.mk
+++ songs.mk
@@ -1164,5 +1164,8 @@ $(MID_SUBDIR)/se_toy_f.s: %.s: %.mid
 $(MID_SUBDIR)/se_toy_g.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
 
+$(MID_SUBDIR)/se_toy_kabe.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G128 -V110 -P4
+
 $(MID_SUBDIR)/se_z_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -P5
