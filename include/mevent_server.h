03badd9c40de5d50562376d39348fb5c85f8340b
PikalaxALT <pikalaxalt@gmail.com>
2019-04-02 11:43:29 -0400

    mevent_server_ish

--- include/mevent_server.h
+++ include/mevent_server.h
@@ -1,53 +1,7 @@
 #ifndef GUARD_MEVENT_SERVER_H
 #define GUARD_MEVENT_SERVER_H
 
-#define ME_SEND_BUF_SIZE 0x400
-
-struct mevent_srv_sub
-{
-    s32 seqno;
-    u8 sendPlayerNo;
-    u8 recvPlayerNo;
-    u16 recvIdent;
-    u16 recvCounter;
-    u16 recvCRC;
-    u16 recvSize;
-    u16 sendIdent;
-    u16 sendCounter;
-    u16 sendCRC;
-    u16 sendSize;
-    void * recvBfr;
-    const void * sendBfr;
-    u32 (*recvFunc)(struct mevent_srv_sub *);
-    u32 (*sendFunc)(struct mevent_srv_sub *);
-};
-
-struct send_recv_header
-{
-    u16 ident;
-    u16 crc;
-    u16 size;
-};
-
-struct mevent_cmd_ish
-{
-    u32 instr;
-    u32 parameter;
-};
-
-struct mevent_srv_ish
-{
-    u32 unk_00;
-    u32 param;
-    u32 mainseqno;
-    u32 flag;
-    u32 cmdidx;
-    void * sendBuffer;
-    void * recvBuffer;
-    struct mevent_cmd_ish * cmdBuffer;
-    void * buffer;
-    struct mevent_srv_sub manager;
-};
+#include "mevent_server_helpers.h"
 
 struct mevent_cmd
 {
