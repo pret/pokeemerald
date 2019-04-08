2b740b46e8254446b2598b4ab13aeb752e060cc0
Phlosioneer <mattmdrr2@gmail.com>
2019-04-08 17:48:36 -0400

    About halfway done with pokenav_unk_1.s

--- include/pokenav.h
+++ include/pokenav.h
@@ -11,6 +11,8 @@ typedef u32 (*LoopedTask)(s32 state);
 #define LT_FINISH 4
 #define LT_SET_STATE(newState) (newState + 5)
 
+#define UNKNOWN_POKENAV_OFFSET 100000
+
 // pokenav.c
 extern u32 sub_81C99D4(void);
 
@@ -22,10 +24,20 @@ void FreePokenavSubstruct(u32 index);
 void *AllocSubstruct(u32 index, u32 size);
 void Pokenav_AllocAndLoadPalettes(const struct SpritePalette *palettes);
 bool32 IsLoopedTaskActive(u32 taskId);
+void SetPokenavMode(u16 mode);
+u32 GetPokenavMode(void);
+bool32 CanViewRibbonsMenu(void);
 
 // pokenav_match_call_ui.c
 
 // pokenav_unk_1.c
 u32 sub_81C9298(void);
+u32 sub_81C92CC(void);
+u32 sub_81C9304(void);
+u32 sub_81C9338(void);
+u32 sub_81C9368(void);
+u32 sub_81C941C(void);
+void sub_81C9430(void);
+
 
 #endif //GUARD_POKENAV_H
