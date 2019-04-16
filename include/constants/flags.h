1dda48ef8b569afedff7d936908b576b78c16050
Fontbane <fontbane@gmail.com>
2019-04-16 10:40:32 -0400

    Doc most OnTransitions

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1248,7 +1248,7 @@
 #define FLAG_UNUSED_0x4F9                                           0x4F9 // Unused Flag
 #define FLAG_UNUSED_0x4FA                                           0x4FA // Unused Flag
 
-#define FLAG_DEFEATED_ELITE_4_SYDNEY                                0x4FB
+#define FLAG_DEFEATED_ELITE_4_SIDNEY                                0x4FB
 #define FLAG_DEFEATED_ELITE_4_PHOEBE                                0x4FC
 #define FLAG_DEFEATED_ELITE_4_GLACIA                                0x4FD
 #define FLAG_DEFEATED_ELITE_4_DRAKE                                 0x4FE
