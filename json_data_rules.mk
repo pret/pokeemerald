db9acebfe9a4bd17d00450069c549f1f6d12a410
Marcus Huderle <huderlem@gmail.com>
2019-04-28 12:12:26 -0500

    Convert wild encounter data to JSON

---
+++ json_data_rules.mk
@@ -0,0 +1,6 @@
+# JSON files are run through jsonproc, which is a tool that converts JSON data to an output file
+# based on an Inja template. https://github.com/pantor/inja
+
+AUTO_GEN_TARGETS += $(DATA_SRC_SUBDIR)/wild_encounters.h
+$(DATA_SRC_SUBDIR)/wild_encounters.h: $(DATA_SRC_SUBDIR)/wild_encounters.json $(DATA_SRC_SUBDIR)/wild_encounters.json.txt
+	$(JSONPROC) $^ $@
