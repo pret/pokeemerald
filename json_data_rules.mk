adb4935583b0c840cef886ad60968f5f831b0c32
PikalaxALT <pikalaxalt@gmail.com>
2019-05-02 10:21:58 -0400

    Fix building

--- json_data_rules.mk
+++ json_data_rules.mk
@@ -5,4 +5,4 @@ AUTO_GEN_TARGETS += $(DATA_SRC_SUBDIR)/wild_encounters.h
 $(DATA_SRC_SUBDIR)/wild_encounters.h: $(DATA_SRC_SUBDIR)/wild_encounters.json $(DATA_SRC_SUBDIR)/wild_encounters.json.txt
 	$(JSONPROC) $^ $@
 
-$(C_BUILDDIR)/wild_encounters.o: c_dep += $(DATA_SRC_SUBDIR)/wild_encounters.h
+$(C_BUILDDIR)/wild_encounter.o: c_dep += $(DATA_SRC_SUBDIR)/wild_encounters.h
