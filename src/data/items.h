c0470c3932d94adbb3894c50b184b95c306e84e2
DizzyEggg <jajkodizzy@wp.pl>
2019-05-03 17:31:23 +0200

    Remove skarmory mega evo

--- src/data/items.h
+++ src/data/items.h
@@ -177,7 +177,6 @@ const struct Item gItems[] =
         .name = _("POTION"),
         .itemId = ITEM_POTION,
         .price = 300,
-        .holdEffect = HOLD_EFFECT_MEGA_STONE,
         .holdEffectParam = 20,
         .description = sPotionDesc,
         .pocket = POCKET_ITEMS,
