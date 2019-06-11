ca41ea10279973ecc9529be73fc96ecd69a1d641
garak <garakmon@gmail.com>
2019-06-11 18:02:06 -0400

    give metatile labels to petalburg gym and pcs

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -1,42 +1,51 @@
 #ifndef GUARD_METATILE_LABELS_H
 #define GUARD_METATILE_LABELS_H
 
+// gTileset_Building
+#define METATILE_Building_PC_Off 0x004
+#define METATILE_Building_PC_On  0x005
+
 // gTileset_MauvilleGym
-#define METATILE_MauvilleGym_RaisedSwitch           0x205
-#define METATILE_MauvilleGym_PressedSwitch          0x206
+#define METATILE_MauvilleGym_RaisedSwitch    0x205
+#define METATILE_MauvilleGym_PressedSwitch   0x206
 #define METATILE_MauvilleGym_FloorTile       0x21A
-
-#define METATILE_MauvilleGym_GreenBeamH1_On     0x220
-#define METATILE_MauvilleGym_GreenBeamH2_On    0x221
-#define METATILE_MauvilleGym_GreenBeamH3_On 0x228
-#define METATILE_MauvilleGym_GreenBeamH4_On 0x229
-
-#define METATILE_MauvilleGym_GreenBeamH1_Off     0x230
-#define METATILE_MauvilleGym_GreenBeamH2_Off    0x231
+#define METATILE_MauvilleGym_GreenBeamH1_On  0x220
+#define METATILE_MauvilleGym_GreenBeamH2_On  0x221
+#define METATILE_MauvilleGym_GreenBeamH3_On  0x228
+#define METATILE_MauvilleGym_GreenBeamH4_On  0x229
+#define METATILE_MauvilleGym_GreenBeamH1_Off 0x230
+#define METATILE_MauvilleGym_GreenBeamH2_Off 0x231
 #define METATILE_MauvilleGym_GreenBeamH3_Off 0x238
 #define METATILE_MauvilleGym_GreenBeamH4_Off 0x239
-
-#define METATILE_MauvilleGym_RedBeamH1_On     0x222
+#define METATILE_MauvilleGym_RedBeamH1_On    0x222
 #define METATILE_MauvilleGym_RedBeamH2_On    0x223
-#define METATILE_MauvilleGym_RedBeamH3_On 0x22A
-#define METATILE_MauvilleGym_RedBeamH4_On 0x22B
+#define METATILE_MauvilleGym_RedBeamH3_On    0x22A
+#define METATILE_MauvilleGym_RedBeamH4_On    0x22B
+#define METATILE_MauvilleGym_RedBeamH1_Off   0x232
+#define METATILE_MauvilleGym_RedBeamH2_Off   0x233
+#define METATILE_MauvilleGym_RedBeamH3_Off   0x23A
+#define METATILE_MauvilleGym_RedBeamH4_Off   0x23B
+#define METATILE_MauvilleGym_GreenBeamV1_On  0x240
+#define METATILE_MauvilleGym_GreenBeamV2_On  0x248
+#define METATILE_MauvilleGym_RedBeamV1_On    0x241
+#define METATILE_MauvilleGym_RedBeamV2_On    0x249
+#define METATILE_MauvilleGym_PoleTop_On      0x250
+#define METATILE_MauvilleGym_PoleTop_Off     0x251
+#define METATILE_MauvilleGym_PoleBottom_On   0x242
+#define METATILE_MauvilleGym_PoleBottom_Off  0x243
 
-#define METATILE_MauvilleGym_RedBeamH1_Off     0x232
-#define METATILE_MauvilleGym_RedBeamH2_Off    0x233
-#define METATILE_MauvilleGym_RedBeamH3_Off 0x23A
-#define METATILE_MauvilleGym_RedBeamH4_Off 0x23B
+// gTileset_PetalburgGym
+#define METATILE_PetalburgGym_SlidingDoor_Frame0 0x218
+#define METATILE_PetalburgGym_SlidingDoor_Frame1 0x219
+#define METATILE_PetalburgGym_SlidingDoor_Frame2 0x21A
+#define METATILE_PetalburgGym_SlidingDoor_Frame3 0x21B
+#define METATILE_PetalburgGym_SlidingDoor_Frame4 0x21C
 
-#define METATILE_MauvilleGym_GreenBeamV1_On 0x240
-#define METATILE_MauvilleGym_GreenBeamV2_On 0x248
-
-#define METATILE_MauvilleGym_RedBeamV1_On 0x241
-#define METATILE_MauvilleGym_RedBeamV2_On 0x249
-
-#define METATILE_MauvilleGym_PoleTop_On 0x250
-#define METATILE_MauvilleGym_PoleTop_Off 0x251
-
-#define METATILE_MauvilleGym_PoleBottom_On 0x242
-#define METATILE_MauvilleGym_PoleBottom_Off 0x243
+// gTileset_BrendansMaysHouse
+#define METATILE_BrendansMaysHouse_BrendanPC_Off 0x25A
+#define METATILE_BrendansMaysHouse_BrendanPC_On  0x27F
+#define METATILE_BrendansMaysHouse_MayPC_Off     0x259
+#define METATILE_BrendansMaysHouse_MayPC_On      0x27E
 
 
 
