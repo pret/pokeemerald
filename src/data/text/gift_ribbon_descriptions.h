dfa3e079c0f55565fc60a6a1ba0ef04de9be94c2
garak <garakmon@gmail.com>
2019-07-19 22:57:33 -0400

    finish pokenav data decomp

---
+++ src/data/text/gift_ribbon_descriptions.h
@@ -0,0 +1,115 @@
+const u8 gGiftRibbonDescriptionPart1_2003RegionalTourney[] = _("2003 REGIONAL TOURNEY");
+const u8 gGiftRibbonDescriptionPart2_Champion[] = _("CHAMPION RIBBON");
+const u8 gGiftRibbonDescriptionPart1_2003NationalTourney[] = _("2003 NATIONAL TOURNEY");
+const u8 gGiftRibbonDescriptionPart1_2003GlobalCup[] = _("2003 GLOBAL CUP");
+const u8 gGiftRibbonDescriptionPart2_RunnerUp[] = _("Runner-up RIBBON");
+const u8 gGiftRibbonDescriptionPart2_Semifinalist[] = _("Semifinalist RIBBON");
+const u8 gGiftRibbonDescriptionPart1_2004RegionalTourney[] = _("2004 REGIONAL TOURNEY");
+const u8 gGiftRibbonDescriptionPart1_2004NationalTourney[] = _("2004 NATIONAL TOURNEY");
+const u8 gGiftRibbonDescriptionPart1_2004GlobalCup[] = _("2004 GLOBAL CUP");
+const u8 gGiftRibbonDescriptionPart1_2005RegionalTourney[] = _("2005 REGIONAL TOURNEY");
+const u8 gGiftRibbonDescriptionPart1_2005NationalTourney[] = _("2005 NATIONAL TOURNEY");
+const u8 gGiftRibbonDescriptionPart1_2005GlobalCup[] = _("2005 GLOBAL CUP");
+const u8 gGiftRibbonDescriptionPart1_PokemonBattleCup[] = _("POKéMON BATTLE CUP");
+const u8 gGiftRibbonDescriptionPart2_Participation[] = _("Participation RIBBON");
+const u8 gGiftRibbonDescriptionPart1_PokemonLeague[] = _("POKéMON LEAGUE");
+const u8 gGiftRibbonDescriptionPart1_AdvanceCup[] = _("ADVANCE CUP");
+const u8 gGiftRibbonDescriptionPart1_PokemonTournament[] = _("POKéMON Tournament");
+const u8 gGiftRibbonDescriptionPart2_Participation2[] = _("Participation RIBBON");
+const u8 gGiftRibbonDescriptionPart1_PokemonEvent[] = _("POKéMON Event");
+const u8 gGiftRibbonDescriptionPart1_PokemonFestival[] = _("POKéMON Festival");
+const u8 gGiftRibbonDescriptionPart1_DifficultyClearing[] = _("Difficulty-clearing");
+const u8 gGiftRibbonDescriptionPart2_Commemorative[] = _("Commemorative RIBBON");
+const u8 gGiftRibbonDescriptionPart1_ClearingAllChallenges[] = _("RIBBON awarded for");
+const u8 gGiftRibbonDescriptionPart2_ClearingAllChallenges[] = _("clearing all challenges.");
+const u8 gGiftRibbonDescriptionPart1_100StraightWin[] = _("100-straight Win");
+const u8 gGiftRibbonDescriptionPart1_DarknessTower[] = _("DARKNESS TOWER Clear");
+const u8 gGiftRibbonDescriptionPart1_RedTower[] = _("RED TOWER Clear");
+const u8 gGiftRibbonDescriptionPart1_BlackironTower[] = _("BLACKIRON TOWER Clear");
+const u8 gGiftRibbonDescriptionPart1_FinalTower[] = _("FINAL TOWER Clear");
+const u8 gGiftRibbonDescriptionPart1_LegendMaking[] = _("Legend-making");
+const u8 gGiftRibbonDescriptionPart1_PokemonCenterTokyo[] = _("POKéMON CENTER TOKYO");
+const u8 gGiftRibbonDescriptionPart1_PokemonCenterOsaka[] = _("POKéMON CENTER OSAKA");
+const u8 gGiftRibbonDescriptionPart1_PokemonCenterNagoya[] = _("POKéMON CENTER NAGOYA");
+const u8 gGiftRibbonDescriptionPart1_PokemonCenterNY[] = _("POKéMON CENTER NY");
+const u8 gGiftRibbonDescriptionPart1_SummerHolidays[] = _("Summer Holidays RIBBON");
+const u8 gGiftRibbonDescriptionPart2_EmptyString[] = _("");
+const u8 gGiftRibbonDescriptionPart1_WinterHolidays[] = _("Winter Holidays RIBBON");
+const u8 gGiftRibbonDescriptionPart1_SpringHolidays[] = _("Spring Holidays RIBBON");
+const u8 gGiftRibbonDescriptionPart1_Evergreen[] = _("Evergreen RIBBON");
+const u8 gGiftRibbonDescriptionPart1_SpecialHoliday[] = _("Special Holiday RIBBON");
+const u8 gGiftRibbonDescriptionPart1_HardWorker[] = _("Hard Worker RIBBON");
+const u8 gGiftRibbonDescriptionPart1_LotsOfFriends[] = _("Lots of Friends RIBBON");
+const u8 gGiftRibbonDescriptionPart1_FullOfEnergy[] = _("Full of Energy RIBBON");
+const u8 gGiftRibbonDescriptionPart1_LovedPokemon[] = _("A commemorative RIBBON");
+const u8 gGiftRibbonDescriptionPart2_LovedPokemon[] = _("for a loved POKéMON.");
+const u8 gGiftRibbonDescriptionPart1_LoveForPokemon[] = _("RIBBON that shows");
+const u8 gGiftRibbonDescriptionPart2_LoveForPokemon[] = _("love for POKéMON.");
+
+const u8 *const gGiftRibbonDescriptionPointers[][2] = 
+{
+    {gGiftRibbonDescriptionPart1_2003RegionalTourney,   gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2003NationalTourney,   gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2003GlobalCup,         gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2003RegionalTourney,   gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2003NationalTourney,   gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2003GlobalCup,         gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2003RegionalTourney,   gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2003NationalTourney,   gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2003GlobalCup,         gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2004RegionalTourney,   gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2004NationalTourney,   gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2004GlobalCup,         gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2004RegionalTourney,   gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2004NationalTourney,   gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2004GlobalCup,         gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2004RegionalTourney,   gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2004NationalTourney,   gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2004GlobalCup,         gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2005RegionalTourney,   gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2005NationalTourney,   gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2005GlobalCup,         gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_2005RegionalTourney,   gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2005NationalTourney,   gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2005GlobalCup,         gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_2005RegionalTourney,   gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2005NationalTourney,   gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_2005GlobalCup,         gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_PokemonBattleCup,      gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_PokemonBattleCup,      gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_PokemonBattleCup,      gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_PokemonBattleCup,      gGiftRibbonDescriptionPart2_Participation},
+    {gGiftRibbonDescriptionPart1_PokemonLeague,         gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_PokemonLeague,         gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_PokemonLeague,         gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_PokemonLeague,         gGiftRibbonDescriptionPart2_Participation},
+    {gGiftRibbonDescriptionPart1_AdvanceCup,            gGiftRibbonDescriptionPart2_Champion},
+    {gGiftRibbonDescriptionPart1_AdvanceCup,            gGiftRibbonDescriptionPart2_RunnerUp},
+    {gGiftRibbonDescriptionPart1_AdvanceCup,            gGiftRibbonDescriptionPart2_Semifinalist},
+    {gGiftRibbonDescriptionPart1_AdvanceCup,            gGiftRibbonDescriptionPart2_Participation},
+    {gGiftRibbonDescriptionPart1_PokemonTournament,     gGiftRibbonDescriptionPart2_Participation2},
+    {gGiftRibbonDescriptionPart1_PokemonEvent,          gGiftRibbonDescriptionPart2_Participation2},
+    {gGiftRibbonDescriptionPart1_PokemonFestival,       gGiftRibbonDescriptionPart2_Participation2},
+    {gGiftRibbonDescriptionPart1_DifficultyClearing,    gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_ClearingAllChallenges, gGiftRibbonDescriptionPart2_ClearingAllChallenges},
+    {gGiftRibbonDescriptionPart1_100StraightWin,        gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_DarknessTower,         gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_RedTower,              gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_BlackironTower,        gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_FinalTower,            gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_LegendMaking,          gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_PokemonCenterTokyo,    gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_PokemonCenterOsaka,    gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_PokemonCenterNagoya,   gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_PokemonCenterNY,       gGiftRibbonDescriptionPart2_Commemorative},
+    {gGiftRibbonDescriptionPart1_SummerHolidays,        gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_WinterHolidays,        gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_SpringHolidays,        gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_Evergreen,             gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_SpecialHoliday,        gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_HardWorker,            gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_LotsOfFriends,         gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_FullOfEnergy,          gGiftRibbonDescriptionPart2_EmptyString},
+    {gGiftRibbonDescriptionPart1_LovedPokemon,          gGiftRibbonDescriptionPart2_LovedPokemon},
+    {gGiftRibbonDescriptionPart1_LoveForPokemon,        gGiftRibbonDescriptionPart2_LoveForPokemon}
+};
---
+++ src/data/text/ribbon_descriptions.h
@@ -0,0 +1,48 @@
+const u8 gRibbonDescriptionPart1_Champion[] = _("CHAMPION-beating, HALL");
+const u8 gRibbonDescriptionPart2_Champion[] = _("OF FAME Member RIBBON");
+const u8 gRibbonDescriptionPart1_CoolContest[] = _("COOL CONTEST");
+const u8 gRibbonDescriptionPart1_BeautyContest[] = _("BEAUTY CONTEST");
+const u8 gRibbonDescriptionPart1_CuteContest[] = _("CUTE CONTEST");
+const u8 gRibbonDescriptionPart1_SmartContest[] = _("SMART CONTEST");
+const u8 gRibbonDescriptionPart1_ToughContest[] = _("TOUGH CONTEST");
+const u8 gRibbonDescriptionPart2_NormalRank[] = _("Normal Rank winner!");
+const u8 gRibbonDescriptionPart2_SuperRank[] = _("Super Rank winner!");
+const u8 gRibbonDescriptionPart2_HyperRank[] = _("Hyper Rank winner!");
+const u8 gRibbonDescriptionPart2_MasterRank[] = _("Master Rank winner!");
+const u8 gRibbonDescriptionPart1_Winning[] = _("For clearing LV50");
+const u8 gRibbonDescriptionPart2_Winning[] = _("at the BATTLE TOWER.");
+const u8 gRibbonDescriptionPart1_Victory[] = _("For clearing Open Level");
+const u8 gRibbonDescriptionPart2_Victory[] = _("at the BATTLE TOWER.");
+const u8 gRibbonDescriptionPart1_Artist[] = _("RIBBON for being chosen");
+const u8 gRibbonDescriptionPart2_Artist[] = _("as a super sketch model.");
+const u8 gRibbonDescriptionPart1_Effort[] = _("RIBBON awarded for");
+const u8 gRibbonDescriptionPart2_Effort[] = _("being a hard worker.");
+
+const u8 *const gRibbonDescriptionPointers[][2] = 
+{
+    {gRibbonDescriptionPart1_Champion,      gRibbonDescriptionPart2_Champion},
+    {gRibbonDescriptionPart1_CoolContest,   gRibbonDescriptionPart2_NormalRank},
+    {gRibbonDescriptionPart1_CoolContest,   gRibbonDescriptionPart2_SuperRank},
+    {gRibbonDescriptionPart1_CoolContest,   gRibbonDescriptionPart2_HyperRank},
+    {gRibbonDescriptionPart1_CoolContest,   gRibbonDescriptionPart2_MasterRank},
+    {gRibbonDescriptionPart1_BeautyContest, gRibbonDescriptionPart2_NormalRank},
+    {gRibbonDescriptionPart1_BeautyContest, gRibbonDescriptionPart2_SuperRank},
+    {gRibbonDescriptionPart1_BeautyContest, gRibbonDescriptionPart2_HyperRank},
+    {gRibbonDescriptionPart1_BeautyContest, gRibbonDescriptionPart2_MasterRank},
+    {gRibbonDescriptionPart1_CuteContest,   gRibbonDescriptionPart2_NormalRank},
+    {gRibbonDescriptionPart1_CuteContest,   gRibbonDescriptionPart2_SuperRank},
+    {gRibbonDescriptionPart1_CuteContest,   gRibbonDescriptionPart2_HyperRank},
+    {gRibbonDescriptionPart1_CuteContest,   gRibbonDescriptionPart2_MasterRank},
+    {gRibbonDescriptionPart1_SmartContest,  gRibbonDescriptionPart2_NormalRank},
+    {gRibbonDescriptionPart1_SmartContest,  gRibbonDescriptionPart2_SuperRank},
+    {gRibbonDescriptionPart1_SmartContest,  gRibbonDescriptionPart2_HyperRank},
+    {gRibbonDescriptionPart1_SmartContest,  gRibbonDescriptionPart2_MasterRank},
+    {gRibbonDescriptionPart1_ToughContest,  gRibbonDescriptionPart2_NormalRank},
+    {gRibbonDescriptionPart1_ToughContest,  gRibbonDescriptionPart2_SuperRank},
+    {gRibbonDescriptionPart1_ToughContest,  gRibbonDescriptionPart2_HyperRank},
+    {gRibbonDescriptionPart1_ToughContest,  gRibbonDescriptionPart2_MasterRank},
+    {gRibbonDescriptionPart1_Winning,       gRibbonDescriptionPart2_Winning},
+    {gRibbonDescriptionPart1_Victory,       gRibbonDescriptionPart2_Victory},
+    {gRibbonDescriptionPart1_Artist,        gRibbonDescriptionPart2_Artist},
+    {gRibbonDescriptionPart1_Effort,        gRibbonDescriptionPart2_Effort},
+};
