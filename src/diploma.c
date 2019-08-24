d3ea25ee7f40c3334a7acdefec8afd65ed7ec56c
ultima-soul <akshayjhanji@hotmail.com>
2019-08-24 11:17:11 -0700

    Synchronize with pokefirered pokedex.c.

--- src/diploma.c
+++ src/diploma.c
@@ -127,7 +127,7 @@ static void Task_DiplomaFadeOut(u8 taskId)
 
 static void DisplayDiplomaText(void)
 {
-    if (sub_80C0944())
+    if (HasAllMons())
     {
         SetGpuReg(REG_OFFSET_BG1HOFS, DISPCNT_BG0_ON);
         StringCopy(gStringVar1, gText_DexNational);
