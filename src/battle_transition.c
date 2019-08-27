405384de8913c9fec97891986d0ee5f41e32c2b8
jiangzhengwenjz <jiangzhengwenjzw@qq.com>
2019-08-28 06:00:34 +0800

    Phase2_Ripple_Func2

--- src/battle_transition.c
+++ src/battle_transition.c
@@ -1961,9 +1961,10 @@ static bool8 Phase2_Ripple_Func2(struct Task *task)
 
     for (i = 0; i < 160; i++, r4 += r8)
     {
-        // todo: fix the asm
         s16 var = r4 >> 8;
-        asm("");
+        
+		++var;
+		--var;
         gScanlineEffectRegBuffers[0][i] = sTransitionStructPtr->field_16 + Sin(var, r3);
     }
 
