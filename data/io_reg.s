cb21f922e7ea987e08b2080c5f2b6c47cd52f6a8
GriffinR <griffin.richards@comcast.net>
2019-11-01 18:43:22 -0400

    Use constants in io_reg.s

--- data/io_reg.s
+++ data/io_reg.s
@@ -25,13 +25,13 @@ gUnref_82EC784:: @ 82EC784
 	.4byte 0x08000400
 
 gOverworldBackgroundLayerFlags:: @ 82EC7C4
-	.2byte 0x0100 /* BLDCNT_TGT2_BG0 */
-	.2byte 0x0200 /* BLDCNT_TGT2_BG1 */
-	.2byte 0x0400 /* BLDCNT_TGT2_BG2 */
-	.2byte 0x0800 /* BLDCNT_TGT2_BG3 */
+	.2byte BLDCNT_TGT2_BG0
+	.2byte BLDCNT_TGT2_BG1
+	.2byte BLDCNT_TGT2_BG2
+	.2byte BLDCNT_TGT2_BG3
 
 gOrbEffectBackgroundLayerFlags:: @ 82EC7CC
-	.2byte 0x0001 /* BLDCNT_TGT1_BG0 */
-	.2byte 0x0002 /* BLDCNT_TGT1_BG1 */
-	.2byte 0x0004 /* BLDCNT_TGT1_BG2 */
-	.2byte 0x0008 /* BLDCNT_TGT1_BG3 */
+	.2byte BLDCNT_TGT1_BG0
+	.2byte BLDCNT_TGT1_BG1
+	.2byte BLDCNT_TGT1_BG2
+	.2byte BLDCNT_TGT1_BG3
