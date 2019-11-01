390d594e21067be8c24447ca5e51689327b8ce04
GriffinR <griffin.richards@comcast.net>
2019-11-01 17:56:35 -0400

    Fix indentation

--- data/io_reg.s
+++ data/io_reg.s
@@ -1,4 +1,4 @@
-    .include "asm/macros.inc"
+	.include "asm/macros.inc"
 	.include "constants/constants.inc"
 
 	.section .rodata
@@ -7,22 +7,22 @@
 
 .align 2
 gUnref_82EC784:: @ 82EC784
-    .4byte 0x00000000
-    .4byte 0x00000000
-    .4byte 0x04000008
-    .4byte 0x0400000A
-    .4byte 0x0400000C
-    .4byte 0x0400000E
-    .4byte 0x04000010
-    .4byte 0x04000014
-    .4byte 0x04000018
-    .4byte 0x0400001C
-    .4byte 0x04000012
-    .4byte 0x04000016
-    .4byte 0x0400001A
-    .4byte 0x0400001E
-    .4byte 0x02000100
-    .4byte 0x08000400
+	.4byte 0x00000000
+	.4byte 0x00000000
+	.4byte 0x04000008
+	.4byte 0x0400000A
+	.4byte 0x0400000C
+	.4byte 0x0400000E
+	.4byte 0x04000010
+	.4byte 0x04000014
+	.4byte 0x04000018
+	.4byte 0x0400001C
+	.4byte 0x04000012
+	.4byte 0x04000016
+	.4byte 0x0400001A
+	.4byte 0x0400001E
+	.4byte 0x02000100
+	.4byte 0x08000400
 
 gOverworldBackgroundLayerFlags:: @ 82EC7C4
 	.2byte 0x0100 /* BLDCNT_TGT2_BG0 */
