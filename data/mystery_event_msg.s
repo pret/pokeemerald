58dcd6d99dd7456006fe834e5fbab6b120698280
GriffinR <griffin.richards@comcast.net>
2019-11-05 15:08:07 -0500

    Clean up mystery event msg

--- data/mystery_event_msg.s
+++ data/mystery_event_msg.s
@@ -1,6 +1,8 @@
+#include "constants/global.h"
 #include "constants/flags.h"
 #include "constants/items.h"
 #include "constants/map_scripts.h"
+#include "constants/mevent.h"
 #include "constants/moves.h"
 #include "constants/region_map_sections.h"
 #include "constants/songs.h"
@@ -16,10 +18,10 @@
 
 MysteryEventScript_StampCard:: @ 8674CB0
 	setvaddress MysteryEventScript_StampCard
-	setorcopyvar VAR_RESULT, 1
-	specialvar VAR_0x8008, sub_813986C
-	setorcopyvar VAR_RESULT, 0
-	specialvar VAR_0x8009, sub_813986C
+	setorcopyvar VAR_RESULT, GET_MAX_STAMPS
+	specialvar VAR_0x8008, GetMysteryEventCardVal
+	setorcopyvar VAR_RESULT, GET_NUM_STAMPS
+	specialvar VAR_0x8009, GetMysteryEventCardVal
 	subvar VAR_0x8008, VAR_0x8009
 	buffernumberstring 0, VAR_0x8008
 	lock
@@ -31,18 +33,21 @@ MysteryEventScript_StampCard:: @ 8674CB0
 	end
 
 sText_MysteryGiftStampCard:
-	.string "Thank you for using the STAMP CARD\nSystem.\pYou have {STR_VAR_1} more to collect to\nfill your STAMP CARD.$"
+	.string "Thank you for using the STAMP CARD\n"
+	.string "System.\p"
+	.string "You have {STR_VAR_1} more to collect to\n"
+	.string "fill your STAMP CARD.$"
 
 MysteryEventScript_SurfPichu:: @ 8674D3D
 	setvaddress MysteryEventScript_SurfPichu
 	checkflag FLAG_MYSTERY_EVENT_DONE
-	vgoto_if 0, SurfPichu_GiveIfPossible
+	vgoto_if_ne SurfPichu_GiveIfPossible
 	returnram
 
 SurfPichu_GiveIfPossible: @ 8674D4C
 	specialvar VAR_EVENT_PICHU_SLOT, CalculatePlayerPartyCount
-	compare_var_to_value VAR_EVENT_PICHU_SLOT, 6
-	vgoto_if 1, SurfPichu_FullParty
+	compare_var_to_value VAR_EVENT_PICHU_SLOT, PARTY_SIZE
+	vgoto_if_eq SurfPichu_FullParty
 	setflag FLAG_MYSTERY_EVENT_DONE
 	vcall SurfPichu_GiveEgg
 	lock
@@ -69,15 +74,15 @@ SurfPichu_GiveEgg: @ 8674D7E
 	setmonobedient VAR_EVENT_PICHU_SLOT
 	setmonmetlocation VAR_EVENT_PICHU_SLOT, METLOC_FATEFUL_ENCOUNTER
 	compare_var_to_value VAR_EVENT_PICHU_SLOT, 1
-	vgoto_if 1, SurfPichu_Slot1
+	vgoto_if_eq SurfPichu_Slot1
 	compare_var_to_value VAR_EVENT_PICHU_SLOT, 2
-	vgoto_if 1, SurfPichu_Slot2
+	vgoto_if_eq SurfPichu_Slot2
 	compare_var_to_value VAR_EVENT_PICHU_SLOT, 3
-	vgoto_if 1, SurfPichu_Slot3
+	vgoto_if_eq SurfPichu_Slot3
 	compare_var_to_value VAR_EVENT_PICHU_SLOT, 4
-	vgoto_if 1, SurfPichu_Slot4
+	vgoto_if_eq SurfPichu_Slot4
 	compare_var_to_value VAR_EVENT_PICHU_SLOT, 5
-	vgoto_if 1, SurfPichu_Slot5
+	vgoto_if_eq SurfPichu_Slot5
 	return
 
 SurfPichu_Slot1: @ 8674DC0
@@ -101,19 +106,26 @@ SurfPichu_Slot5: @ 8674DD8
 	return
 
 sText_MysteryGiftEgg:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pFrom the POKéMON CENTER we\nhave a gift--a POKéMON EGG!\pPlease raise it with love and\nkindness.$"
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "From the POKéMON CENTER we\n"
+	.string "have a gift--a POKéMON EGG!\p"
+	.string "Please raise it with love and\n"
+	.string "kindness.$"
 
 sText_FullParty:
-	.string "Oh, your party appears to be full.\pPlease come see me after storing\na POKéMON on a PC.$"
+	.string "Oh, your party appears to be full.\p"
+	.string "Please come see me after storing\n"
+	.string "a POKéMON on a PC.$"
 
 MysteryEventScript_VisitingTrainer:: @ 8674EC1
 	setvaddress MysteryEventScript_VisitingTrainer
 	special ValidateEReaderTrainer
 	compare_var_to_value VAR_RESULT, 0
-	vgoto_if 1, MysteryEventScript_VisitingTrainerArrived
+	vgoto_if_eq MysteryEventScript_VisitingTrainerArrived
 	lock
 	faceplayer
-	vmessage sText_MysteryGiftVisitingTrainer
+	vmessage sText_MysteryGiftVisitingTrainerInstructions
 	waitmessage
 	waitbuttonpress
 	release
@@ -122,29 +134,51 @@ MysteryEventScript_VisitingTrainer:: @ 8674EC1
 MysteryEventScript_VisitingTrainerArrived: @ 8674EDF
 	lock
 	faceplayer
-	vmessage sText_MysteryGiftVisitingTrainer_2
+	vmessage sText_MysteryGiftVisitingTrainerArrived
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
-sText_MysteryGiftVisitingTrainer:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pBy holding this WONDER CARD, you\nmay take part in a survey at a\lPOKéMON MART.\pUse these surveys to invite\nTRAINERS to SOOTOPOLIS CITY.\p…Let me give you a secret\npassword for a survey:\p“GIVE ME\nAWESOME TRAINER”\pWrite that in on a survey and send\nit to the WIRELESS\lCOMMUNICATION SYSTEM.$"
+sText_MysteryGiftVisitingTrainerInstructions:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "By holding this WONDER CARD, you\n"
+	.string "may take part in a survey at a\l"
+	.string "POKéMON MART.\p"
+	.string "Use these surveys to invite\n"
+	.string "TRAINERS to SOOTOPOLIS CITY.\p"
+	.string "…Let me give you a secret\n"
+	.string "password for a survey:\p"
+	.string "“GIVE ME\n"
+	.string "AWESOME TRAINER”\p"
+	.string "Write that in on a survey and send\n"
+	.string "it to the WIRELESS\l"
+	.string "COMMUNICATION SYSTEM.$"
 
-sText_MysteryGiftVisitingTrainer_2:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pA TRAINER has arrived in\nSOOTOPOLIS CITY looking for you.\pWe hope you will enjoy\nbattling the visiting TRAINER.\pYou may invite other TRAINERS by\nentering other passwords.\pTry looking for other passwords\nthat may work.$"
+sText_MysteryGiftVisitingTrainerArrived:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "A TRAINER has arrived in\n"
+	.string "SOOTOPOLIS CITY looking for you.\p"
+	.string "We hope you will enjoy\n"
+	.string "battling the visiting TRAINER.\p"
+	.string "You may invite other TRAINERS by\n"
+	.string "entering other passwords.\p"
+	.string "Try looking for other passwords\n"
+	.string "that may work.$"
 
 MysteryEventScript_BattleCard:: @ 867513C
 	setvaddress MysteryEventScript_BattleCard
 	checkflag FLAG_MYSTERY_EVENT_DONE
-	vgoto_if 1, MysteryEventScript_BattleCardInfo
-	setorcopyvar VAR_RESULT, 2
-	specialvar VAR_0x8008, sub_813986C
-	compare_var_to_value VAR_0x8008, 3
-	vgoto_if 0, MysteryEventScript_BattleCardInfo
+	vgoto_if_eq MysteryEventScript_BattleCardInfo
+	setorcopyvar VAR_RESULT, GET_CARD_BATTLES_WON
+	specialvar VAR_0x8008, GetMysteryEventCardVal
+	compare_var_to_value VAR_0x8008, REQUIRED_CARD_BATTLES
+	vgoto_if_ne MysteryEventScript_BattleCardInfo
 	lock
 	faceplayer
-	vmessage sText_MysteryGiftBattleCountCard_2
+	vmessage sText_MysteryGiftBattleCountCard_WonPrize
 	waitmessage
 	waitbuttonpress
 	giveitem_std ITEM_POTION
@@ -162,122 +196,157 @@ MysteryEventScript_BattleCardInfo: @ 8675179
 	end
 
 sText_MysteryGiftBattleCountCard:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pYour BATTLE COUNT CARD keeps\ntrack of your battle record against\lTRAINERS with the same CARD.\pLook for and battle TRAINERS who\nhave the same CARD as you.\pYou may check the overall rankings\nby reading the NEWS.\pPlease do give it a try!$"
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "Your BATTLE COUNT CARD keeps\n"
+	.string "track of your battle record against\l"
+	.string "TRAINERS with the same CARD.\p"
+	.string "Look for and battle TRAINERS who\n"
+	.string "have the same CARD as you.\p"
+	.string "You may check the overall rankings\n"
+	.string "by reading the NEWS.\p"
+	.string "Please do give it a try!$"
 
-sText_MysteryGiftBattleCountCard_2:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pCongratulations!\pYou have won a prize for winning\nthree battles!\pWe hope you will be inspired to\nbattle some more.$"
+sText_MysteryGiftBattleCountCard_WonPrize:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "Congratulations!\p"
+	.string "You have won a prize for winning\n"
+	.string "three battles!\p"
+	.string "We hope you will be inspired to\n"
+	.string "battle some more.$"
 
 MysteryEventScript_AuroraTicket:: @ 867533C
 	setvaddress MysteryEventScript_AuroraTicket
 	lock
 	faceplayer
 	checkflag FLAG_RECEIVED_AURORA_TICKET
-	vgoto_if 1, AuroraTicket_Obtained
+	vgoto_if_eq AuroraTicket_Obtained
 	checkflag FLAG_BATTLED_DEOXYS
-	vgoto_if 1, AuroraTicket_Obtained
+	vgoto_if_eq AuroraTicket_Obtained
 	checkitem ITEM_AURORA_TICKET, 1
-	compare_var_to_value VAR_RESULT, 1
-	vgoto_if 1, AuroraTicket_Obtained
-	vmessage sText_AuroraTicket1
+	compare_var_to_value VAR_RESULT, TRUE
+	vgoto_if_eq AuroraTicket_Obtained
+	vmessage sText_AuroraTicketForYou
 	waitmessage
 	waitbuttonpress
 	checkitemspace ITEM_AURORA_TICKET, 1
-	compare_var_to_value VAR_RESULT, 0
-	vgoto_if 1, AuroraTicket_NoBagSpace
+	compare_var_to_value VAR_RESULT, FALSE
+	vgoto_if_eq AuroraTicket_NoBagSpace
 	giveitem_std ITEM_AURORA_TICKET
 	setflag FLAG_ENABLE_SHIP_BIRTH_ISLAND
 	setflag FLAG_RECEIVED_AURORA_TICKET
-	vmessage sText_AuroraTicket2
+	vmessage sText_AuroraTicketUseAtPort
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
 AuroraTicket_NoBagSpace: @ 8675397
-	vmessage sText_AuroraTicketNoPlace
+	vmessage sText_AuroraTicketBagFull
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
 AuroraTicket_Obtained: @ 86753A0
-	vmessage sText_AuroraTicketGot
+	vmessage sText_AuroraTicketThankYou
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
-sText_AuroraTicket1:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pYou must be {PLAYER}.\nThere is a ticket here for you.$"
+sText_AuroraTicketForYou:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "You must be {PLAYER}.\n"
+	.string "There is a ticket here for you.$"
 
-sText_AuroraTicket2:
-	.string "It appears to be for use at the\nLILYCOVE CITY port.\pWhy not give it a try and see what\nit is about?$"
+sText_AuroraTicketUseAtPort:
+	.string "It appears to be for use at the\n"
+	.string "LILYCOVE CITY port.\p"
+	.string "Why not give it a try and see what\n"
+	.string "it is about?$"
 
-sText_AuroraTicketGot:
-	.string "Thank you for using the MYSTERY\nGIFT System.$"
+sText_AuroraTicketThankYou:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.$"
 
-sText_AuroraTicketNoPlace:
-	.string "Oh, I'm sorry, {PLAYER}.\nYour BAG's KEY ITEMS POCKET is full.\pPlease store something on your PC,\nthen come back for this.$"
+sText_AuroraTicketBagFull:
+	.string "Oh, I'm sorry, {PLAYER}.\n"
+	.string "Your BAG's KEY ITEMS POCKET is full.\p"
+	.string "Please store something on your PC,\n"
+	.string "then come back for this.$"
 
 MysteryEventScript_MysticTicket:: @ 867550B
 	setvaddress MysteryEventScript_MysticTicket
 	lock
 	faceplayer
 	checkflag FLAG_RECEIVED_MYSTIC_TICKET
-	vgoto_if 1, MysticTicket_Obtained
+	vgoto_if_eq MysticTicket_Obtained
 	checkflag FLAG_CAUGHT_LUGIA
-	vgoto_if 1, MysticTicket_Obtained
+	vgoto_if_eq MysticTicket_Obtained
 	checkflag FLAG_CAUGHT_HO_OH
-	vgoto_if 1, MysticTicket_Obtained
+	vgoto_if_eq MysticTicket_Obtained
 	checkitem ITEM_MYSTIC_TICKET, 1
-	compare_var_to_value VAR_RESULT, 1
-	vgoto_if 1, MysticTicket_Obtained
-	vmessage sText_MysticTicket2
+	compare_var_to_value VAR_RESULT, TRUE
+	vgoto_if_eq MysticTicket_Obtained
+	vmessage sText_MysticTicketForYou
 	waitmessage
 	waitbuttonpress
 	checkitemspace ITEM_MYSTIC_TICKET, 1
-	compare_var_to_value VAR_RESULT, 0
-	vgoto_if 1, MysticTicket_NoBagSpace
+	compare_var_to_value VAR_RESULT, FALSE
+	vgoto_if_eq MysticTicket_NoBagSpace
 	giveitem_std ITEM_MYSTIC_TICKET
 	setflag FLAG_ENABLE_SHIP_NAVEL_ROCK
 	setflag FLAG_RECEIVED_MYSTIC_TICKET
-	vmessage sText_MysticTicket1
+	vmessage sText_MysticTicketUseAtPort
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
 MysticTicket_NoBagSpace: @ 867556F
-	vmessage sText_MysticTicketNoPlace
+	vmessage sText_MysticTicketBagFull
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
 MysticTicket_Obtained: @ 8675578
-	vmessage sText_MysticTicketGot
+	vmessage sText_MysticTicketThankYou
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
-sText_MysticTicket2:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pYou must be {PLAYER}.\nThere is a ticket here for you.$"
+sText_MysticTicketForYou:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "You must be {PLAYER}.\n"
+	.string "There is a ticket here for you.$"
 
-sText_MysticTicket1:
-	.string "It appears to be for use at the\nLILYCOVE CITY port.\pWhy not give it a try and see what\nit is about?$"
+sText_MysticTicketUseAtPort:
+	.string "It appears to be for use at the\n"
+	.string "LILYCOVE CITY port.\p"
+	.string "Why not give it a try and see what\n"
+	.string "it is about?$"
 
-sText_MysticTicketGot:
-	.string "Thank you for using the MYSTERY\nGIFT System.$"
+sText_MysticTicketThankYou:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.$"
 
-sText_MysticTicketNoPlace:
-	.string "Oh, I'm sorry, {PLAYER}.\nYour BAG's KEY ITEMS POCKET is full.\pPlease store something on your PC,\nthen come back for this.$"
+sText_MysticTicketBagFull:
+	.string "Oh, I'm sorry, {PLAYER}.\n"
+	.string "Your BAG's KEY ITEMS POCKET is full.\p"
+	.string "Please store something on your PC,\n"
+	.string "then come back for this.$"
 
 MysteryEventScript_AlteringCave:: @ 86756E3
 	setvaddress MysteryEventScript_AlteringCave
 	addvar VAR_ALTERING_CAVE_WILD_SET, 1
 	compare_var_to_value VAR_ALTERING_CAVE_WILD_SET, 10
-	vgoto_if 0, MysteryEventScript_AlteringCave_
+	vgoto_if_ne MysteryEventScript_AlteringCave_
 	setvar VAR_ALTERING_CAVE_WILD_SET, 0
 MysteryEventScript_AlteringCave_: @ 86756FD
 	lock
@@ -289,56 +358,74 @@ MysteryEventScript_AlteringCave_: @ 86756FD
 	end
 
 sText_MysteryGiftAlteringCave::
-	.string "Thank you for using the MYSTERY\nGIFT System.\pThere appears to be a rumor about\nrare POKéMON sightings.\pThe sightings reportedly came from\nthe ALTERING CAVE on ROUTE 103.\pPerhaps it would be worthwhile for\nyou to investigate this rumor.$"
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "There appears to be a rumor about\n"
+	.string "rare POKéMON sightings.\p"
+	.string "The sightings reportedly came from\n"
+	.string "the ALTERING CAVE on ROUTE 103.\p"
+	.string "Perhaps it would be worthwhile for\n"
+	.string "you to investigate this rumor.$"
 
 MysteryEventScript_OldSeaMap:: @ 86757F4
 	setvaddress MysteryEventScript_OldSeaMap
 	lock
 	faceplayer
 	checkflag FLAG_RECEIVED_OLD_SEA_MAP
-	vgoto_if 1, OldSeaMap_Obtained
+	vgoto_if_eq OldSeaMap_Obtained
 	checkflag FLAG_CAUGHT_MEW
-	vgoto_if 1, OldSeaMap_Obtained
+	vgoto_if_eq OldSeaMap_Obtained
 	checkitem ITEM_OLD_SEA_MAP, 1
-	compare_var_to_value VAR_RESULT, 1
-	vgoto_if 1, OldSeaMap_Obtained
-	vmessage sText_MysteryGiftOldSeaMap
+	compare_var_to_value VAR_RESULT, TRUE
+	vgoto_if_eq OldSeaMap_Obtained
+	vmessage sText_MysteryGiftOldSeaMapForYou
 	waitmessage
 	waitbuttonpress
 	checkitemspace ITEM_OLD_SEA_MAP, 1
-	compare_var_to_value VAR_RESULT, 0
-	vgoto_if 1, OldSeaMap_NoBagSpace
+	compare_var_to_value VAR_RESULT, FALSE
+	vgoto_if_eq OldSeaMap_NoBagSpace
 	giveitem_std ITEM_OLD_SEA_MAP
 	setflag FLAG_ENABLE_SHIP_FARAWAY_ISLAND
 	setflag FLAG_RECEIVED_OLD_SEA_MAP
-	vmessage sText_MysteryGiftOldSeaMap_1
+	vmessage sText_MysteryGiftOldSeaMapUseAtPort
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
 OldSeaMap_NoBagSpace: @ 867584F
-	vmessage sText_MysteryGiftOldSeaMap_3
+	vmessage sText_MysteryGiftOldSeaMapBagFull
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
 OldSeaMap_Obtained: @ 8675858
-	vmessage sText_MysteryGiftOldSeaMap_2
+	vmessage sText_MysteryGiftOldSeaMapThankYou
 	waitmessage
 	waitbuttonpress
 	release
 	end
 
-sText_MysteryGiftOldSeaMap:
-	.string "Thank you for using the MYSTERY\nGIFT System.\pLet me confirm--you are {PLAYER}?\pWe received this OLD SEA MAP\naddressed to you.$"
+sText_MysteryGiftOldSeaMapForYou:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.\p"
+	.string "Let me confirm--you are {PLAYER}?\p"
+	.string "We received this OLD SEA MAP\n"
+	.string "addressed to you.$"
 
-sText_MysteryGiftOldSeaMap_1:
-	.string "It appears to be for use at the\nLILYCOVE CITY port.\pWhy not give it a try and see what\nit is about?$"
+sText_MysteryGiftOldSeaMapUseAtPort:
+	.string "It appears to be for use at the\n"
+	.string "LILYCOVE CITY port.\p"
+	.string "Why not give it a try and see what\n"
+	.string "it is about?$"
 
-sText_MysteryGiftOldSeaMap_2:
-	.string "Thank you for using the MYSTERY\nGIFT System.$"
+sText_MysteryGiftOldSeaMapThankYou:
+	.string "Thank you for using the MYSTERY\n"
+	.string "GIFT System.$"
 
-sText_MysteryGiftOldSeaMap_3:
-	.string "Oh, I'm sorry, {PLAYER}.\nYour BAG's KEY ITEMS POCKET is full.\pPlease store something on your PC,\nthen come back for this.$"
+sText_MysteryGiftOldSeaMapBagFull:
+	.string "Oh, I'm sorry, {PLAYER}.\n"
+	.string "Your BAG's KEY ITEMS POCKET is full.\p"
+	.string "Please store something on your PC,\n"
+	.string "then come back for this.$"
