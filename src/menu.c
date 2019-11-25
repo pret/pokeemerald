8006d530372ec5ea70cd303446f4e54276561716
GriffinR <griffin.richards@comcast.net>
2019-11-24 19:11:36 -0500

    Name text speed delays

--- src/menu.c
+++ src/menu.c
@@ -62,7 +62,13 @@ static EWRAM_DATA u16 gUnknown_0203CDA8 = 0;
 static EWRAM_DATA void *gUnknown_0203CDAC[0x20] = {NULL};
 
 const u16 gUnknown_0860F074[] = INCBIN_U16("graphics/interface/860F074.gbapal");
-static const u8 gUnknown_0860F094[] = { 8, 4, 1 };
+
+static const u8 sTextSpeedFrameDelays[] = 
+{ 
+    [OPTIONS_TEXT_SPEED_SLOW] = 8, 
+    [OPTIONS_TEXT_SPEED_MID]  = 4, 
+    [OPTIONS_TEXT_SPEED_FAST] = 1 
+};
 
 static const struct WindowTemplate sStandardTextBox_WindowTemplates[] =
 {
@@ -480,7 +486,7 @@ u8 GetPlayerTextSpeedDelay(void)
     if (gSaveBlock2Ptr->optionsTextSpeed > OPTIONS_TEXT_SPEED_FAST)
         gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_MID;
     speed = GetPlayerTextSpeed();
-    return gUnknown_0860F094[speed];
+    return sTextSpeedFrameDelays[speed];
 }
 
 u8 sub_81979C4(u8 a1)
