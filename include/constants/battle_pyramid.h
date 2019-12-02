b34a3ab47baaa6b11771fe3c493de1b23f615c74
GriffinR <griffin.richards@comcast.net>
2019-12-02 09:52:16 -0500

    Set up Battle Pyramid macros

--- include/constants/battle_pyramid.h
+++ include/constants/battle_pyramid.h
@@ -27,23 +27,35 @@
 #define OBJ_POSITIONS_NEAR_EXIT             4
 
 // Functions IDs for sBattlePyramidFunctions / CallBattlePyramidFunction
-#define BATTLE_PYRAMID_FUNC_INIT_CHALLENGE      0
+#define BATTLE_PYRAMID_FUNC_INIT                0
 #define BATTLE_PYRAMID_FUNC_GET_DATA            1
 #define BATTLE_PYRAMID_FUNC_SET_DATA            2
-#define BATTLE_PYRAMID_FUNC_3                   3
-#define BATTLE_PYRAMID_FUNC_SET_REWARD          4
-#define BATTLE_PYRAMID_FUNC_GIVE_REWARD         5
+#define BATTLE_PYRAMID_FUNC_SAVE                3
+#define BATTLE_PYRAMID_FUNC_SET_PRIZE           4
+#define BATTLE_PYRAMID_FUNC_GIVE_PRIZE          5 // unused
 #define BATTLE_PYRAMID_FUNC_SEED_FLOOR          6
 #define BATTLE_PYRAMID_FUNC_SET_ITEM            7
 #define BATTLE_PYRAMID_FUNC_HIDE_ITEM           8
 #define BATTLE_PYRAMID_FUNC_INIT_TRAINERS       9
 #define BATTLE_PYRAMID_FUNC_SHOW_HINT_TEXT      10
-#define BATTLE_PYRAMID_FUNC_UPDATE_STREAK       11
+#define BATTLE_PYRAMID_FUNC_UPDATE_STREAK       11 // unused
 #define BATTLE_PYRAMID_FUNC_IS_IN               12
 #define BATTLE_PYRAMID_FUNC_UPDATE_LIGHT        13
 #define BATTLE_PYRAMID_FUNC_CLEAR_HELD_ITEMS    14
 #define BATTLE_PYRAMID_FUNC_SET_FLOOR_PALETTE   15
-#define BATTLE_PYRAMID_FUNC_16                  16
+#define BATTLE_PYRAMID_FUNC_16                  16 // unused
 #define BATTLE_PYRAMID_FUNC_RESTORE_PARTY       17
+
+#define PYRAMID_DATA_PRIZE                   0
+#define PYRAMID_DATA_WIN_STREAK              1
+#define PYRAMID_DATA_WIN_STREAK_ACTIVE       2
+#define PYRAMID_DATA_WIN_STREAK_50           3 // the below streak data are redundant with the above when frontier.lvlMode is correct
+#define PYRAMID_DATA_WIN_STREAK_OPEN         4
+#define PYRAMID_DATA_WIN_STREAK_ACTIVE_50    5
+#define PYRAMID_DATA_WIN_STREAK_ACTIVE_OPEN  6
+#define PYRAMID_DATA_TRAINER_FLAGS           7
+
+#define PYRAMID_LIGHT_SET_RADIUS  0
+#define PYRAMID_LIGHT_INCR_RADIUS 1
 
 #endif // GUARD_CONSTANTS_BATTLE_PYRAMID_H
