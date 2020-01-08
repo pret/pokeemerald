e736c2d2fd7bfb7cbc722a4bdf2e8c7d6bb4376d
GriffinR <griffin.g.richards@gmail.com>
2020-01-08 15:26:14 -0500

    Document some easy chat specials

--- src/dewford_trend.c
+++ src/dewford_trend.c
@@ -25,12 +25,12 @@ void InitDewfordTrend(void)
 
     for (i = 0; i < 5; i++)
     {
-        gSaveBlock1Ptr->easyChatPairs[i].words[0] = sub_811EE38(EC_GROUP_CONDITIONS);
+        gSaveBlock1Ptr->easyChatPairs[i].words[0] = GetRandomEasyChatWordFromGroup(EC_GROUP_CONDITIONS);
 
         if (Random() & 1)
-            gSaveBlock1Ptr->easyChatPairs[i].words[1] = sub_811EE38(EC_GROUP_LIFESTYLE);
+            gSaveBlock1Ptr->easyChatPairs[i].words[1] = GetRandomEasyChatWordFromGroup(EC_GROUP_LIFESTYLE);
         else
-            gSaveBlock1Ptr->easyChatPairs[i].words[1] = sub_811EE38(EC_GROUP_HOBBIES);
+            gSaveBlock1Ptr->easyChatPairs[i].words[1] = GetRandomEasyChatWordFromGroup(EC_GROUP_HOBBIES);
 
         gSaveBlock1Ptr->easyChatPairs[i].unk1_6 = Random() & 1;
         sub_8122B28(&(gSaveBlock1Ptr->easyChatPairs[i]));
