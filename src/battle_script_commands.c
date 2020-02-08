c9774f6e2533410bcc52abc3ecb0c65e60369163
DizzyEggg <jajkodizzy@wp.pl>
2020-02-08 12:09:38 +0100

    Transformed Aegi cannot change forms

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -972,7 +972,9 @@ static bool32 NoTargetPresent(u32 move)
 
 static bool32 TryAegiFormChange(void)
 {
-    if (GetBattlerAbility(gBattlerAttacker) != ABILITY_STANCE_CHANGE)
+    // Only Aegislash with Stance Change can transform, transformed mons cannot.
+    if (GetBattlerAbility(gBattlerAttacker) != ABILITY_STANCE_CHANGE
+        || gBattleMons[gBattlerAttacker].status2 & STATUS2_TRANSFORMED)
         return FALSE;
 
     switch (gBattleMons[gBattlerAttacker].species)
