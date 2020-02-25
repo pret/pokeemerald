42a467814f4ca5d711492532991f9e5fa0f66e53
GriffinR <griffin.g.richards@gmail.com>
2020-02-25 10:14:04 -0500

    Convert base_stats.h to UTF-8

--- src/data/pokemon/base_stats.h
+++ src/data/pokemon/base_stats.h
@@ -1,5 +1,5 @@
-// Maximum value for a female Pokémon is 254 (MON_FEMALE) which is 100% female.
-// 255 (MON_GENDERLESS) is reserved for genderless Pokémon.
+// Maximum value for a female PokÃ©mon is 254 (MON_FEMALE) which is 100% female.
+// 255 (MON_GENDERLESS) is reserved for genderless PokÃ©mon.
 #define PERCENT_FEMALE(percent) min(254, ((percent * 255) / 100))
 
 #define OLD_UNOWN_BASE_STATS                \
