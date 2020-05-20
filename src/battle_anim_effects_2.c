a3b3f245eb8b10f0174bc91a068e43b027419e34
GriffinR <griffin.g.richards@gmail.com>
2020-05-20 15:23:21 -0400

    Use fixed point macros for math util

--- src/battle_anim_effects_2.c
+++ src/battle_anim_effects_2.c
@@ -1578,7 +1578,7 @@ static void AnimAirWaveProjectile(struct Sprite *sprite)
     {
         sprite->data[0] = 8;
         task->data[5] = 4;
-        a = MathUtil_Inv16(0x1000);
+        a = MathUtil_Inv16(Q_8_8(16));
         sprite->pos1.x += sprite->pos2.x;
         sprite->pos1.y += sprite->pos2.y;
         sprite->pos2.y = 0;
@@ -1597,8 +1597,8 @@ static void AnimAirWaveProjectile(struct Sprite *sprite)
         sprite->data[1] = 0;
         sprite->data[6] = 0;
         sprite->data[5] = 0;
-        sprite->data[3] = MathUtil_Mul16(MathUtil_Mul16(b, a), MathUtil_Inv16(0x1C0));
-        sprite->data[4] = MathUtil_Mul16(MathUtil_Mul16(c, a), MathUtil_Inv16(0x1C0));
+        sprite->data[3] = MathUtil_Mul16(MathUtil_Mul16(b, a), MathUtil_Inv16(Q_8_8(1.75)));
+        sprite->data[4] = MathUtil_Mul16(MathUtil_Mul16(c, a), MathUtil_Inv16(Q_8_8(1.75)));
         sprite->callback = AnimAirWaveProjectile_Step1;
     }
 }
@@ -1690,7 +1690,7 @@ void AnimTask_AirCutterProjectile(u8 taskId)
         xDiff = attackerX - targetX;
 
     gTasks[taskId].data[5] = MathUtil_Mul16(xDiff, MathUtil_Inv16(gBattleAnimArgs[2] & ~1));
-    gTasks[taskId].data[6] = MathUtil_Mul16(gTasks[taskId].data[5], 0x80);
+    gTasks[taskId].data[6] = MathUtil_Mul16(gTasks[taskId].data[5], Q_8_8(0.5));
     gTasks[taskId].data[7] = gBattleAnimArgs[2];
     if (targetY >= attackerY)
     {
