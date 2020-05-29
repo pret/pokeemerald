f1ed5e2a8dd10c82bfadf6792f42b9f140b511d6
discoverthemetroid <discoverthemetroid@gmail.com>
2020-05-29 15:24:52 -0500

    added reusable tams
    here we fucking go boys

--- src/party_menu.c
+++ src/party_menu.c
@@ -4702,8 +4702,6 @@ static void Task_LearnedMove(u8 taskId)
     if (move[1] == 0)
     {
         AdjustFriendship(mon, FRIENDSHIP_EVENT_LEARN_TMHM);
-        if (item < ITEM_HM01_CUT)
-            RemoveBagItem(item, 1);
     }
     GetMonNickname(mon, gStringVar1);
     StringCopy(gStringVar2, gMoveNames[move[0]]);
