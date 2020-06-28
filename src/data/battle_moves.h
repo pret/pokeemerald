5d037198b3eb0c8ad6242fdf891fecd8bf03e34a
LOuroboros <lunosouroboros@gmail.com>
2020-06-28 05:11:06 -0300

    Made Throat Chop be affected by Sheer Force (#455)
    Addresses #452

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -8964,7 +8964,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED | FLAG_SHEER_FORCE_BOOST,
         .split = SPLIT_PHYSICAL,
     },
 
