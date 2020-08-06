9afd126a637ad66156f3c1a034a58c6815ccf6ee
Ryan Roelke <rroelke0@gmail.com>
2020-08-05 22:57:41 -0400

    Fix whitespace in json_data_rules.mk

--- json_data_rules.mk
+++ json_data_rules.mk
@@ -19,36 +19,36 @@ PKMN_SUBDIR := $(DATA_SRC_SUBDIR)/pokemon
 PKMN_GRAPHICS_SUBDIR := $(DATA_SRC_SUBDIR)/pokemon_graphics
 
 
-SPECIES_HEADERS :=									\
-	include/constants/species.h						\
-	include/graphics_pokemon.h						\
-	$(GRAPHICS_SUBDIR)/pokemon.h					\
-	$(PKMN_SUBDIR)/base_stats.h						\
-	$(PKMN_SUBDIR)/egg_moves.h						\
+SPECIES_HEADERS :=                                  \
+    include/constants/species.h                     \
+    include/graphics_pokemon.h                      \
+    $(GRAPHICS_SUBDIR)/pokemon.h                    \
+    $(PKMN_SUBDIR)/base_stats.h                     \
+    $(PKMN_SUBDIR)/egg_moves.h                      \
     $(PKMN_SUBDIR)/evolution.h                      \
-	$(PKMN_SUBDIR)/pokedex_entries.h				\
-	$(PKMN_SUBDIR)/pokedex_text.h					\
-	$(PKMN_SUBDIR)/pokemon_jump_table.h				\
-	$(PKMN_SUBDIR)/level_up_learnsets.h				\
-	$(PKMN_SUBDIR)/level_up_learnset_pointers.h		\
-	$(PKMN_SUBDIR)/tutor_learnsets.h                \
-	$(PKMN_SUBDIR)/tmhm_learnsets.h 				\
-	$(PKMN_GRAPHICS_SUBDIR)/back_animation.h		\
-	$(PKMN_GRAPHICS_SUBDIR)/back_pic_coordinates.h	\
-	$(PKMN_GRAPHICS_SUBDIR)/back_pic_table.h		\
-	$(PKMN_GRAPHICS_SUBDIR)/footprint_table.h		\
-	$(PKMN_GRAPHICS_SUBDIR)/front_animation.h		\
-	$(PKMN_GRAPHICS_SUBDIR)/front_pic_anims.h		\
-	$(PKMN_GRAPHICS_SUBDIR)/front_pic_coordinates.h	\
-	$(PKMN_GRAPHICS_SUBDIR)/front_pic_table.h		\
-	$(PKMN_GRAPHICS_SUBDIR)/icon.h					\
-	$(PKMN_GRAPHICS_SUBDIR)/palette_table.h			\
-	$(PKMN_GRAPHICS_SUBDIR)/shiny_palette_table.h	\
-	$(PKMN_GRAPHICS_SUBDIR)/still_front_pic_table.h	\
-	$(PKMN_GRAPHICS_SUBDIR)/unknown_anims.h			\
-	$(TEXT_SUBDIR)/species_names.h
-SPECIES_SRCS :=										\
-	src/anim_mon_front_pics.c
+    $(PKMN_SUBDIR)/pokedex_entries.h                \
+    $(PKMN_SUBDIR)/pokedex_text.h                   \
+    $(PKMN_SUBDIR)/pokemon_jump_table.h             \
+    $(PKMN_SUBDIR)/level_up_learnsets.h             \
+    $(PKMN_SUBDIR)/level_up_learnset_pointers.h     \
+    $(PKMN_SUBDIR)/tutor_learnsets.h                \
+    $(PKMN_SUBDIR)/tmhm_learnsets.h                 \
+    $(PKMN_GRAPHICS_SUBDIR)/back_animation.h        \
+    $(PKMN_GRAPHICS_SUBDIR)/back_pic_coordinates.h  \
+    $(PKMN_GRAPHICS_SUBDIR)/back_pic_table.h        \
+    $(PKMN_GRAPHICS_SUBDIR)/footprint_table.h       \
+    $(PKMN_GRAPHICS_SUBDIR)/front_animation.h       \
+    $(PKMN_GRAPHICS_SUBDIR)/front_pic_anims.h       \
+    $(PKMN_GRAPHICS_SUBDIR)/front_pic_coordinates.h \
+    $(PKMN_GRAPHICS_SUBDIR)/front_pic_table.h       \
+    $(PKMN_GRAPHICS_SUBDIR)/icon.h                  \
+    $(PKMN_GRAPHICS_SUBDIR)/palette_table.h         \
+    $(PKMN_GRAPHICS_SUBDIR)/shiny_palette_table.h   \
+    $(PKMN_GRAPHICS_SUBDIR)/still_front_pic_table.h \
+    $(PKMN_GRAPHICS_SUBDIR)/unknown_anims.h         \
+    $(TEXT_SUBDIR)/species_names.h
+SPECIES_SRCS :=                                     \
+    src/anim_mon_front_pics.c
 
 AUTO_GEN_HEADERS += $(SPECIES_HEADERS)
 AUTO_GEN_SRCS    += $(SPECIES_SRCS)
