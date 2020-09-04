41b7d0e71c2cf009a4082cfb7a0bacdef13c8326
PokeCodec <67983839+PokeCodec@users.noreply.github.com>
2020-09-04 17:27:06 -0400

    Done

--- src/battle_factory.c
+++ src/battle_factory.c
@@ -614,7 +614,6 @@ static void GetOpponentMostCommonMonType(void)
     }
 
     gSpecialVar_Result = gSpecialVar_Result; // Needed to match. Don't ask me why.
-
     if (typesCount[usedType[0]] != 0 && (typesCount[usedType[0]] > typesCount[usedType[1]] || usedType[0] == usedType[1]))
         gSpecialVar_Result = usedType[0];
     else
