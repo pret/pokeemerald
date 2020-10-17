2b95f9fc43bf05d498a50efe999b9abdfa38a56c
Eduardo Quezada <eduardo602002@gmail.com>
2020-10-16 21:04:11 -0300

    Updated types and stats in config.

---
+++ include/constants/pokemon_config.h
@@ -0,0 +1,19 @@
+#ifndef GUARD_CONSTANTS_POKEMON_CONFIG_H
+#define GUARD_CONSTANTS_POKEMON_CONFIG_H
+
+// Used by other branches to communicate with each other.
+#define POKEMON_EXPANSION
+
+#ifndef GEN_3
+#define GEN_3 0
+#define GEN_4 1
+#define GEN_5 2
+#define GEN_6 3
+#define GEN_7 4
+#define GEN_8 5
+#endif
+
+#define P_UPDATED_TYPES     GEN_8 // In Gen6+, several Pokémon were changed to be partially or fully Fairy type.
+#define P_UPDATED_STATS     GEN_8 // Since Gen 6, Pokémon stats are updated with each passing generation.
+
+#endif // GUARD_CONSTANTS_POKEMON_CONFIG_H
