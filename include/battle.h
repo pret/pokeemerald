90211248af2f02aa696b38fbf77ddfd2d2950312
Kurausukun <lord.uber1@gmail.com>
2020-10-23 20:47:10 -0400

    re-match Cmd_unknown_24 (from #1140)
    changes to data structs are necessary. other things that look
    unnecessary, like the order of operands, are also necessary. fuck this
    function.

--- include/battle.h
+++ include/battle.h
@@ -132,14 +132,14 @@ struct ProtectStruct
 
 struct SpecialStatus
 {
-    u8 statLowered:1;
-    u8 lightningRodRedirected:1;
-    u8 restoredBattlerSprite: 1;
-    u8 intimidatedMon:1;
-    u8 traced:1;
-    u8 ppNotAffectedByPressure:1;
-    u8 flag40:1;
-    u8 focusBanded:1;
+    u32 statLowered:1;
+    u32 lightningRodRedirected:1;
+    u32 restoredBattlerSprite: 1;
+    u32 intimidatedMon:1;
+    u32 traced:1;
+    u32 ppNotAffectedByPressure:1;
+    u32 flag40:1;
+    u32 focusBanded:1;
     s32 dmg;
     s32 physicalDmg;
     s32 specialDmg;
@@ -254,7 +254,7 @@ struct BattleResults
     u16 playerMon2Species;    // 0x26
     u16 caughtMonSpecies;     // 0x28
     u8 caughtMonNick[POKEMON_NAME_LENGTH + 1];     // 0x2A
-    u8 filler35[1];           // 0x35
+    u8 filler35;           // 0x35
     u8 catchAttempts[11];     // 0x36
 };
 
