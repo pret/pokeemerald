3f22d0518ff3e1893a60e827aa078e68b975794e
thespbgamer <thespbgamer@users.noreply.github.com>
2020-11-21 17:18:44 +0000

    Surfing Dismount Ground Effects Fix

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1665,6 +1665,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         ScriptContext2_Disable();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
