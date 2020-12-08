c496e0ce6ccadb54d18296a8d89a5a954ad215a7
LOuroboros <lunosouroboros@gmail.com>
2020-12-08 16:20:04 -0300

    Tweaked the X Items' effects' entries in src/data/pokemon/item_effects.h

--- src/data/pokemon/item_effects.h
+++ src/data/pokemon/item_effects.h
@@ -325,42 +325,66 @@ const u8 gItemEffect_DireHit[8] = {
 };
 
 const u8 gItemEffect_XAttack[8] = {
+#if X_ITEMS_REWORK == TRUE
+    [1] = ITEM1_X_ATTACK,
+#else
     [0] = 1,
+#endif
     [5] = ITEM5_FRIENDSHIP_LOW | ITEM5_FRIENDSHIP_MID,
     [6] = 1,
     [7] = 1,
 };
 
 const u8 gItemEffect_XDefend[8] = {
+#if X_ITEMS_REWORK == TRUE
+    [1] = ITEM1_X_DEFEND,
+#else
     [1] = 1 << 4,
+#endif
     [5] = ITEM5_FRIENDSHIP_LOW | ITEM5_FRIENDSHIP_MID,
     [6] = 1,
     [7] = 1,
 };
 
 const u8 gItemEffect_XSpeed[8] = {
+#if X_ITEMS_REWORK == TRUE
+    [1] = ITEM1_X_SPEED,
+#else
     [1] = 1,
+#endif
     [5] = ITEM5_FRIENDSHIP_LOW | ITEM5_FRIENDSHIP_MID,
     [6] = 1,
     [7] = 1,
 };
 
 const u8 gItemEffect_XAccuracy[8] = {
+#if X_ITEMS_REWORK == TRUE
+    [1] = ITEM1_X_ACCURACY,
+#else
     [2] = 1 << 4,
+#endif
     [5] = ITEM5_FRIENDSHIP_LOW | ITEM5_FRIENDSHIP_MID,
     [6] = 1,
     [7] = 1,
 };
 
 const u8 gItemEffect_XSpecialAttack[8] = {
+#if X_ITEMS_REWORK == TRUE
+    [1] = ITEM1_X_SPATK,
+#else
     [2] = 1,
+#endif
     [5] = ITEM5_FRIENDSHIP_LOW | ITEM5_FRIENDSHIP_MID,
     [6] = 1,
     [7] = 1,
 };
 
 const u8 gItemEffect_XSpecialDefense[8] = {
+#if X_ITEMS_REWORK == TRUE
+    [1] = ITEM1_X_SPDEF,
+#else
     [2] = 1,
+#endif
     [5] = ITEM5_FRIENDSHIP_LOW | ITEM5_FRIENDSHIP_MID,
     [6] = 1,
     [7] = 1,
