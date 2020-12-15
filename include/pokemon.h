305246c2de8ba375ef26ff0087b6a19e6aa4fa21
ExpoSeed <>
2020-12-14 20:16:07 -0600

    Introduce NUM_ABILITY_SLOTS

--- include/pokemon.h
+++ include/pokemon.h
@@ -199,7 +199,7 @@ struct BaseStats
  /* 0x13 */ u8 growthRate;
  /* 0x14 */ u8 eggGroup1;
  /* 0x15 */ u8 eggGroup2;
- /* 0x16 */ u8 abilities[2];
+ /* 0x16 */ u8 abilities[NUM_ABILITY_SLOTS];
  /* 0x18 */ u8 safariZoneFleeRate;
  /* 0x19 */ u8 bodyColor : 7;
             u8 noFlip : 1;
