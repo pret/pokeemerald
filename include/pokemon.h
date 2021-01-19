305ebdbc4a41960faa4fdde2422b443036f1edad
GriffinR <griffin.g.richards@gmail.com>
2021-01-19 12:36:36 -0500

    Parentheses for GET_UNOWN_LETTER

--- include/pokemon.h
+++ include/pokemon.h
@@ -240,11 +240,11 @@ struct Evolution
 
 #define NUM_UNOWN_FORMS 28
 
-#define GET_UNOWN_LETTER(personality) (( \
-      ((personality & 0x03000000) >> 18) \
-    | ((personality & 0x00030000) >> 12) \
-    | ((personality & 0x00000300) >> 6)  \
-    | ((personality & 0x00000003) >> 0)  \
+#define GET_UNOWN_LETTER(personality) ((   \
+      (((personality) & 0x03000000) >> 18) \
+    | (((personality) & 0x00030000) >> 12) \
+    | (((personality) & 0x00000300) >> 6)  \
+    | (((personality) & 0x00000003) >> 0)  \
 ) % NUM_UNOWN_FORMS)
 
 extern u8 gPlayerPartyCount;
