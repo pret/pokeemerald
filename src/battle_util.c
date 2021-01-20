2a888a7b3e9b53bc9254850c30ad8c0f402cfc43
LOuroboros <lunosouroboros@gmail.com>
2021-01-20 19:30:48 -0300

    Corrected base species check for Thick Club's effect
    Co-authored-by: ultima-soul <33333039+ultima-soul@users.noreply.github.com>

--- src/battle_util.c
+++ src/battle_util.c
@@ -7116,8 +7116,8 @@ static u32 CalcAttackStat(u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, b
     switch (GetBattlerHoldEffect(battlerAtk, TRUE))
     {
     case HOLD_EFFECT_THICK_CLUB:
-        if ((GET_BASE_SPECIES_ID(battlerAtk) == SPECIES_CUBONE
-         || GET_BASE_SPECIES_ID(battlerAtk) == SPECIES_MAROWAK)
+        if ((GET_BASE_SPECIES_ID(gBattleMons[battlerAtk].species) == SPECIES_CUBONE
+         || GET_BASE_SPECIES_ID(gBattleMons[battlerAtk].species) == SPECIES_MAROWAK)
          && IS_MOVE_PHYSICAL(move))
             MulModifier(&modifier, UQ_4_12(2.0));
         break;
