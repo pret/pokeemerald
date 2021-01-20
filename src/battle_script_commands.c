57c460f40a9b99a4a47851b5191a289c641998cc
Evan <eroelke@gmail.com>
2021-01-19 22:10:03 -0700

    fix two turn moves failing on turn 1 w no target

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1319,7 +1319,7 @@ static void Cmd_attackcanceler(void)
     }
 
     gHitMarker |= HITMARKER_OBEYS;
-    if (NoTargetPresent(gCurrentMove))
+    if (NoTargetPresent(gCurrentMove) && (!IsTwoTurnsMove(gCurrentMove) || (gBattleMons[gBattlerAttacker].status2 & STATUS2_MULTIPLETURNS)))
     {
         gBattlescriptCurrInstr = BattleScript_ButItFailedAtkStringPpReduce;
         if (!IsTwoTurnsMove(gCurrentMove) || (gBattleMons[gBattlerAttacker].status2 & STATUS2_MULTIPLETURNS))
