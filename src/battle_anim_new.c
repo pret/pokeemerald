847700fbb67c3ba1e0429ebef1faff2358a837df
Evan <eroelke@gmail.com>
2021-03-01 17:14:56 -0700

    fix GetIllusionMonPtr

--- src/battle_anim_new.c
+++ src/battle_anim_new.c
@@ -7680,7 +7680,7 @@ void AnimTask_GetLycanrocForm(u8 taskId)
 {
 
     #ifdef POKEMON_EXPANSION
-    if (GetIllusionPartyData(gBattleAnimAttacker)->species == SPECIES_LYCANROC_MIDNIGHT)
+    if (GetIllusionMonPtr(gBattleAnimAttacker)->species == SPECIES_LYCANROC_MIDNIGHT)
         gBattleAnimArgs[0] = 1;
     else
     #endif
