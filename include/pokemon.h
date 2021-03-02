7e8659c4eeaa6e9e984ffed598a183d39d3a2086
ExpoSeed <>
2021-03-02 14:17:48 -0600

    Fix missed merge conflict

--- include/pokemon.h
+++ include/pokemon.h
@@ -244,14 +244,12 @@ struct Evolution
     u16 targetSpecies;
 };
 
-<<<<<<< HEAD
 struct FormChange {
     u16 method;
     u16 targetSpecies;
     u16 param1;
     u16 param2;
 };
-=======
 #define NUM_UNOWN_FORMS 28
 
 #define GET_UNOWN_LETTER(personality) ((   \
@@ -260,7 +258,6 @@ struct FormChange {
     | (((personality) & 0x00000300) >> 6)  \
     | (((personality) & 0x00000003) >> 0)  \
 ) % NUM_UNOWN_FORMS)
->>>>>>> b2a541a33b2a5116f6780f1830f2606a5dbc065e
 
 extern u8 gPlayerPartyCount;
 extern struct Pokemon gPlayerParty[PARTY_SIZE];
