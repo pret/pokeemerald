6739433e9ec9c0372fd2e94aeb6411929f5f6bcf
ghoulslash <41651341+ghoulslash@users.noreply.github.com>
2021-03-03 10:48:05 -0700

    Update src/battle_util.c
    Co-authored-by: ultima-soul <33333039+ultima-soul@users.noreply.github.com>

--- src/battle_util.c
+++ src/battle_util.c
@@ -6215,7 +6215,7 @@ u8 ItemBattleEffects(u8 caseID, u8 battlerId, bool8 moveTurn)
                  && TARGET_TURN_DAMAGED
                  && !DoesSubstituteBlockMove(gBattlerAttacker, battlerId, gCurrentMove)
                  && IS_MOVE_PHYSICAL(gCurrentMove)
-                  && GetBattlerAbility(gBattlerAttacker) != ABILITY_MAGIC_GUARD)
+                 && GetBattlerAbility(gBattlerAttacker) != ABILITY_MAGIC_GUARD)
                 {
                     gBattleMoveDamage = gBattleMons[gBattlerAttacker].maxHP / 8;
                     if (gBattleMoveDamage == 0)
