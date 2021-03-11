722ab79088d4c72912287d471dc0c91684055cbb
N <71219152+SleepyDweams@users.noreply.github.com>
2021-03-11 09:42:38 -0500

    Simplify MPlayStart

--- src/m4a.c
+++ src/m4a.c
@@ -599,50 +599,46 @@ void MPlayStart(struct MusicPlayerInfo *mplayInfo, struct SongHeader *songHeader
 
     unk_B = mplayInfo->unk_B;
 
-    if (!unk_B
-        || ((!mplayInfo->songHeader || !(mplayInfo->tracks[0].flags & MPT_FLG_START))
-            && ((mplayInfo->status & MUSICPLAYER_STATUS_TRACK) == 0
-                || (mplayInfo->status & MUSICPLAYER_STATUS_PAUSE)))
-        || (mplayInfo->priority <= songHeader->priority))
+    if (unk_B && ((mplayInfo->songHeader && (mplayInfo->tracks[0].flags & MPT_FLG_START)) || ((mplayInfo->status & MUSICPLAYER_STATUS_TRACK) && !(mplayInfo->status & MUSICPLAYER_STATUS_PAUSE))) && (mplayInfo->priority > songHeader->priority))
+        return;
+
+    mplayInfo->ident++;
+    mplayInfo->status = 0;
+    mplayInfo->songHeader = songHeader;
+    mplayInfo->tone = songHeader->tone;
+    mplayInfo->priority = songHeader->priority;
+    mplayInfo->clock = 0;
+    mplayInfo->tempoD = 150;
+    mplayInfo->tempoI = 150;
+    mplayInfo->tempoU = 0x100;
+    mplayInfo->tempoC = 0;
+    mplayInfo->fadeOI = 0;
+
+    i = 0;
+    track = mplayInfo->tracks;
+
+    while (i < songHeader->trackCount && i < mplayInfo->trackCount)
     {
-        mplayInfo->ident++;
-        mplayInfo->status = 0;
-        mplayInfo->songHeader = songHeader;
-        mplayInfo->tone = songHeader->tone;
-        mplayInfo->priority = songHeader->priority;
-        mplayInfo->clock = 0;
-        mplayInfo->tempoD = 150;
-        mplayInfo->tempoI = 150;
-        mplayInfo->tempoU = 0x100;
-        mplayInfo->tempoC = 0;
-        mplayInfo->fadeOI = 0;
-
-        i = 0;
-        track = mplayInfo->tracks;
-
-        while (i < songHeader->trackCount && i < mplayInfo->trackCount)
-        {
-            TrackStop(mplayInfo, track);
-            track->flags = MPT_FLG_EXIST | MPT_FLG_START;
-            track->chan = 0;
-            track->cmdPtr = songHeader->part[i];
-            i++;
-            track++;
-        }
-
-        while (i < mplayInfo->trackCount)
-        {
-            TrackStop(mplayInfo, track);
-            track->flags = 0;
-            i++;
-            track++;
-        }
-
-        if (songHeader->reverb & SOUND_MODE_REVERB_SET)
-            m4aSoundMode(songHeader->reverb);
-
-        mplayInfo->ident = ID_NUMBER;
+        TrackStop(mplayInfo, track);
+        track->flags = MPT_FLG_EXIST | MPT_FLG_START;
+        track->chan = 0;
+        track->cmdPtr = songHeader->part[i];
+        i++;
+        track++;
     }
+
+    while (i < mplayInfo->trackCount)
+    {
+        TrackStop(mplayInfo, track);
+        track->flags = 0;
+        i++;
+        track++;
+    }
+
+    if (songHeader->reverb & SOUND_MODE_REVERB_SET)
+        m4aSoundMode(songHeader->reverb);
+
+    mplayInfo->ident = ID_NUMBER;
 }
 
 void m4aMPlayStop(struct MusicPlayerInfo *mplayInfo)
