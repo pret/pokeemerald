937c24ca514c409b246656fab26473525ac94496
BuffelSaft <buffalojuice@gmail.com>
2021-03-16 23:37:02 +1300

    Remove special Body Press effect
    Only include the official effect for Body Press to stay faithful to the gen 8 games.

--- src/battle_util.c
+++ src/battle_util.c
@@ -7309,16 +7309,8 @@ static u32 CalcAttackStat(u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, b
     }
     if (gBattleMoves[move].effect == EFFECT_BODY_PRESS)
     {
-        if (IS_MOVE_PHYSICAL(move))
-        {
-            atkStat = gBattleMons[battlerAtk].defense;
-            atkStage = gBattleMons[battlerAtk].statStages[STAT_DEF];
-        }
-        else
-        {
-            atkStat = gBattleMons[battlerAtk].spDefense;
-            atkStage = gBattleMons[battlerAtk].statStages[STAT_SPDEF];
-        }
+        atkStat = gBattleMons[battlerAtk].defense;
+        atkStage = gBattleMons[battlerAtk].statStages[STAT_DEF];
     }
     else
     {
