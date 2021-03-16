22e2c0a47ca876313604f0dd031abb324477186f
GriffinR <griffin.g.richards@gmail.com>
2021-03-16 05:40:42 -0400

    Document Berry Crush

--- include/global.h
+++ include/global.h
@@ -209,7 +209,7 @@ struct PyramidBag
 
 struct BerryCrush
 {
-    u16 berryCrushResults[4];
+    u16 pressingSpeeds[4]; // For the record with each possible group size, 2-5 players
     u32 berryPowderAmount;
     u32 unk;
 };
