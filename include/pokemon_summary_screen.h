72ba8cf58de8297c56d2269e0519064dc2de5f76
GriffinR <griffin.g.richards@gmail.com>
2021-04-09 12:41:02 -0400

    Document pokemon animation types

--- include/pokemon_summary_screen.h
+++ include/pokemon_summary_screen.h
@@ -12,8 +12,7 @@ void ShowPokemonSummaryScreen(u8 mode, void *mons, u8 monIndex, u8 maxMonIndex, 
 void ShowSelectMovePokemonSummaryScreen(struct Pokemon *mons, u8 monIndex, u8 maxMonIndex, void (*callback)(void), u16 newMove);
 void ShowPokemonSummaryScreenSet40EF(u8 mode, struct BoxPokemon *mons, u8 monIndex, u8 maxMonIndex, void (*callback)(void));
 u8 GetMoveSlotToReplace(void);
-void SummaryScreen_SetUnknownTaskId(u8 taskId);
-void SummaryScreen_DestroyUnknownTask(void);
+void SummaryScreen_SetAnimDelayTaskId(u8 taskId);
 
 // The Pokemon Summary Screen can operate in different modes. Certain features,
 // such as move re-ordering, are available in the different modes.
