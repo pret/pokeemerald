ee93c24369748b1cb1f4005903a7a9b4aa437fea
ProfLeonDias <76541857+ProfLeonDias@users.noreply.github.com>
2021-05-14 03:20:45 -0400

    Update mystery_event_msg.c

--- src/mystery_event_msg.c
+++ src/mystery_event_msg.c
@@ -1,13 +1,13 @@
 #include "global.h"
 
-const u8 gText_MysteryGiftBerry[] = _("Obtained a {STR_VAR_2} BERRY!\nDad has it at PETALBURG GYM.");
-const u8 gText_MysteryGiftBerryTransform[] = _("The {STR_VAR_1} BERRY transformed into\none {STR_VAR_2} BERRY.");
-const u8 gText_MysteryGiftBerryObtained[] = _("The {STR_VAR_1} BERRY has already been\nobtained.");
-const u8 gText_MysteryGiftSpecialRibbon[] = _("A special RIBBON was awarded to\nyour party POKéMON.");
-const u8 gText_MysteryGiftNationalDex[] = _("The POKéDEX has been upgraded\nwith the NATIONAL MODE.");
+const u8 gText_MysteryGiftBerry[] = _("Obtained a {STR_VAR_2} Berry!\nDad has it at Petalburg Gym.");
+const u8 gText_MysteryGiftBerryTransform[] = _("The {STR_VAR_1} Berry transformed into\none {STR_VAR_2} Berry.");
+const u8 gText_MysteryGiftBerryObtained[] = _("The {STR_VAR_1} Berry has already been\nobtained.");
+const u8 gText_MysteryGiftSpecialRibbon[] = _("A special Ribbon was awarded to\nyour party Pokémon.");
+const u8 gText_MysteryGiftNationalDex[] = _("The Pokédex has been upgraded\nwith the National Mode.");
 const u8 gText_MysteryGiftRareWord[] = _("A rare word has been added.");
 const u8 gText_MysteryGiftSentOver[] = _("{STR_VAR_1} was sent over!");
 const u8 gText_MysteryGiftFullParty[] = _("Your party is full.\n{STR_VAR_1} could not be sent over.");
-const u8 gText_MysteryGiftNewTrainer[] = _("A new TRAINER has arrived in\nHOENN.");
-const u8 gText_MysteryGiftNewAdversaryInBattleTower[] = _("A new adversary has arrived in the\nBATTLE TOWER.");
+const u8 gText_MysteryGiftNewTrainer[] = _("A new Trainer has arrived in\nHoenn.");
+const u8 gText_MysteryGiftNewAdversaryInBattleTower[] = _("A new adversary has arrived in the\nBattle Tower.");
 const u8 gText_MysteryGiftCantBeUsed[] = _("This data can't be used in\nthis version.");
