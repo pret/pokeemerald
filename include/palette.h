1140bf5cff4a0f70c4f3e99f908806845aa61230
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-05-25 13:11:26 -0400

    Fix structs

--- include/palette.h
+++ include/palette.h
@@ -34,15 +34,15 @@ struct PaletteFadeControl
     u16 targetY:5; // target blend coefficient
     u16 blendColor:15;
     bool16 active:1;
-    u16 multipurpose2:6;
-    bool16 yDec:1; // whether blend coefficient is decreasing
-    bool16 bufferTransferDisabled:1;
-    u16 mode:2;
-    bool16 shouldResetBlendRegisters:1;
-    bool16 hardwareFadeFinishing:1;
-    u16 softwareFadeFinishingCounter:5;
-    bool16 softwareFadeFinishing:1;
-    bool16 objPaletteToggle:1;
+    u8 multipurpose2:6;
+    bool8 yDec:1; // whether blend coefficient is decreasing
+    bool8 bufferTransferDisabled:1;
+    u8 mode:2;
+    bool8 shouldResetBlendRegisters:1;
+    bool8 hardwareFadeFinishing:1;
+    u8 softwareFadeFinishingCounter:5;
+    bool8 softwareFadeFinishing:1;
+    bool8 objPaletteToggle:1;
     u8 deltaY:4; // rate of change of blend coefficient
 };
 
