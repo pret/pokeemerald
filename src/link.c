09add9500a590010c957a8fc796241ea3544930c
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-05-30 18:58:32 -0400

    Undo change

--- src/link.c
+++ src/link.c
@@ -508,7 +508,9 @@ u16 LinkMain2(const u16 *heldKeys)
 static void HandleReceiveRemoteLinkPlayer(u8 who)
 {
     int i;
-    int count = 0;
+    int count;
+
+    count = 0;
     gRemoteLinkPlayersNotReceived[who] = FALSE;
     for (i = 0; i < GetLinkPlayerCount_2(); i++)
     {
