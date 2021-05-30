041b387fa70fe54100fbe9bd0b7e7596f6f642a4
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-05-30 15:40:37 -0400

    Update battle_anim_effects_3.c

--- src/battle_anim_effects_3.c
+++ src/battle_anim_effects_3.c
@@ -5001,8 +5001,8 @@ void AnimTask_GetReturnPowerLevel(u8 taskId)
 void AnimTask_SnatchOpposingMonMove(u8 taskId)
 {
     u8 spriteId, spriteId2;
-    int personality;
-    int otId;
+    u32 personality;
+    u32 otId;
     u16 species;
     u8 subpriority;
     bool8 isBackPic;
