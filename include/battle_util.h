2c1237365b935aa537c2f3d1e1b7c10728fe02a1
LOuroboros <lunosouroboros@gmail.com>
2021-06-07 22:31:12 -0300

    Fixed Emergency Exit The ability was getting triggered even if the user was the only Pokémon alive in the party.

--- include/battle_util.h
+++ include/battle_util.h
@@ -138,6 +138,7 @@ bool32 IsTelekinesisBannedSpecies(u16 species);
 bool32 IsHealBlockPreventingMove(u32 battler, u32 move);
 bool32 IsThawingMove(u8 battlerId, u16 move);
 bool32 HasEnoughHpToEatBerry(u32 battlerId, u32 hpFraction, u32 itemId);
+bool32 IsLastAliveMonInParty(u8 battlerId);
 
 // ability checks
 bool32 IsRolePlayBannedAbilityAtk(u16 ability);
