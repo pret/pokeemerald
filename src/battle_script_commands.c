1f3983cb05fc91945004c5dd75507def95e01c05
BuffelSaft <buffalojuice@gmail.com>
2021-08-07 20:38:35 +1200

    Shields Down protects all meteor form Minior
    This was only working with red core minior.

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -7138,7 +7138,7 @@ u32 IsLeafGuardProtected(u32 battler)
 bool32 IsShieldsDownProtected(u32 battler)
 {
     return (gBattleMons[battler].ability == ABILITY_SHIELDS_DOWN
-            && gBattleMons[battler].species == SPECIES_MINIOR);
+            && GetFormIdFromFormSpeciesId(gBattleMons[battler].species) < 6); // Minior is not in core form
 }
 
 u32 IsAbilityStatusProtected(u32 battler)
