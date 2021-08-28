30c56be32d4659cd92db10336babadffe2647204
chowder908 <chowder90856@gmail.com>
2021-08-28 03:00:44 -0500

    Working on ass clap move

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -1159,7 +1159,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
 
     [MOVE_EARTHQUAKE] =
     {
-        .effect = EFFECT_TEST,
+        .effect = EFFECT_EARTHQUAKE,
         .power = 100,
         .type = TYPE_GROUND,
         .accuracy = 100,
@@ -4629,7 +4629,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     },
     [MOVE_TEST] =
     {
-        .effect = EFFECT_EARTHQUAKE,
+        .effect = EFFECT_TEST,
         .power = 100,
         .type = TYPE_GROUND,
         .accuracy = 100,
