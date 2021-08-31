d0fd883ec71fdefa62e510dc17ac1f3fd580e079
LOuroboros <lunosouroboros@gmail.com>
2021-08-30 21:49:45 -0300

    Small comment tweak

--- src/battle_util.c
+++ src/battle_util.c
@@ -7891,7 +7891,7 @@ static u32 CalcFinalDmg(u32 dmg, u16 move, u8 battlerAtk, u8 battlerDef, u8 move
         && gBattleMoves[move].effect != EFFECT_FACADE && abilityAtk != ABILITY_GUTS)
         dmg = ApplyModifier(UQ_4_12(0.5), dmg);
 
-    // check sunny/rain weather
+    // check weather
     if (WEATHER_HAS_EFFECT && gBattleWeather & WEATHER_RAIN_ANY)
     {
         if (moveType == TYPE_FIRE)
