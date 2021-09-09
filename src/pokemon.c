f5c93c6f9c7aba25e2b7b06e38797bfc651ed052
CyanSMP64 <jmleuschke64@yahoo.com>
2021-09-09 20:45:16 +1200

    final touches?

--- src/pokemon.c
+++ src/pokemon.c
@@ -6304,8 +6304,17 @@ u16 GetBattleBGM(void)
         case SPECIES_ARTICUNO:
         case SPECIES_ZAPDOS:
         case SPECIES_MOLTRES:
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_ARTICUNO_GALARIAN:
+        case SPECIES_ZAPDOS_GALARIAN:
+        case SPECIES_MOLTRES_GALARIAN:
+        #endif
             return MUS_RG_VS_LEGEND;
         case SPECIES_MEWTWO:
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_MEWTWO_MEGA_X:
+        case SPECIES_MEWTWO_MEGA_Y:
+        #endif
             return MUS_RG_VS_MEWTWO;
         case SPECIES_MEW:
             return MUS_VS_MEW;
@@ -6319,16 +6328,115 @@ u16 GetBattleBGM(void)
             return HG_SEQ_GS_VS_LUGIA;
         case SPECIES_HO_OH:
             return HG_SEQ_GS_VS_HOUOU;
+        case SPECIES_CELEBI:
+            return HG_SEQ_GS_VS_NORAPOKE;
         case SPECIES_REGIROCK:
         case SPECIES_REGICE:
         case SPECIES_REGISTEEL:
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_REGIGIGAS:
+        case SPECIES_REGIELEKI:
+        case SPECIES_REGIDRAGO:
+        #endif
             return MUS_VS_REGI;
+        case SPECIES_LATIAS:
+        case SPECIES_LATIOS:
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_LATIAS_MEGA:
+        case SPECIES_LATIOS_MEGA:
+        #endif
+            return MUS_VS_WILD;
         case SPECIES_GROUDON:
         case SPECIES_KYOGRE:
         case SPECIES_RAYQUAZA:
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_RAYQUAZA_MEGA:
+        case SPECIES_KYOGRE_PRIMAL:
+        case SPECIES_GROUDON_PRIMAL:
+        #endif
             return MUS_VS_KYOGRE_GROUDON;
+        case SPECIES_JIRACHI:
+            return MUS_VS_WILD;
         case SPECIES_DEOXYS:
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_DEOXYS_ATTACK:
+        case SPECIES_DEOXYS_DEFENSE:
+        case SPECIES_DEOXYS_SPEED:
+        #endif
             return MUS_RG_VS_DEOXYS;
+        #ifdef POKEMON_EXPANSION
+        case SPECIES_UXIE:
+        case SPECIES_MESPRIT:
+        case SPECIES_AZELF:
+            return DP_SEQ_BA_DPOKE1;
+        case SPECIES_DIALGA:
+        case SPECIES_PALKIA:
+            return DP_SEQ_BA_DPOKE2;
+        case SPECIES_ROTOM:
+        case SPECIES_ROTOM_HEAT:
+        case SPECIES_ROTOM_WASH:
+        case SPECIES_ROTOM_FROST:
+        case SPECIES_ROTOM_FAN:
+        case SPECIES_ROTOM_MOW:
+        case SPECIES_HEATRAN:
+        case SPECIES_MANAPHY:
+        case SPECIES_DARKRAI:
+            return DP_SEQ_BA_SECRET2;
+        case SPECIES_GIRATINA:
+        case SPECIES_GIRATINA_ORIGIN:
+            return PL_SEQ_PL_BA_GIRA;
+        case SPECIES_CRESSELIA:
+        case SPECIES_PHIONE:
+        case SPECIES_SHAYMIN:
+        case SPECIES_SHAYMIN_SKY:
+            return DP_SEQ_BA_POKE;
+        case SPECIES_ARCEUS:
+        case SPECIES_ARCEUS_FIGHTING:
+        case SPECIES_ARCEUS_FLYING:
+        case SPECIES_ARCEUS_POISON:
+        case SPECIES_ARCEUS_GROUND:
+        case SPECIES_ARCEUS_ROCK:
+        case SPECIES_ARCEUS_BUG:
+        case SPECIES_ARCEUS_GHOST:
+        case SPECIES_ARCEUS_STEEL:
+        case SPECIES_ARCEUS_FIRE:
+        case SPECIES_ARCEUS_WATER:
+        case SPECIES_ARCEUS_GRASS:
+        case SPECIES_ARCEUS_ELECTRIC:
+        case SPECIES_ARCEUS_PSYCHIC:
+        case SPECIES_ARCEUS_ICE:
+        case SPECIES_ARCEUS_DRAGON:
+        case SPECIES_ARCEUS_DARK:
+        case SPECIES_ARCEUS_FAIRY:
+            return DP_SEQ_BA_SECRET1;
+        case SPECIES_VICTINI:
+        case SPECIES_COBALION:
+        case SPECIES_TERRAKION:
+        case SPECIES_VIRIZION:
+        case SPECIES_TORNADUS:
+        case SPECIES_THUNDURUS:
+        case SPECIES_LANDORUS:
+        case SPECIES_TORNADUS_THERIAN:
+        case SPECIES_THUNDURUS_THERIAN:
+        case SPECIES_LANDORUS_THERIAN:
+        case SPECIES_KELDEO:
+        case SPECIES_KELDEO_RESOLUTE:
+        case SPECIES_MELOETTA:
+        case SPECIES_MELOETTA_PIROUETTE:
+        case SPECIES_GENESECT:
+        case SPECIES_GENESECT_DOUSE_DRIVE:
+        case SPECIES_GENESECT_SHOCK_DRIVE:
+        case SPECIES_GENESECT_BURN_DRIVE:
+        case SPECIES_GENESECT_CHILL_DRIVE:
+            return BW_SEQ_BGM_VS_MOVEPOKE;
+        case SPECIES_RESHIRAM:
+        case SPECIES_ZEKROM:
+            return BW_SEQ_BGM_VS_SHIN;
+        case SPECIES_KYUREM:
+        case SPECIES_KYUREM_WHITE:
+        case SPECIES_KYUREM_BLACK:
+            return BW_SEQ_BGM_VS_RAI;
+        #endif
         default:
             return MUS_VS_WILD;
         }
