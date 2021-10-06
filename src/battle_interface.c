3d24fd5f3094a20cf1325607148323e397a6f00c
ghoulslash <pokevoyager0@gmail.com>
2021-10-06 13:56:04 -0400

    last ball defaults to first bag pocket item

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -3206,10 +3206,8 @@ bool32 CanThrowLastUsedBall(void)
 void TryAddLastUsedBallItemSprites(void)
 {
     #if B_LAST_USED_BALL == TRUE
-    if (gLastThrownBall == 0)
-        gLastThrownBall = ITEM_POKE_BALL;
-    
-    if (gLastThrownBall != 0 && !CheckBagHasItem(gLastThrownBall, 1))
+    if ((gLastThrownBall == 0
+      || (gLastThrownBall != 0 && !CheckBagHasItem(gLastThrownBall, 1)))
     {
         // we're out of the last used ball, so just set it to the first ball in the bag
         // we have to compact the bag first bc it is typically only compacted when you open it
