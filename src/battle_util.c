c0f3b7c3b4ef754ed190e8af169876f6512f4e65
ghoulslash <pokevoyager0@gmail.com>
2021-10-12 11:58:37 -0400

    record abilities from wandering spirit

--- src/battle_util.c
+++ src/battle_util.c
@@ -4826,10 +4826,10 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
                     break;
                 default:
                     gLastUsedAbility = gBattleMons[gBattlerAttacker].ability;
-                    
                     gBattleMons[gBattlerAttacker].ability = gBattleMons[gBattlerTarget].ability;
                     gBattleMons[gBattlerTarget].ability = gLastUsedAbility;
-                    
+                    RecordAbilityBattle(gBattlerAttacker, gBattleMons[gBattlerAttacker].ability);
+                    RecordAbilityBattle(gBattlerTarget, gBattleMons[gBattlerTarget].ability);
                     BattleScriptPushCursor();
                     gBattlescriptCurrInstr = BattleScript_WanderingSpiritActivates;
                     effect++;
