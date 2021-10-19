4673ff30e47f3a08a3f7ad2418c36aad9a508014
Xavion3 <xavion333@gmail.com>
2021-10-19 23:00:40 +1100

    Allow the AI to see damage against it's partner

--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -177,10 +177,10 @@ void BattleAI_SetupAIData(u8 defaultScoreMoves)
     }
 
     sBattler_AI = gActiveBattler;
-    // Simulate dmg for all AI moves against all opposing targets
+    // Simulate dmg for all AI moves against all other targets
     for (gBattlerTarget = 0; gBattlerTarget < gBattlersCount; gBattlerTarget++)
     {
-        if (GET_BATTLER_SIDE2(sBattler_AI) == GET_BATTLER_SIDE2(gBattlerTarget))
+        if (sBattler_AI == gBattlerTarget)
             continue;
         for (i = 0; i < MAX_MON_MOVES; i++)
         {
