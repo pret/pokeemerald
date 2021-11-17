7ae30f891333f945ed489fc092d9c9ceea2c3ac2
LOuroboros <lunosouroboros@gmail.com>
2021-11-17 07:15:14 -0300

    I_LEGACY_HEALING_ITEMS -> I_HEALTH_RECOVERY Also fixed its inaccurate description.

--- include/constants/item_config.h
+++ include/constants/item_config.h
@@ -16,7 +16,7 @@
 #define I_SHINY_CHARM_REROLLS   3       // Amount of re-rolls if the player has the Shiny Charm. Set to 0 to disable Shiny Charm's effects.
 #define I_KEY_FOSSILS           GEN_7   // In Gen4+, all Gen 3 fossils became regular items.
 #define I_KEY_ESCAPE_ROPE       GEN_7   // In Gen8, Escape Rope became a Key Item. Keep in mind, this will make it free to buy in marts.
-#define I_LEGACY_HEALING_ITEMS  GEN_7   // In Gen7+, certain healing items recover less HP than they used to.
+#define I_HEALTH_RECOVERY       GEN_7   // In Gen7+, certain healing items recover a different amount of HP than they used to.
 #define I_SITRUS_BERRY_HEAL     GEN_7   // In Gen4+, Sitrus Berry was changed from healing 30 HP to healing 25% of Max HP.
 
 #endif // GUARD_CONSTANTS_ITEM_CONFIG_H
