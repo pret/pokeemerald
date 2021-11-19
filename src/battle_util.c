fb468b67762fd08f9021152bf7fa70216ab4d771
ghoulslash <pokevoyager0@gmail.com>
2021-11-19 16:32:00 -0500

    fix unseen fist check

--- src/battle_util.c
+++ src/battle_util.c
@@ -7520,7 +7520,7 @@ bool32 IsBattlerProtected(u8 battlerId, u16 move)
 
     // Protective Pads doesn't stop Unseen Fist from bypassing Protect effects, so IsMoveMakingContact() isn't used here.
     // This means extra logic is needed to handle Shell Side Arm.
-    if (GetBattlerAbility(gBattlerAttacker == ABILITY_UNSEEN_FIST)
+    if (GetBattlerAbility(gBattlerAttacker) == ABILITY_UNSEEN_FIST
         && (gBattleMoves[move].flags & FLAG_MAKES_CONTACT || (gBattleMoves[move].effect == EFFECT_SHELL_SIDE_ARM && gSwapDamageCategory)))
         return FALSE;
     else if (!(gBattleMoves[move].flags & FLAG_PROTECT_AFFECTED))
