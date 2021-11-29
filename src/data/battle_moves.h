ec596cf9b2fea01b686aa093021b4264055c0b3d
SonikkuA-DatH <58025603+SonikkuA-DatH@users.noreply.github.com>
2021-11-28 19:58:18 -0800

    Update battle_moves.h
    Not sure why it works better with User than "All targets"

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -10857,13 +10857,13 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
 
     [MOVE_TEATIME] =
     {
-        .effect = EFFECT_PLACEHOLDER,   //TODO
+        .effect = EFFECT_TEATIME,
         .power = 0,
         .type = TYPE_NORMAL,
         .accuracy = 0,
         .pp = 10,
         .secondaryEffectChance = 0,
-        .target = MOVE_TARGET_ALL_BATTLERS,
+        .target = MOVE_TARGET_USER,
         .priority = 0,
         .flags = 0,
         .split = SPLIT_STATUS,
