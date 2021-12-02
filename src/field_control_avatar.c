a36445ea11cb889dba82fffe1f91699ac3a9e147
TheXaman <48356183+TheXaman@users.noreply.github.com>
2021-12-02 18:16:41 +0100

    Changed opening to summary screen SELECT

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -20,7 +20,6 @@
 #include "metatile_behavior.h"
 #include "overworld.h"
 #include "pokemon.h"
-#include "pokemon_debug.h"
 #include "safari_zone.h"
 #include "script.h"
 #include "secret_base.h"
@@ -131,14 +130,6 @@ void FieldGetPlayerInput(struct FieldInput *input, u16 newKeys, u16 heldKeys)
         input->dpadDirection = DIR_WEST;
     else if (heldKeys & DPAD_RIGHT)
         input->dpadDirection = DIR_EAST;
-
-    #ifdef P_ENABLE_DEBUG
-    if ((heldKeys & R_BUTTON) && input->pressedStartButton)
-    {
-        input->input_field_1_2 = TRUE;
-        input->pressedStartButton = FALSE;
-    }
-    #endif
 }
 
 int ProcessPlayerFieldInput(struct FieldInput *input)
@@ -197,21 +188,6 @@ int ProcessPlayerFieldInput(struct FieldInput *input)
     }
     if (input->pressedSelectButton && UseRegisteredKeyItemOnField() == TRUE)
         return TRUE;
-
-    
-    #ifdef P_ENABLE_DEBUG
-    if (input->input_field_1_2)
-    {
-        //PlaySE(SE_WIN_OPEN);
-        //Debug_ShowMainMenu();
-
-        //PlayRainStoppingSoundEffect();
-        CleanupOverworldWindowsAndTilemaps();
-        SetMainCallback2(CB2_Debug_Pokemon);
-    
-        return TRUE;
-    }
-    #endif
 
     return FALSE;
 }
