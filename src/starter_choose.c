6de03e91a3faf7f61ed09d1582b9cc4cf3e5b715
lemondrop81 <hacker814@hotmail.com>
2021-12-17 11:07:16 -0600

    Change starter pokemon

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -117,9 +117,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_ELEKID,
+    SPECIES_MAGBY,
+    SPECIES_SMOOCHUM,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
