94244a0b28577d8b8c221ac285daf3f7323cd758
lemondrop81 <hacker814@hotmail.com>
2021-12-17 11:08:11 -0600

    Faster HP drain

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -2218,7 +2218,7 @@ s32 MoveBattleBar(u8 battlerId, u8 healthboxSpriteId, u8 whichBar, u8 unused)
                     gBattleSpritesDataPtr->battleBars[battlerId].oldValue,
                     gBattleSpritesDataPtr->battleBars[battlerId].receivedValue,
                     &gBattleSpritesDataPtr->battleBars[battlerId].currValue,
-                    B_HEALTHBAR_PIXELS / 8, 1);
+                    B_HEALTHBAR_PIXELS / 8, max(gBattleSpritesDataPtr->battleBars[battlerId].maxValue / 32, 1));
     }
     else // exp bar
     {
