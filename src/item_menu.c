9f88d5d58193b0ff767c68c485d70561eb03b5b8
Eduardo Quezada D'Ottone <eduardo602002@gmail.com>
2021-12-18 15:35:24 -0300

    Fixed MAX_POCKET_ITEMS macro.

--- src/item_menu.c
+++ src/item_menu.c
@@ -63,7 +63,9 @@
                             max(BAG_BERRIES_COUNT,           \
                             max(BAG_ITEMS_COUNT,             \
                             max(BAG_KEYITEMS_COUNT,          \
-                                BAG_POKEBALLS_COUNT))))) + 1)
+                            max(BAG_TREASURES_COUNT,         \
+                            max(BAG_MEDICINE_COUNT,          \
+                                BAG_POKEBALLS_COUNT))))))) + 1)
 
 // Up to 8 item slots can be visible at a time
 #define MAX_ITEMS_SHOWN 8
