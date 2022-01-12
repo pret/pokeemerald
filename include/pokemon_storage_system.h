42777ef901ad81aa53e8a4ce08a32f8d9f866ccb
Lawrence Li <16lawrencel@gmail.com>
2022-01-11 22:04:30 -0800

    Decrease number of boxes

--- include/pokemon_storage_system.h
+++ include/pokemon_storage_system.h
@@ -1,7 +1,7 @@
 #ifndef GUARD_POKEMON_STORAGE_SYSTEM_H
 #define GUARD_POKEMON_STORAGE_SYSTEM_H
 
-#define TOTAL_BOXES_COUNT       14
+#define TOTAL_BOXES_COUNT       12
 #define IN_BOX_ROWS             5 // Number of rows, 6 Pokémon per row
 #define IN_BOX_COLUMNS          6 // Number of columns, 5 Pokémon per column
 #define IN_BOX_COUNT            (IN_BOX_ROWS * IN_BOX_COLUMNS)
