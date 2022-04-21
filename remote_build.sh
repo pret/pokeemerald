eeaefee12d6d80124aa26240ba5990bd71776b86
Ariel A <24759293+aarant@users.noreply.github.com>
2022-04-20 20:22:55 -0400

    Updated / genericized remote_build.sh.

--- remote_build.sh
+++ remote_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Git REmote Make
 # Push to SSH machine; build and scp build products back
 
 set -e
@@ -7,9 +8,10 @@ git_branch=$(git branch --show-current)
 # See https://unix.stackexchange.com/a/13472
 remote_host=$(git remote get-url build | sed -nr -e "s/ssh:\/\/(\w+@?\w*):.*/\1/p") # Extract remote host
 remote_path=$(git remote get-url build | sed -nr -e "s/ssh:\/\/\w+@?\w*://p") # Extract remote path
+make_cmd=$(git config --local remake.make)
+make_product=$(git config --local remake.src)
+make_dest=$(git config --local remake.dest)
 set -x
 git push build --force
-# `nproc` must be escaped so that it executes on the remote machine
-ssh $remote_host "cd $remote_path && git reset --hard && git checkout $git_branch && \
-                  echo \"Using \`nproc\` job(s)\" && make DDEBUG=1 -j\`nproc\`"
-scp "$remote_host:$remote_path/pokeemerald.gba" romhack.gba
+ssh $remote_host "cd $remote_path && git reset --hard && git checkout $git_branch && $make_cmd"
+scp "$remote_host:$remote_path/$make_product" $make_dest
