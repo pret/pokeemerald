7d42f9386151eabd2b471f591b28b55a519dbb62
AgustinGDLV <agustingomezdelavega@gmail.com>
2022-05-05 11:55:31 -0700

    added special overworld evo changes

--- src/pokemon.c
+++ src/pokemon.c
@@ -6755,6 +6755,28 @@ u16 GetEvolutionTargetSpecies(struct Pokemon *mon, u8 mode, u16 evolutionItem, s
             }
         }
         break;
+    // Overworld evolution without leveling; evolution method is being passed into the evolutionItem arg.
+    case EVO_MODE_OVERWORLD_SPECIAL:
+        for (i = 0; i < EVOS_PER_MON; i++)
+        {
+            switch (gEvolutionTable[species][i].method)
+            {
+            case EVO_SCRIPT_TRIGGER_DMG:
+                if (evolutionItem == EVO_SCRIPT_TRIGGER_DMG 
+                    && (GetMonData(mon, MON_DATA_MAX_HP, NULL) - GetMonData(mon, MON_DATA_HP, NULL) >= gEvolutionTable[species][i].param))
+                    targetSpecies = gEvolutionTable[species][i].targetSpecies;
+                break;
+            case EVO_DARK_SCROLL:
+                if (evolutionItem == EVO_DARK_SCROLL)
+                        targetSpecies = gEvolutionTable[species][i].targetSpecies;
+                    break;
+            case EVO_WATER_SCROLL:
+                    if (evolutionItem == EVO_WATER_SCROLL)
+                        targetSpecies = gEvolutionTable[species][i].targetSpecies;
+                    break;
+            }
+        }
+        break;
     }
 
     return targetSpecies;
@@ -8429,5 +8451,22 @@ static void RemoveIVIndexFromList(u8 *ivs, u8 selectedIv)
     {
         if (temp[i] != 0xFF)
             ivs[j++] = temp[i];
+    }
+}
+
+// Attempts to perform non-level/item related overworld evolutions; called by tryspecialevo command.
+void TrySpecialOverworldEvo(void)
+{
+    u8 i;
+    u8 evoMethod = gSpecialVar_0x8000;
+
+    for (i = 0; i < PARTY_SIZE; i++)
+    {
+        u16 targetSpecies = GetEvolutionTargetSpecies(&gPlayerParty[i], EVO_MODE_OVERWORLD_SPECIAL, evoMethod, SPECIES_NONE);
+        if (targetSpecies != SPECIES_NONE)
+        {
+            gCB2_AfterEvolution = CB2_ReturnToField;
+            BeginEvolutionScene(&gPlayerParty[i], targetSpecies, TRUE, i);
+        }   
     }
 }
