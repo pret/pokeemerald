6b217bbfcf6d13860d677d9dd0b24239b943312c
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-05-15 00:13:29 +0200

    fixes sprites on form pages not respecting the form (correct branch this time), credits @AsparagusEduardo

--- src/pokedex.c
+++ src/pokedex.c
@@ -5261,7 +5261,7 @@ static u32 GetPokedexMonPersonality(u16 species)
 
 u16 CreateMonSpriteFromNationalDexNumber(u16 nationalNum, s16 x, s16 y, u16 paletteSlot)
 {
-    nationalNum = NationalPokedexNumToSpecies(nationalNum);
+    nationalNum = NationalPokedexNumToSpeciesHGSS(nationalNum);
     return CreateMonPicSprite_HandleDeoxys(nationalNum, SHINY_ODDS, GetPokedexMonPersonality(nationalNum), TRUE, x, y, paletteSlot, TAG_NONE);
 }
 
