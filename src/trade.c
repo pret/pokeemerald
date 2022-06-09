7470a56d44fa05749c4832e7c18570978afc5163
LOuroboros <lunosouroboros@gmail.com>
2022-06-09 14:59:21 -0300

    Untangled CheckPartnersMonForRibbons

--- src/trade.c
+++ src/trade.c
@@ -4813,7 +4813,7 @@ static void CheckPartnersMonForRibbons(void)
 {
     u8 i;
     u8 numRibbons = 0;
-    for (i = 0; i < 12; i ++)
+    for (i = 0; i < (MON_DATA_UNUSED_RIBBONS - MON_DATA_CHAMPION_RIBBON); i ++)
     {
         numRibbons += GetMonData(&gEnemyParty[gSelectedTradeMonPositions[TRADE_PARTNER] % PARTY_SIZE], MON_DATA_CHAMPION_RIBBON + i);
     }
