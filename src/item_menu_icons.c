b885e01b39b0e17acd1b23305143f8218c9ecd47
Colton G. Rushton <colton51919@gmail.com>
2022-06-17 18:16:14 -0300

    Update src/item_menu_icons.c
    Co-authored-by: sneed <56992013+Sneed69@users.noreply.github.com>

--- src/item_menu_icons.c
+++ src/item_menu_icons.c
@@ -544,20 +544,20 @@ void AddBagItemIconSprite(u16 itemId, u8 id)
 
 void RemoveBagItemIconSprite(u8 id)
 {
-    #ifdef BUGFIX
-        u8 *spriteId = &gBagMenu->spriteIds[ITEMMENUSPRITE_ITEM];
+#ifdef BUGFIX
+    u8 *spriteId = &gBagMenu->spriteIds[ITEMMENUSPRITE_ITEM];
 
-    if (spriteId[id ^ 1] != SPRITE_NONE)
-        gSprites[spriteId[id ^ 1]].invisible = TRUE;
+if (spriteId[id ^ 1] != SPRITE_NONE)
+    gSprites[spriteId[id ^ 1]].invisible = TRUE;
 
-    if (spriteId[id] != SPRITE_NONE)
-    {
-        DestroySpriteAndFreeResources(&gSprites[spriteId[id]]);
-        spriteId[id] = SPRITE_NONE;
-    }
-    #else
-        RemoveBagSprite(id + ITEMMENUSPRITE_ITEM);
-    #endif
+if (spriteId[id] != SPRITE_NONE)
+{
+    DestroySpriteAndFreeResources(&gSprites[spriteId[id]]);
+    spriteId[id] = SPRITE_NONE;
+}
+#else
+    RemoveBagSprite(id + ITEMMENUSPRITE_ITEM);
+#endif
 }
 
 void CreateItemMenuSwapLine(void)
