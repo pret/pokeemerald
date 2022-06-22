87140b935262d0fd6c1266791bf22c9db9037655
Ariel A <24759293+aarant@users.noreply.github.com>
2022-06-21 20:03:27 -0400

    Fixed Regirock & Registeel puzzle freezing follower pokemon.

--- src/braille_puzzles.c
+++ src/braille_puzzles.c
@@ -1,5 +1,6 @@
 #include "global.h"
 #include "event_data.h"
+#include "event_object_movement.h"
 #include "field_camera.h"
 #include "field_effect.h"
 #include "script.h"
@@ -214,6 +215,7 @@ static void DoBrailleRegirockEffect(void)
     PlaySE(SE_BANG);
     FlagSet(FLAG_SYS_REGIROCK_PUZZLE_COMPLETED);
     ScriptContext2_Disable();
+    UnfreezeObjectEvents();
 }
 
 bool8 ShouldDoBrailleRegisteelEffect(void)
@@ -253,6 +255,7 @@ static void DoBrailleRegisteelEffect(void)
     PlaySE(SE_BANG);
     FlagSet(FLAG_SYS_REGISTEEL_PUZZLE_COMPLETED);
     ScriptContext2_Disable();
+    UnfreezeObjectEvents();
 }
 
 // theory: another commented out DoBrailleWait and Task_BrailleWait.
