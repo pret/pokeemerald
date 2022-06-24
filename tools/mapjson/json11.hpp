1d2ac5de67a42a00bfec3f0f9264565e672c8bb3
Colton G. Rushton <colton51919@gmail.com>
2022-06-24 10:37:07 -0300

    Remove Dropbox copyright in json11.hpp

--- tools/mapjson/json11.hpp
+++ tools/mapjson/json11.hpp
@@ -27,9 +27,7 @@
  * will be exact for +/- 275 years.)
  */
 
-/* Copyright (c) 2013 Dropbox, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
+/* Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
