7e32e6f56d668ca559c9a441002ba99e7444e7e5
tritenour <tritenour64@gmail.com>
2022-06-28 03:25:49 -0400

    increase save size

--- include/save.h
+++ include/save.h
@@ -3,8 +3,8 @@
 
 // Each 4 KiB flash sector contains 3968 bytes of actual data followed by a 128 byte footer.
 // Only 12 bytes of the footer are used.
-#define SECTOR_DATA_SIZE 3968
-#define SECTOR_FOOTER_SIZE 128
+#define SECTOR_DATA_SIZE 4084
+#define SECTOR_FOOTER_SIZE 12
 #define SECTOR_SIZE (SECTOR_DATA_SIZE + SECTOR_FOOTER_SIZE)
 
 #define NUM_SAVE_SLOTS 2
