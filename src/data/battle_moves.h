cec7e30ef10d7ba24c34f5c0969363fee2420d00
LOuroboros <lunosouroboros@gmail.com>
2022-07-15 01:51:36 -0300

    Update battle_moves.h

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -7850,7 +7850,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_SOUND,
         .split = SPLIT_SPECIAL,
     },
 
