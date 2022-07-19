99424e1aafe2239116ad91443124c81d5c08fb85
ghoulslash <41651341+ghoulslash@users.noreply.github.com>
2022-07-19 19:03:05 -0400

    Update include/battle.h
    Co-authored-by: Eduardo Quezada D'Ottone <eduardo602002@gmail.com>

--- include/battle.h
+++ include/battle.h
@@ -615,7 +615,7 @@ struct BattleStruct
     u8 quickClawBattlerId;
     struct StolenItem itemStolen[PARTY_SIZE];  // Player's team that had items stolen (two bytes per party member)
     u8 blunderPolicy:1; // should blunder policy activate
-    u8 swapDamageCategory:1;
+    u8 swapDamageCategory:1; // Photon Geyser, Shell Side Arm, Light That Burns the Sky
     u8 ballSpriteIds[2];    // item gfx, window gfx
     u8 stickyWebUser;
     u8 appearedInBattle; // Bitfield to track which Pokemon appeared in battle. Used for Burmy's form change
