61c95421eb75a391dfde3e0f6d408e6737ad9b8d
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-08-30 17:01:46 +0200

    Update src/event_object_movement.c
    Co-authored-by: LOuroboros <lunosouroboros@gmail.com>

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -4633,7 +4633,7 @@ u8 GetCollisionAtCoords(struct ObjectEvent *objectEvent, s16 x, s16 y, u32 dir)
     #if TX_DEBUG_SYSTEM == TRUE
     if (FlagGet(FLAG_SYS_NO_COLLISION))
         return COLLISION_NONE;
-    #endif //
+    #endif
 
     if (IsCoordOutsideObjectEventMovementRange(objectEvent, x, y))
         return COLLISION_OUTSIDE_RANGE;
