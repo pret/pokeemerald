3debf0442dc88b948899c73a3b98a0d76b1f45f7
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-08-30 18:02:23 +0200

    changed the locations of tx_debug_system related #defines to its own file 'constants/debug_config.h'

--- include/debug.h
+++ include/debug.h
@@ -1,11 +1,8 @@
 #ifndef GUARD_DEBUG_H
 #define GUARD_DEBUG_H
 
-#define TX_DEBUG_SYSTEM             TRUE    // Enables a overworld debug menu for changing flags, variables, giving pokemon and more, accessed by pressing R plus START.
-#define TX_DEBUG_SYSTEM_IN_MENU     FALSE   // Replaces the overworld debug menu button combination with a start menu entry (replaces the exit entry).
-
+#include "constants/debug_config.h"
 
 void Debug_ShowMainMenu(void);
-
 
 #endif // GUARD_DEBUG_H
