79c4fa560fb3d7ab59060f9bec638bc9238a5ad9
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-08-30 17:03:07 +0200

    Update src/start_menu.c
    Co-authored-by: LOuroboros <lunosouroboros@gmail.com>

--- src/start_menu.c
+++ src/start_menu.c
@@ -174,7 +174,7 @@ static const struct MenuAction sStartMenuItems[] =
     [MENU_ACTION_REST_FRONTIER]   = {gText_MenuRest,    {.u8_void = StartMenuSaveCallback}},
     [MENU_ACTION_RETIRE_FRONTIER] = {gText_MenuRetire,  {.u8_void = StartMenuBattlePyramidRetireCallback}},
     [MENU_ACTION_PYRAMID_BAG]     = {gText_MenuBag,     {.u8_void = StartMenuBattlePyramidBagCallback}},
-    [MENU_ACTION_DEBUG]     = {gText_MenuDebug,     {.u8_void = StartMenuDebugCallback}},
+    [MENU_ACTION_DEBUG]                 = {gText_MenuDebug, {.u8_void = StartMenuDebugCallback}},
 };
 
 static const struct BgTemplate sBgTemplates_LinkBattleSave[] =
