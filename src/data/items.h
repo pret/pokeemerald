0daeee83f386edf3f2ecd160928420846d07fbfc
Jaizu <jaizu85@gmail.com>
2022-09-11 18:06:05 +0200

    Fix Lure prices
    This change reflects the original prices used in Pokémon let's go pikachu/eevee

--- src/data/items.h
+++ src/data/items.h
@@ -1560,7 +1560,7 @@ const struct Item gItems[] =
     {
         .name = _("Lure"),
         .itemId = ITEM_LURE,
-        .price = 350,
+        .price = 400,
         .holdEffectParam = 100,
         .description = sLureDesc,
         .pocket = POCKET_ITEMS,
@@ -1574,7 +1574,7 @@ const struct Item gItems[] =
     {
         .name = _("Super Lure"),
         .itemId = ITEM_SUPER_LURE,
-        .price = 500,
+        .price = 700,
         .holdEffectParam = 200,
         .description = sSuperLureDesc,
         .pocket = POCKET_ITEMS,
@@ -1588,7 +1588,7 @@ const struct Item gItems[] =
     {
         .name = _("Max Lure"),
         .itemId = ITEM_MAX_LURE,
-        .price = 700,
+        .price = 900,
         .holdEffectParam = 250,
         .description = sMaxLureDesc,
         .pocket = POCKET_ITEMS,
