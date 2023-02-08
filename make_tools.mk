f933ad31837cf34e4cd968f74659d1867621a4ce
Martin Griffin <martin.griffin@algosport.co.uk>
2023-02-08 17:35:28 +0000

    Build patchelf and hydra only for check

--- make_tools.mk
+++ make_tools.mk
@@ -1,7 +1,6 @@
-
 MAKEFLAGS += --no-print-directory
 
-TOOLDIRS := $(filter-out tools/mgba tools/agbcc tools/binutils,$(wildcard tools/*))
+TOOLDIRS := tools/aif2pcm tools/bin2c tools/gbafix tools/gbagfx tools/jsonproc tools/mapjson tools/mid2agb tools/preproc tools/ramscrgen tools/rsfont tools/scaninc
 
 .PHONY: all $(TOOLDIRS)
 
