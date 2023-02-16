978d34295356e255eea3bcdfca18122ce97c42dd
DizzyEggg <jajkodizzy@wp.pl>
2023-02-16 15:26:26 +0100

    use NUM_BATTLE_SIDES

--- include/battle.h
+++ include/battle.h
@@ -653,7 +653,7 @@ struct BattleStruct
     u8 targetsDone[MAX_BATTLERS_COUNT]; // Each battler as a bit.
     u16 overwrittenAbilities[MAX_BATTLERS_COUNT];    // abilities overwritten during battle (keep separate from battle history in case of switching)
     bool8 allowedToChangeFormInWeather[PARTY_SIZE][2]; // For each party member and side, used by Ice Face.
-    u8 battleBondTransformed[2]; // Bitfield for each party.
+    u8 battleBondTransformed[NUM_BATTLE_SIDES]; // Bitfield for each party.
 };
 
 #define F_DYNAMIC_TYPE_1 (1 << 6)
