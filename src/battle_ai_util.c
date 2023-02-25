fc5c93fd83efaf64129a32f52646d7922177e0a8
Alex <alexthenotes@gmail.com>
2023-02-25 12:15:40 +0100

    exclude status move type check

--- src/battle_ai_util.c
+++ src/battle_ai_util.c
@@ -802,7 +802,16 @@ s32 AI_CalcDamage(u16 move, u8 battlerAtk, u8 battlerDef, u8 *typeEffectiveness,
     }
     else
     {
-        effectivenessMultiplier = CalcTypeEffectivenessMultiplier(move, moveType, battlerAtk, battlerDef, FALSE);
+        #if B_GLARE_GHOST == GEN_3
+        else if (move == MOVE_GLARE || move == MOVE_THUNDER_WAVE)
+            effectivenessMultiplier = CalcTypeEffectivenessMultiplier(move, moveType, battlerAtk, battlerDef, FALSE);
+        else
+#       endif
+        if (move == MOVE_THUNDER_WAVE)
+            effectivenessMultiplier = CalcTypeEffectivenessMultiplier(move, moveType, battlerAtk, battlerDef, FALSE);
+        else
+            effectivenessMultiplier = UQ_4_12(1.0);
+
         dmg = 0;
     }
 
