c34e2f6868ad200958dfe41618d422661b3eb558
moonlight-SYY <64934042+moonlight-SYY@users.noreply.github.com>
2023-02-27 21:05:23 -0500

    Update starter_choose.c

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_CHIKORITA,
+    SPECIES_CYNDAQUIL,
+    SPECIES_TOTODILE,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
