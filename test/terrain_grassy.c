1da65c40517a64a4ef0695b27d3aa3d26ecb2e9f
Eduardo Quezada D'Ottone <eduardo602002@gmail.com>
2023-03-23 22:33:47 -0300

    Fixed Grassy Terrain not healing

---
+++ test/terrain_grassy.c
@@ -0,0 +1,16 @@
+#include "global.h"
+#include "test_battle.h"
+
+SINGLE_BATTLE_TEST("Grassy Terrain recovers 1/16th HP at end of turn")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { MaxHP(100); HP(1); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_GRASSY_TERRAIN); }
+    } SCENE {
+        s32 maxHP = GetMonData(&PLAYER_PARTY[0], MON_DATA_MAX_HP);
+        MESSAGE("Wobbuffet is healed by the grassy terrain!");
+        HP_BAR(player, damage: -maxHP / 16);
+    }
+}
