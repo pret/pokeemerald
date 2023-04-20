6c7f5881e453631a1f336618b7e0120c33b7f140
citrusbolt <32605812+citrusbolt@users.noreply.github.com>
2023-04-20 08:05:32 -0600

    Add two EV-related configs (#2825)

--- include/config/item.h
+++ include/config/item.h
@@ -2,12 +2,13 @@
 #define GUARD_CONFIG_ITEM_H
 
 // Item config
-#define I_SHINY_CHARM_REROLLS   3           // Amount of re-rolls if the player has the Shiny Charm. Set to 0 to disable Shiny Charm's effects.
-#define I_KEY_FOSSILS           GEN_LATEST  // In Gen4+, all Gen 3 fossils became regular items.
-#define I_KEY_ESCAPE_ROPE       GEN_LATEST  // In Gen8, Escape Rope became a Key Item. Keep in mind, this will make it free to buy in marts.
-#define I_HEALTH_RECOVERY       GEN_LATEST  // In Gen7+, certain healing items recover a different amount of HP than they used to.
-#define I_SITRUS_BERRY_HEAL     GEN_LATEST  // In Gen4+, Sitrus Berry was changed from healing 30 HP to healing 25% of Max HP.
-#define I_VITAMIN_EV_CAP        GEN_LATEST  // In Gen8, the Vitamins no longer have a cap of 100 EV per stat.
+#define I_SHINY_CHARM_REROLLS       3           // Amount of re-rolls if the player has the Shiny Charm. Set to 0 to disable Shiny Charm's effects.
+#define I_KEY_FOSSILS               GEN_LATEST  // In Gen4+, all Gen 3 fossils became regular items.
+#define I_KEY_ESCAPE_ROPE           GEN_LATEST  // In Gen8, Escape Rope became a Key Item. Keep in mind, this will make it free to buy in marts.
+#define I_HEALTH_RECOVERY           GEN_LATEST  // In Gen7+, certain healing items recover a different amount of HP than they used to.
+#define I_SITRUS_BERRY_HEAL         GEN_LATEST  // In Gen4+, Sitrus Berry was changed from healing 30 HP to healing 25% of Max HP.
+#define I_VITAMIN_EV_CAP            GEN_LATEST  // In Gen8+, the Vitamins no longer have a cap of 100 EV per stat.
+#define I_BERRY_EV_JUMP             GEN_LATEST  // In Gen4 only, EV-lowering Berries lower a stat's EV to 100 if it is above 100.
 
 // TM config
 #define I_REUSABLE_TMS          FALSE       // In Gen5-8, TMs are reusable. Setting this to TRUE will make all vanilla TMs reusable, though they can also be cherry-picked by setting their importance to 1.
