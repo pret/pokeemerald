15ce9eb71cde27b9142bb20c067fef19333ca852
CallmeEchoo <samedding@protonmail.com>
2023-05-01 18:53:06 +0200

    fix dire hit message mismatch

--- test/item_effect_set_focus_energy.c
+++ test/item_effect_set_focus_energy.c
@@ -3,10 +3,6 @@
 
 SINGLE_BATTLE_TEST("Dire Hit increases a battler's critical hit chance by 2 stages")
 {
-    /* KNOWN_FAILING due to message mismatch for "Wobbuffet used Dire Hit to get pumped!"
-     * As of the time of writing, this test passes if the above mentioned message is removed
-     * For more details refer to description of PR #2940 */
-    KNOWN_FAILING;
     ASSUME(B_CRIT_CHANCE >= GEN_7);
     PASSES_RANDOMLY(1, 2, RNG_CRITICAL_HIT);
     GIVEN {
