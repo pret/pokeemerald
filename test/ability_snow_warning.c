6758f6ebe755e6961d365ec2ecdfffea6f9c6f2f
CallmeEchoo <samedding@protonmail.com>
2023-05-02 16:16:58 +0200

    add some snow related tests

---
+++ test/ability_snow_warning.c
@@ -0,0 +1,20 @@
+#include "global.h"
+#include "test_battle.h"
+
+SINGLE_BATTLE_TEST("Snow Warning summons hail(snow)")
+{
+    GIVEN {
+        PLAYER(SPECIES_ABOMASNOW) { Ability(ABILITY_SNOW_WARNING); };
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN {}
+    } SCENE {
+        #if B_SNOW_WARNING < GEN_9
+        MESSAGE("It started to hail!");
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_HAIL_CONTINUES);
+        #elif B_SNOW_WARNING >= GEN_9
+        MESSAGE("It started to snow!");
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_SNOW_CONTINUES);
+        #endif
+    }
+}
