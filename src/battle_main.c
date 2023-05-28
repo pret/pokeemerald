7edd1a45a4d119e3adf746850fb1be3487cd4bc7
Alex <alexthenotes@gmail.com>
2023-05-28 11:31:22 +0200

    restore items in wild battles

--- src/battle_main.c
+++ src/battle_main.c
@@ -5404,7 +5404,6 @@ static void HandleEndTurn_FinishBattle(void)
         BeginFastPaletteFade(3);
         FadeOutMapMusic(5);
     #if B_TRAINERS_KNOCK_OFF_ITEMS == TRUE || B_RESTORE_HELD_BATTLE_ITEMS == TRUE
-        if (gBattleTypeFlags & BATTLE_TYPE_TRAINER)
             TryRestoreHeldItems();
     #endif
         for (i = 0; i < PARTY_SIZE; i++)
