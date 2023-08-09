f6e6a1f3839b82e547bc54c5163132927b3a3f15
Martin Griffin <martinrgriffin@gmail.com>
2023-08-09 15:06:03 +0100

    Initialize save to empty in test runner (#3215)

--- test/test_runner.c
+++ test/test_runner.c
@@ -2,6 +2,7 @@
 #include "global.h"
 #include "characters.h"
 #include "gpu_regs.h"
+#include "load_save.h"
 #include "main.h"
 #include "malloc.h"
 #include "random.h"
@@ -113,6 +114,10 @@ void CB2_TestRunner(void)
             gTestRunnerState.exitCode = 2;
             return;
         }
+
+        MoveSaveBlocks_ResetHeap();
+        ClearSav1();
+        ClearSav2();
 
         gIntrTable[7] = Intr_Timer2;
 
