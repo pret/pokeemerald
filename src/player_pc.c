84ddffeadbea78c0628371b89045c639482b9949
Thomas Winwood <twwinwood@gmail.com>
2023-10-02 19:13:33 +0100

    We'll just pretend that didn't happen okay

--- src/player_pc.c
+++ src/player_pc.c
@@ -224,8 +224,8 @@ static const struct MenuAction sItemStorage_MenuActions[] =
 
 static const u16 sNewGamePCItems[][2] =
 {
-    [ITEM_POTION, 1],
-    [ITEM_NONE, 0]
+    { ITEM_POTION, 1 },
+    { ITEM_NONE, 0 }
 };
 
 const struct MenuAction gMailboxMailOptions[] =
