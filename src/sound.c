11e840795801310b5bbfaaf8ff551b00998a3662
Philipp AUER <SBird1337@users.noreply.github.com>
2023-11-09 13:04:29 +0100

    [audio] resample oversampled cries to 13379Hz (#3359)
    Co-authored-by: sbird <sbird@no.tld>
    Co-authored-by: Eduardo Quezada D'Ottone <eduardo602002@gmail.com>

--- src/sound.c
+++ src/sound.c
@@ -375,7 +375,7 @@ void PlayCryInternal(u16 species, s8 pan, s8 volume, u8 priority, u8 mode)
 
     // Set default values
     // May be overridden depending on mode.
-    length = 140;
+    length = 210;
     reverse = FALSE;
     release = 0;
     pitch = 15360;
