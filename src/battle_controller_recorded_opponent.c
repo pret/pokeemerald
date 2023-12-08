24b5901a937847e75d9a01e4dbcab174cd23dcce
AreaZeroArven <122058867+AreaZeroArven@users.noreply.github.com>
2023-12-08 18:38:00 -0500

    Update battle_controller_recorded_opponent.c

--- src/battle_controller_recorded_opponent.c
+++ src/battle_controller_recorded_opponent.c
@@ -1022,7 +1022,7 @@ static void SetRecordedOpponentMonData(u8 monId)
         SetMonData32(&gEnemyParty[monId], MON_DATA_PERSONALITY, &gBattleBufferA[gActiveBattler][3]);
         break;
     case REQUEST_CHECKSUM_BATTLE:
-        SetMonData32(&gEnemyParty[monId], MON_DATA_CHECKSUM, &gBattleBufferA[gActiveBattler][3]);
+        SetMonData16(&gEnemyParty[monId], MON_DATA_CHECKSUM, &gBattleBufferA[gActiveBattler][3]);
         break;
     case REQUEST_STATUS_BATTLE:
         SetMonData32(&gEnemyParty[monId], MON_DATA_STATUS, &gBattleBufferA[gActiveBattler][3]);
