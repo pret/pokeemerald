22280b47788d0e2bef9b8953cbab298c45f4256f
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2023-12-11 19:20:14 +0100

    Fixes z-move priority (#3690)
    * Fixes z-move priority
    
    * consider prankster affected z moves
    
    * consider only damaging moves

--- src/battle_main.c
+++ src/battle_main.c
@@ -4691,6 +4691,9 @@ s8 GetMovePriority(u32 battler, u16 move)
     s8 priority;
     u16 ability = GetBattlerAbility(battler);
 
+    if (gBattleStruct->zmove.toBeUsed[battler] && gBattleMoves[move].power != 0)
+        move = gBattleStruct->zmove.toBeUsed[battler];
+
     priority = gBattleMoves[move].priority;
     if (ability == ABILITY_GALE_WINGS
     #if B_GALE_WINGS >= GEN_7
