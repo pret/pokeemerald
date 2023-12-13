8b2d9d200860a76bab4056e424675401e6439603
Drew Hughes <drew.james.hughes@gmail.com>
2023-12-13 15:33:18 -0500

    Updated sprites and positions

--- src/debug.c
+++ src/debug.c
@@ -3490,7 +3490,7 @@ static void DebugAction_Fill_PCBoxes_Fast(u8 taskId) //Credit: Sierraffinity
     int boxId, boxPosition;
     u32 personality;
     struct BoxPokemon boxMon;
-    u16 species = SPECIES_BULBASAUR;
+    u16 species = SPECIES_SNIVY; //SPECIES_BULBASAUR;
     u8 speciesName[POKEMON_NAME_LENGTH + 1];
 
     personality = Random32();
