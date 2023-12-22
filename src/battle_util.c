38d67d90511edf0e7b760852859e8c6e58f04573
ghoulslash <41651341+ghoulslash@users.noreply.github.com>
2023-12-22 10:24:46 -0500

    fix exp gain when defeating two opponents at once (#3798)
    Co-authored-by: ghoulslash <pokevoyager0@gmail.com>

--- src/battle_util.c
+++ src/battle_util.c
@@ -3338,7 +3338,8 @@ bool32 HandleFaintedMonActions(void)
                 && gCurrentTurnActionNumber != gBattlersCount)
             {
                 gAbsentBattlerFlags |= gBitTable[gBattlerFainted];
-                return FALSE;
+                if (gBattleStruct->faintedActionsState != 1)
+                    return FALSE;
             }
             break;
         case 3:
