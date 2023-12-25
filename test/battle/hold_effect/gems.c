c47a20a446f71e16d7a0eaaf7919e6b2870af8dc
Bassoonian <iasperbassoonian@gmail.com>
2023-12-25 01:14:01 +0100

    Fix gem boost description + test (#3817)

--- test/battle/hold_effect/gems.c
+++ test/battle/hold_effect/gems.c
@@ -32,7 +32,7 @@ SINGLE_BATTLE_TEST("Gem boost is only applied once")
     s16 normalHit;
 
     GIVEN {
-        ASSUME(I_GEM_BOOST_POWER >= GEN_5);
+        ASSUME(I_GEM_BOOST_POWER >= GEN_6);
         PLAYER(SPECIES_WOBBUFFET) { Item(ITEM_NORMAL_GEM); };
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
