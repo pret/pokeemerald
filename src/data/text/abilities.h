bc3737772aa688edfd7712d6451c1d3707af9328
Bassoonian <iasperbassoonian@gmail.com>
2023-12-28 12:33:29 +0100

    Define new abilities (#3838)
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- src/data/text/abilities.h
+++ src/data/text/abilities.h
@@ -297,6 +297,10 @@ static const u8 sEmbodyAspectWellspringDescription[] = _("Raises Sp. Def.");
 static const u8 sEmbodyAspectCornerstoneDescription[] = _("Raises Defense.");
 static const u8 sToxicChainDescription[] = _("Moves can poison.");
 static const u8 sSupersweetSyrupDescription[] = _("Lowers the foe's Speed.");
+static const u8 sTeraShiftDescription[] = _("Terasteralizes upon entry.");
+static const u8 sTeraShellDescription[] = _("Resistant to types at full HP.");
+static const u8 sTeraformZeroDescription[] = _("Removes weather and terrain.");
+static const u8 sPoisonPuppeteerDescription[] = _("Confuses poisoned foes.");
 
 #if B_EXPANDED_ABILITY_NAMES == TRUE
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
@@ -608,6 +612,10 @@ const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
     [ABILITY_EMBODY_ASPECT_CORNERSTONE] = _("Embody Aspect"),
     [ABILITY_TOXIC_CHAIN] = _("Toxic Chain"),
     [ABILITY_SUPERSWEET_SYRUP] = _("Supersweet Syrup"),
+    [ABILITY_TERA_SHIFT] = _("Tera Shift"),
+    [ABILITY_TERA_SHELL] = _("Tera Shell"),
+    [ABILITY_TERAFORM_ZERO] = _("Teraform Zero"),
+    [ABILITY_POISON_PUPPETEER] = _("Poison Puppeteer"),
 };
 #else   // 12 characters
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
@@ -919,6 +927,10 @@ const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
     [ABILITY_EMBODY_ASPECT_CORNERSTONE] = _("EmbodyAspect"),
     [ABILITY_TOXIC_CHAIN] = _("Toxic Chain"),
     [ABILITY_SUPERSWEET_SYRUP] = _("SuprswtSyrup"),
+    [ABILITY_TERA_SHIFT] = _("Tera Shift"),
+    [ABILITY_TERA_SHELL] = _("Tera Shell"),
+    [ABILITY_TERAFORM_ZERO] = _("TeraformZero"),
+    [ABILITY_POISON_PUPPETEER] = _("PoisnPuppter"),
 };
 #endif
 
@@ -1231,4 +1243,8 @@ const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
     [ABILITY_EMBODY_ASPECT_CORNERSTONE] = sEmbodyAspectCornerstoneDescription,
     [ABILITY_TOXIC_CHAIN] = sToxicChainDescription,
     [ABILITY_SUPERSWEET_SYRUP] = sSupersweetSyrupDescription,
+    [ABILITY_TERA_SHIFT] = sTeraShiftDescription,
+    [ABILITY_TERA_SHELL] = sTeraShellDescription,
+    [ABILITY_TERAFORM_ZERO] = sTeraformZeroDescription,
+    [ABILITY_POISON_PUPPETEER] = sPoisonPuppeteerDescription,
 };
