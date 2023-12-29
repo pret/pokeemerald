16a3954f0ea31b5beca36d335a269ad4c017fd7f
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2023-12-29 14:12:17 +0100

    Add MOVES_COUNT and NUM_SPECIES to RHH rom header (#3831)
    * Add MOVES_COUNT and NUM_SPECIES to RHH rom header

--- src/rom_header_rhh.c
+++ src/rom_header_rhh.c
@@ -1,5 +1,7 @@
 #include "global.h"
 #include "constants/expansion.h"
+#include "constants/moves.h"
+#include "constants/species.h"
 
 // Similar to the GF ROM header, this struct allows external programs to
 // detect details about Expansion.
@@ -14,6 +16,8 @@ struct RHHRomHeader
     /*0x07*/ u8 expansionVersionMinor;
     /*0x08*/ u8 expansionVersionPatch;
     /*0x09*/ u8 expansionVersionFlags;
+    /*0x0C*/ u32 movesCount;
+    /*0x10*/ u32 numSpecies;
 };
 
 static const struct RHHRomHeader sRHHRomHeader =
@@ -23,4 +27,6 @@ static const struct RHHRomHeader sRHHRomHeader =
     .expansionVersionMinor = EXPANSION_VERSION_MINOR,
     .expansionVersionPatch = EXPANSION_VERSION_PATCH,
     .expansionVersionFlags = (EXPANSION_TAGGED_RELEASE << 0),
+    .movesCount = MOVES_COUNT,
+    .numSpecies = NUM_SPECIES,
 };
