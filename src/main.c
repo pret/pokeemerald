f24deb930131819e5ff3be401742b41162e4829a
aronson <i@pingas.org>
2024-01-02 07:46:01 -0600

    Allocate initialized sections for EWRAM and IWRAM (#3877)

--- src/main.c
+++ src/main.c
@@ -93,11 +93,6 @@ void EnableVCountIntrAtLine150(void);
 
 void AgbMain()
 {
-    // Modern compilers are liberal with the stack on entry to this function,
-    // so RegisterRamReset may crash if it resets IWRAM.
-#if !MODERN
-    RegisterRamReset(RESET_ALL);
-#endif //MODERN
     *(vu16 *)BG_PLTT = RGB_WHITE; // Set the backdrop to white on startup
     InitGpuRegManager();
     REG_WAITCNT = WAITCNT_PREFETCH_ENABLE | WAITCNT_WS0_S_1 | WAITCNT_WS0_N_3;
