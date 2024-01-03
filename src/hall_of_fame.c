973dae73be0df69b1b4f2ee6d133edd9ee9e22e2
Bassoonian <iasperbassoonian@gmail.com>
2024-01-03 18:48:22 +0000

    Fix HoF dex numbers (#3901)

--- src/hall_of_fame.c
+++ src/hall_of_fame.c
@@ -1128,6 +1128,12 @@ static void HallOfFame_PrintMonInfo(struct HallofFameMon* currMon, u8 unused1, u
         dexNumber = SpeciesToPokedexNum(currMon->species);
         if (dexNumber != 0xFFFF)
         {
+            if (IsNationalPokedexEnabled())
+            {
+                stringPtr[0] = (dexNumber / 1000) + CHAR_0;
+                stringPtr++;
+                dexNumber %= 1000;
+            }
             stringPtr[0] = (dexNumber / 100) + CHAR_0;
             stringPtr++;
             dexNumber %= 100;
