fd5d5cd055c01dda6b76860cc8dda1933afc2478
Eduardo Quezada D'Ottone <eduardo602002@gmail.com>
2024-01-19 05:28:51 -0300

    Fixed incorrect family toggle preproc blocks (#4024)
    * Fixed incorrect family toggle preproc blocks
    
    * More fixes

--- src/data/pokemon_graphics/front_pic_anims.h
+++ src/data/pokemon_graphics/front_pic_anims.h
@@ -279,7 +279,6 @@ static const union AnimCmd sAnim_Raticate_1[] =
 
 #if P_ALOLAN_FORMS
 PLACEHOLDER_ANIM_SINGLE_FRAME(RattataAlolan);
-
 PLACEHOLDER_ANIM_SINGLE_FRAME(RaticateAlolan);
 #endif //P_ALOLAN_FORMS
 #endif //P_FAMILY_RATTATA
@@ -353,12 +352,15 @@ static const union AnimCmd sAnim_Pikachu_1[] =
     ANIMCMD_END,
 };
 
+#if P_COSPLAY_PIKACHU_FORMS
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuCosplay);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuRockStar);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuBelle);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuPopStar);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuPhD);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuLibre);
+#endif //P_COSPLAY_PIKACHU_FORMS
+#if P_CAP_PIKACHU_FORMS
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuOriginalCap);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuHoennCap);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuSinnohCap);
@@ -367,6 +369,7 @@ PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuKalosCap);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuAlolaCap);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuPartnerCap);
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuWorldCap);
+#endif //P_CAP_PIKACHU_FORMS
 
 #if P_GIGANTAMAX_FORMS
 PLACEHOLDER_ANIM_SINGLE_FRAME(PikachuGigantamax);
@@ -4685,7 +4688,9 @@ static const union AnimCmd sAnim_Milotic_1[] =
     ANIMCMD_FRAME(0, 15),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_FEEBAS
 
+#if P_FAMILY_CASTFORM
 static const union AnimCmd sAnim_CastformNormal_1[] =
 {
     ANIMCMD_FRAME(0, 12),
@@ -4727,7 +4732,7 @@ static const union AnimCmd sAnim_CastformSnowy_1[] =
     ANIMCMD_FRAME(0, 12),
     ANIMCMD_END,
 };
-#endif //P_FAMILY_FEEBAS
+#endif //P_FAMILY_CASTFORM
 
 #if P_FAMILY_KECLEON
 static const union AnimCmd sAnim_Kecleon_1[] =
@@ -5773,13 +5778,16 @@ static const union AnimCmd sAnim_Chatot_1[] =
     ANIMCMD_FRAME(0, 5),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_CHATOT
+
+#if P_FAMILY_SPIRITOMB
 static const union AnimCmd sAnim_Spiritomb_1[] =
 {
     ANIMCMD_FRAME(1, 20),
     ANIMCMD_FRAME(0, 10),
     ANIMCMD_END,
 };
-#endif //P_FAMILY_CHATOT
+#endif //P_FAMILY_SPIRITOMB
 
 #if P_FAMILY_GIBLE
 static const union AnimCmd sAnim_Gible_1[] =
@@ -7844,7 +7852,9 @@ static const union AnimCmd sAnim_Virizion_1[] =
     ANIMCMD_FRAME(0, 20),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_VIRIZION
 
+#if P_FAMILY_TORNADUS
 static const union AnimCmd sAnim_TornadusIncarnate_1[] =
 {
     ANIMCMD_FRAME(1, 2),
@@ -7881,7 +7891,9 @@ static const union AnimCmd sAnim_TornadusTherian_1[] =
     ANIMCMD_FRAME(0, 5),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_TORNADUS
 
+#if P_FAMILY_THUNDURUS
 static const union AnimCmd sAnim_ThundurusIncarnate_1[] =
 {
     ANIMCMD_FRAME(1, 2),
@@ -7929,7 +7941,7 @@ static const union AnimCmd sAnim_ThundurusTherian_1[] =
     ANIMCMD_FRAME(0, 5),
     ANIMCMD_END,
 };
-#endif //P_FAMILY_VIRIZION
+#endif //P_FAMILY_THUNDURUS
 
 #if P_FAMILY_RESHIRAM
 static const union AnimCmd sAnim_Reshiram_1[] =
@@ -7947,7 +7959,9 @@ static const union AnimCmd sAnim_Zekrom_1[] =
     ANIMCMD_FRAME(0, 5),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_ZEKROM
 
+#if P_FAMILY_LANDORUS
 static const union AnimCmd sAnim_LandorusIncarnate_1[] =
 {
     ANIMCMD_FRAME(1, 2),
@@ -7984,7 +7998,7 @@ static const union AnimCmd sAnim_LandorusTherian_1[] =
     ANIMCMD_FRAME(0, 15),
     ANIMCMD_END,
 };
-#endif //P_FAMILY_ZEKROM
+#endif //P_FAMILY_LANDORUS
 
 #if P_FAMILY_KYUREM
 static const union AnimCmd sAnim_Kyurem_1[] =
@@ -8013,7 +8027,9 @@ static const union AnimCmd sAnim_KyuremBlack_1[] =
     ANIMCMD_END,
 };
 #endif //P_FUSION_FORMS
+#endif //P_FAMILY_KYUREM
 
+#if P_FAMILY_KELDEO
 static const union AnimCmd sAnim_KeldeoOrdinary_1[] =
 {
     ANIMCMD_FRAME(1, 32),
@@ -8028,7 +8044,9 @@ static const union AnimCmd sAnim_KeldeoResolute_1[] =
     ANIMCMD_FRAME(0, 10),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_KELDEO
 
+#if P_FAMILY_MELOETTA
 static const union AnimCmd sAnim_MeloettaAria_1[] =
 {
     ANIMCMD_FRAME(0, 15),
@@ -8051,7 +8069,7 @@ static const union AnimCmd sAnim_MeloettaPirouette_1[] =
     ANIMCMD_FRAME(0, 15),
     ANIMCMD_END,
 };
-#endif //P_FAMILY_KYUREM
+#endif //P_FAMILY_MELOETTA
 
 #if P_FAMILY_GENESECT
 static const union AnimCmd sAnim_Genesect_1[] =
@@ -9067,10 +9085,12 @@ static const union AnimCmd sAnim_LycanrocDusk_1[] =
     ANIMCMD_FRAME(0, 5),
     ANIMCMD_END,
 };
+#endif //P_FAMILY_ROCKRUFF
 
+#if P_FAMILY_WISHIWASHI
 PLACEHOLDER_ANIM_SINGLE_FRAME(WishiwashiSolo);
 PLACEHOLDER_ANIM_SINGLE_FRAME(WishiwashiSchool);
-#endif //P_FAMILY_ROCKRUFF
+#endif //P_FAMILY_WISHIWASHI
 
 #if P_FAMILY_MAREANIE
 PLACEHOLDER_ANIM_SINGLE_FRAME(Mareanie);
@@ -10189,6 +10209,8 @@ SINGLE_ANIMATION(PikachuBelle);
 SINGLE_ANIMATION(PikachuPopStar);
 SINGLE_ANIMATION(PikachuPhD);
 SINGLE_ANIMATION(PikachuLibre);
+#endif //P_COSPLAY_PIKACHU_FORMS
+#if P_CAP_PIKACHU_FORMS
 SINGLE_ANIMATION(PikachuOriginalCap);
 SINGLE_ANIMATION(PikachuHoennCap);
 SINGLE_ANIMATION(PikachuSinnohCap);
