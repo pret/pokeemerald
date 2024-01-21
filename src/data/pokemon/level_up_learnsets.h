b99ec5d43cb9535592f6bc3cc077906fef18b642
Bassoonian <iasperbassoonian@gmail.com>
2024-01-22 00:07:19 +0100

    Update level_up_learnsets.h (#4046)

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -3500,7 +3500,7 @@ static const struct LevelUpMove sKingdraLevelUpLearnset[] = {
     LEVEL_UP_MOVE(60, MOVE_HYDRO_PUMP),
     LEVEL_UP_END
 };
-#endif //P_GEN_4_CROSS_EVOS
+#endif //P_GEN_2_CROSS_EVOS
 #endif //P_FAMILY_HORSEA
 
 #if P_FAMILY_GOLDEEN
