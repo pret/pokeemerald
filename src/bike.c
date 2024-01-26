1e5fd80b7e268138865e0efeb61eed4c58ae7e01
Ariel A <24759293+aarant@users.noreply.github.com>
2024-01-26 18:48:55 -0500

    Revert "Allowed running inside buildings."
    This reverts commit 4e3c2d84911614cfa917e2fa5d0fe1511fa5e41b.

--- src/bike.c
+++ src/bike.c
@@ -1053,7 +1053,7 @@ void Bike_HandleBumpySlopeJump(void)
 
 bool32 IsRunningDisallowed(u8 metatile)
 {
-    if (IsRunningDisallowedByMetatile(metatile) == TRUE)
+    if (!gMapHeader.allowRunning || IsRunningDisallowedByMetatile(metatile) == TRUE)
         return TRUE;
     else
         return FALSE;
