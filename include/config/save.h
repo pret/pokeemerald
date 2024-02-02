b8b7dd304bea82ad22f2c4f09db26870b01e0a1a
Bassoonian <iasperbassoonian@gmail.com>
2024-02-02 16:38:33 +0100

    Add final config documentation

--- include/config/save.h
+++ include/config/save.h
@@ -1,26 +1,22 @@
 #ifndef GUARD_CONFIG_SAVE_H
 #define GUARD_CONFIG_SAVE_H
 
-// free saveblock 1 defines
+// SaveBlock1 configs
 #define FREE_EXTRA_SEEN_FLAGS               FALSE   // Free up unused Pokédex seen flags (52 bytes).
 #define FREE_TRAINER_HILL                   FALSE   // Frees up Trainer Hill data (28 bytes). WARNING THIS HAS BEEN SHOWN TO BREAK MULTI BATTLES
-#define FREE_MYSTERY_EVENT_BUFFERS          FALSE   //frees up mystery event and ramScript. roughly 1880 bytes       Needs FREE_BATTLE_TOWER_E_READER
+#define FREE_MYSTERY_EVENT_BUFFERS          FALSE   // Frees up ramScript (1104 bytes).
 #define FREE_MATCH_CALL                     FALSE   // Frees up match call and rematch data. (104 bytes).
 #define FREE_UNION_ROOM_CHAT                FALSE   // Frees up union room chat (212 bytes).
 #define FREE_ENIGMA_BERRY                   FALSE   // Frees up E-Reader Enigma Berry data (52 bytes).
 #define FREE_LINK_BATTLE_RECORDS            FALSE   // Frees up link battle record data (88 bytes).
 #define FREE_MYSTERY_GIFT                   FALSE   // Frees up Mystery Gift data (876 bytes). WARNING THIS HAS BEEN SHOWN TO BREAK THE POKÉ MARTS' QUESTIONNAIRE
-                                            // saveblock1 total: 1846 bytes
-//free saveblock 2 defines
+                                            // SaveBlock1 total: 2516 bytes
+// SaveBlock2 configs
 #define FREE_BATTLE_TOWER_E_READER          FALSE   // Frees up Battle Tower E-Reader data (188 bytes).
 #define FREE_POKEMON_JUMP                   FALSE   // Frees up Pokémon Jump data (16 bytes).
 #define FREE_RECORD_MIXING_HALL_RECORDS     FALSE   // Frees up hall records for record mixing (1032 bytes).
-                                        // saveblock2 total: 1236 bytes
+                                            // SaveBlock2 total: 1166 bytes
 
-                                        //grand total: 3082
-
-//#if (FREE_MYSTERY_EVENT_BUFFERS == TRUE) && (FREE_BATTLE_TOWER_E_READER == FALSE)
-//#error "FREE_BATTLE_TOWER_E_READER must be enabled for FREE_MYSTERY_EVENT_BUFFERS to work"
-//#endif
+                                            // Grand Total: 3682
 
 #endif // GUARD_CONFIG_SAVE_H
