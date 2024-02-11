3598a187031c7b93c760919bc2bc8826c638609f
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2024-02-11 10:40:30 +0100

    Adds config for Soundproof change during Uproar status (#4174)

---
+++ test/battle/move_effect/uproar.c
@@ -0,0 +1,28 @@
+#include "global.h"
+#include "test/battle.h"
+
+ASSUMPTIONS
+{
+    ASSUME(gBattleMoves[MOVE_UPROAR].effect == EFFECT_UPROAR);
+}
+
+DOUBLE_BATTLE_TEST("Uproar status causes sleeping pokemon to wake up during an attack")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_WOBBUFFET) { Status1(STATUS1_SLEEP); }
+        OPPONENT(SPECIES_VOLTORB) { Ability(ABILITY_SOUNDPROOF); Status1(STATUS1_SLEEP); }
+        OPPONENT(SPECIES_WOBBUFFET) { Status1(STATUS1_SLEEP); }
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_UPROAR); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_UPROAR, playerLeft);
+        HP_BAR(opponentRight);
+        MESSAGE("Wobbuffet woke up in the UPROAR!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, playerRight);
+        MESSAGE("Foe Voltorb woke up in the UPROAR!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponentLeft);
+        MESSAGE("Foe Wobbuffet woke up in the UPROAR!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponentRight);
+    }
+}
