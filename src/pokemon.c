88565970c8de64300c234f16cade9171d7996be5
Eduardo Quezada <eduardo602002@gmail.com>
2024-03-24 08:19:24 -0300

    Fixed unintended form behavior (#4299)

--- src/pokemon.c
+++ src/pokemon.c
@@ -4554,7 +4554,7 @@ u16 NationalPokedexNumToSpecies(u16 nationalNum)
     if (species == NUM_SPECIES)
         return NATIONAL_DEX_NONE;
 
-    return species;
+    return GET_BASE_SPECIES_ID(species);
 }
 
 u16 NationalToHoennOrder(u16 nationalNum)
