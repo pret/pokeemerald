6bde095ae883cd4ee0d78608839cea22878376c2
Bassoonian <iasperbassoonian@gmail.com>
2024-04-22 10:20:37 +0200

    Partner fixes (#4422)
    * Partner fixes
    
    * Generalise TRAINER_PARTNER(PARTNER_NONE)

--- src/battle_tower.c
+++ src/battle_tower.c
@@ -1435,7 +1435,7 @@ u8 GetFrontierOpponentClass(u16 trainerId)
     }
     else if (trainerId > TRAINER_PARTNER(PARTNER_NONE))
     {
-        trainerClass = gBattlePartners[GetTrainerClassFromId(trainerId - TRAINER_PARTNER(PARTNER_NONE))].trainerClass;
+        trainerClass = gBattlePartners[trainerId - TRAINER_PARTNER(PARTNER_NONE)].trainerClass;
     }
     else if (trainerId < FRONTIER_TRAINERS_COUNT)
     {
