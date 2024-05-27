767a1c79374c33d79c033a16186e50fc4976d814
Eduardo Quezada <eduardo602002@gmail.com>
2024-05-27 09:06:34 -0400

    Version 1.8.4 (#4639)
    * Version 1.8.4
    
    * Missing labels

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -1,13 +1,13 @@
 #ifndef GUARD_CONSTANTS_EXPANSION_H
 #define GUARD_CONSTANTS_EXPANSION_H
 
-// 1.8.3
+// 1.8.4
 #define EXPANSION_VERSION_MAJOR 1
 #define EXPANSION_VERSION_MINOR 8
-#define EXPANSION_VERSION_PATCH 3
+#define EXPANSION_VERSION_PATCH 4
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
-#define EXPANSION_TAGGED_RELEASE FALSE
+#define EXPANSION_TAGGED_RELEASE TRUE
 
 #endif
