0ed8d09d4777608d7749a66d171c438fca655f0c
WillKolada <57021938+WillKolada@users.noreply.github.com>
2024-05-31 15:33:31 -0500

    Revert Dynamax HP on fainting (#4681)
    * Revert Dynamax HP on fainting
    
    Calls UndoDynamax whenever a mon faints to ensure that its HP is reverted and the relevant data is cleared. Updates UndoDynamax to CalculateMonStats after applying the HP reversion to prevent fainted mons from retaining their boosted Max HP until the end of the battle.
    
    * Update src/battle_main.c
    
    ---------
    
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- src/battle_main.c
+++ src/battle_main.c
@@ -3677,6 +3677,9 @@ const u8* FaintClearSetData(u32 battler)
     gBattleStruct->zmove.active = FALSE;
     gBattleStruct->zmove.toBeUsed[battler] = MOVE_NONE;
     gBattleStruct->zmove.effect = EFFECT_HIT;
+    // Clear Dynamax data
+    UndoDynamax(battler);	
+	
     return result;
 }
 
