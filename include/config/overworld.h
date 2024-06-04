f4c890df1e18cc8156b7527d2839fcb59bcc2f76
Bassoonian <iasperbassoonian@gmail.com>
2024-06-04 15:37:02 +0200

    Disable OW followers by default

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -35,7 +35,7 @@
 #define OW_BERRY_DRAIN_RATE            GEN_6_ORAS // If OW_BERRY_MOISTURE is enabled, this setting changes how fast the soil dries out. GEN_4 uses a Berry-dependent drain rate, GEN_6_XY dries out in 24 hours (4 hours with the relevant Mulch) and GEN_6_ORAS dries out in 4 hours. Other values are illegal.
 
 // Follower Pokémon
-#define OW_FOLLOWERS_ENABLED           TRUE       // Enables follower Pokémon, HGSS style.
+#define OW_FOLLOWERS_ENABLED           FALSE      // Enables follower Pokémon, HGSS style.
 #define OW_MON_BOBBING                 TRUE       // If true, follower pokemon will bob up and down during their idle & walking animations
 #define LARGE_OW_SUPPORT               TRUE       // If true, adds a small amount of overhead to OW code so that large (48x48, 64x64) OWs will display correctly under bridges, etc.
 #define OW_FOLLOWERS_SHARE_PALETTE     FALSE      // [WIP!! NOT ALL PALETTES HAVE BEEN ADJUSTED FOR THIS!!] If TRUE, follower palettes are taken from battle sprites.
