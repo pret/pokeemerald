8cb0b3b2f25ce88729f431bce42443e533b5b09c
wiz1989 <danny.suhr@live.com>
2024-06-22 18:26:44 +0200

    update pocket for ITEM_FAIRY_FEATHER

--- src/data/items.h
+++ src/data/items.h
@@ -14686,7 +14686,7 @@ const struct Item gItemsInfo[] =
             "A hold item that\n"
             "raises the power of\n"
             "Fairy-type moves."),
-        .pocket = POCKET_ITEMS,
+        .pocket = POCKET_BATTLE_ITEMS,
         .type = ITEM_USE_BAG_MENU,
         .fieldUseFunc = ItemUseOutOfBattle_CannotUse,
         .flingPower = 10,
