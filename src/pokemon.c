72071753cbad3102f8c917a170eca60e734f0036
TeamAquasHideout <jonathonheddings@gmail.com>
2024-07-10 03:02:16 -0400

    Shuffle List Before Random (Might Undo Later)

--- src/pokemon.c
+++ src/pokemon.c
@@ -8984,7 +8984,12 @@ u16 GetSpeciesRandomSeeded(u16 species)
 
 u16 GetSpeciesRandomNotSeeded(u16 species)
 {
-    return sRandomSpeciesLegendary[RandomModulo(species, RANDOM_SPECIES_COUNT_LEGENDARY)];
+    u16 *stemp = Alloc(sizeof(sRandomSpeciesLegendary));
+    DmaCopy16(3, sRandomSpeciesLegendary, stemp, sizeof(sRandomSpeciesLegendary));
+    Shuffle16(stemp, RANDOM_SPECIES_COUNT_LEGENDARY);
+    species = stemp[RandomModulo(species, RANDOM_SPECIES_COUNT_LEGENDARY)];
+    Free(stemp);
+    return species;
 }
 
 u16 GetRandomMove(u16 move, u16 species)
