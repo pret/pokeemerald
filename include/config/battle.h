5c38532281d97f9a51e383dce134c38f6e11d8ba
psf <77138753+pkmnsnfrn@users.noreply.github.com>
2024-07-14 05:46:04 -0700

    Add B_ENEMY_THROW_BALLS, B_PLAYER_THROW_BALLS_SOUND, and B_ENEMY_THROW_BALLS_SOUND (#4953)
    * First proof of concept works
    
    * Added B_THROW_BALLS_SOUND
    
    * Made generic MonSendOut
    
    * Changed magic number to constant
    
    * added acute e and changed to GEN_LATEST
    
    * Removed tabs
    
    * Undef'd HIBYTE and removed extra bool32
    
    * Made functions inline per https://github.com/rh-hideout/pokeemerald-expansion/pull/4953/files\#r1675397114
    Split into B_THROW_BALLS_SOUND B_PLAYER_THROW_BALLS_SOUND and B_ENEMY_THROW_BALLS_SOUND per https://github.com/rh-hideout/pokeemerald-expansion/pull/4953/files\#r1676143761
    
    * Reset debug script
    
    * Removed static assert per Discord conversation with Alex

--- include/config/battle.h
+++ include/config/battle.h
@@ -268,4 +268,9 @@
 #define B_NEW_IMPACT_PALETTE            TRUE    // If set to TRUE, it updates the basic 'hit' palette.
 #define B_NEW_SURF_PARTICLE_PALETTE     TRUE    // If set to TRUE, it updates Surf's wave palette.
 
+// Poké Ball animation and sounds
+#define B_ENEMY_THROW_BALLS          GEN_LATEST  // In GEN_6+, enemy Trainers throw Poké Balls into battle instead of them just appearing on the ground and opening.
+#define B_ENEMY_THROW_BALLS_SOUND    GEN_LATEST  // In GEN_5+, enemy Trainer's Poké Balls make a sound when thrown to send out a Pokémon. This can only be used when B_ENEMY_THROW_BALLS is set to GEN_6 or later.
+#define B_PLAYER_THROW_BALLS_SOUND   GEN_LATEST  // In GEN_5+, the player's Poké Balls make a sound when thrown to send out a Pokémon.
+
 #endif // GUARD_CONFIG_BATTLE_H
