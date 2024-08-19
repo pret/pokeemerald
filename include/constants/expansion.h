becb2fcac839a36df868297a8945ec51f32123ad
Eduardo Quezada <eduardo602002@gmail.com>
2024-08-19 18:29:48 -0400

    Start 1.9.2 cycle

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -4,10 +4,10 @@
 // 1.9.1
 #define EXPANSION_VERSION_MAJOR 1
 #define EXPANSION_VERSION_MINOR 9
-#define EXPANSION_VERSION_PATCH 1
+#define EXPANSION_VERSION_PATCH 2
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
-#define EXPANSION_TAGGED_RELEASE TRUE
+#define EXPANSION_TAGGED_RELEASE FALSE
 
 #endif
