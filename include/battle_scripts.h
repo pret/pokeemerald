ceec17ec7b93cd645336b8a28555bf586728e20b
Salem <65783283+u8-Salem@users.noreply.github.com>
2024-08-25 23:13:40 +0200

    remove unused bs signature (#5257)

--- include/battle_scripts.h
+++ include/battle_scripts.h
@@ -469,7 +469,6 @@ extern const u8 BattleScript_AngerShellActivates[];
 extern const u8 BattleScript_WellBakedBodyActivates[];
 extern const u8 BattleScript_WindRiderActivatesMoveEnd[];
 extern const u8 BattleScript_WindPowerActivates[];
-extern const u8 BattleScript_WindPowerActivatesEnd2[];
 extern const u8 BattleScript_ProtosynthesisActivates[];
 extern const u8 BattleScript_QuarkDriveActivates[];
 extern const u8 BattleScript_GoodAsGoldActivates[];
