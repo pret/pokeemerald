80ad7f15cc6806bad97d05e5a9bb0591fc760334
kittenchilly <mario.pikmin.zelda@gmail.com>
2024-08-26 13:09:50 -0500

    Chilly Reception AI

--- test/battle/ai/ai_flag_sequence_switching.c
+++ test/battle/ai/ai_flag_sequence_switching.c
@@ -64,7 +64,7 @@ AI_SINGLE_BATTLE_TEST("AI_FLAG_SEQUENCE_SWITCHING: Roar and Dragon Tail still fo
     }
 }
 
-AI_SINGLE_BATTLE_TEST("AI_FLAG_SEQUENCE_SWITCHING: AI will always switch into lowest party index after U-Turn, Parting Shot, and Baton Pass")
+AI_SINGLE_BATTLE_TEST("AI_FLAG_SEQUENCE_SWITCHING: AI will always switch into lowest party index after U-Turn, Parting Shot, Baton Pass, and Chilly Reception")
 {
     u32 j, aiSequenceSwitchingFlag = 0, move = MOVE_NONE;
 
@@ -72,6 +72,7 @@ AI_SINGLE_BATTLE_TEST("AI_FLAG_SEQUENCE_SWITCHING: AI will always switch into lo
         MOVE_U_TURN,
         MOVE_PARTING_SHOT,
         MOVE_BATON_PASS,
+        MOVE_CHILLY_RECEPTION,
     };
 
     for (j = 0; j < ARRAY_COUNT(switchMoves); j++)
@@ -84,6 +85,7 @@ AI_SINGLE_BATTLE_TEST("AI_FLAG_SEQUENCE_SWITCHING: AI will always switch into lo
         ASSUME(gMovesInfo[MOVE_U_TURN].effect == EFFECT_HIT_ESCAPE);
         ASSUME(gMovesInfo[MOVE_PARTING_SHOT].effect == EFFECT_PARTING_SHOT);
         ASSUME(gMovesInfo[MOVE_BATON_PASS].effect == EFFECT_BATON_PASS);
+        ASSUME(gMovesInfo[MOVE_CHILLY_RECEPTION].effect == EFFECT_CHILLY_RECEPTION);
         AI_FLAGS(AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY | AI_FLAG_TRY_TO_FAINT | aiSequenceSwitchingFlag);
         PLAYER(SPECIES_SWELLOW) { Level (50); }
         OPPONENT(SPECIES_MACHOP) { Level(1); Moves(move); }
