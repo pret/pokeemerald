889a02d263ddef7383666aa2d5df26ef159e64f0
Eduardo Quezada <eduardo602002@gmail.com>
2024-09-16 12:12:34 -0300

    Version 1.9.2 (#5357)
    * Version 1.9.2
    
    * Apply suggestions from code review
    
    Co-authored-by: Pawkkie <61265402+Pawkkie@users.noreply.github.com>
    
    * Updated to latest master
    
    * Updated to latest master
    
    * Fix last PR
    
    * Corrected order + Pumpkaboo/Gourgeist credits
    
    ---------
    
    Co-authored-by: Pawkkie <61265402+Pawkkie@users.noreply.github.com>

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -1,13 +1,13 @@
 #ifndef GUARD_CONSTANTS_EXPANSION_H
 #define GUARD_CONSTANTS_EXPANSION_H
 
-// 1.9.1
+// 1.9.2
 #define EXPANSION_VERSION_MAJOR 1
 #define EXPANSION_VERSION_MINOR 9
 #define EXPANSION_VERSION_PATCH 2
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
-#define EXPANSION_TAGGED_RELEASE FALSE
+#define EXPANSION_TAGGED_RELEASE TRUE
 
 #endif
