39a8a77d3d74294b52ca5c81333132398126a952
kittenchilly <mario.pikmin.zelda@gmail.com>
2024-09-21 13:28:20 -0500

    Play point animation when sending a follower into battle (#5406)
    * Add point animation when sending a follower into battle
    
    * Update trainers.h

--- src/battle_controllers.c
+++ src/battle_controllers.c
@@ -2903,7 +2903,7 @@ void BtlController_HandleIntroTrainerBallThrow(u32 battler, u16 tagTrainerPal, c
     if (side == B_SIDE_PLAYER)
     {
         StoreSpriteCallbackInData6(&gSprites[gBattlerSpriteIds[battler]], SpriteCB_FreePlayerSpriteLoadMonSprite);
-        StartSpriteAnim(&gSprites[gBattlerSpriteIds[battler]], 1);
+        StartSpriteAnim(&gSprites[gBattlerSpriteIds[battler]], ShouldDoSlideInAnim() ? 2 : 1);
 
         paletteNum = AllocSpritePalette(tagTrainerPal);
         LoadCompressedPalette(trainerPal, OBJ_PLTT_ID(paletteNum), PLTT_SIZE_4BPP);
