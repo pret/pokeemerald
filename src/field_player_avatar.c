264c94d906bd241106914de28beb0a7519ab3104
zzebedy <zzebedy@proton.me>
2024-09-24 05:37:46 +0100

    faster surfing

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -630,7 +630,7 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
     if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
     {
         // same speed as running
-        PlayerWalkFast(direction);
+        PlayerWalkFaster(direction);
         return;
     }
 
