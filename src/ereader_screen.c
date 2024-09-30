0d4dafa314a2d3074fa760ac8fd8dc491729e06f
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2024-09-30 17:35:21 -0400

    Remove explicit symbol sizes in sym_common.txt

--- src/ereader_screen.c
+++ src/ereader_screen.c
@@ -40,6 +40,9 @@ struct EReaderData
 
 static void Task_EReader(u8);
 
+// This belongs in COMMON somewhere between party_menu and ereader_screen, but it's unused so it's unclear where.
+COMMON_DATA UNUSED u8 gUnknownSpace[64] = {0};
+
 COMMON_DATA struct EReaderData gEReaderData = {0};
 
 extern const u8 gMultiBootProgram_EReader_Start[];
