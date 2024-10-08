a76f7fe92e960039071109731f5e9f2721f106d7
Eduardo Quezada <eduardo602002@gmail.com>
2024-10-08 14:45:09 -0300

    Version 1.9.3 (#5485)
    * Version 1.9.3
    
    * Updated to latest master

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -1,13 +1,13 @@
 #ifndef GUARD_CONSTANTS_EXPANSION_H
 #define GUARD_CONSTANTS_EXPANSION_H
 
-// Last version: 1.9.2
+// Last version: 1.9.3
 #define EXPANSION_VERSION_MAJOR 1
 #define EXPANSION_VERSION_MINOR 9
 #define EXPANSION_VERSION_PATCH 3
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
-#define EXPANSION_TAGGED_RELEASE FALSE
+#define EXPANSION_TAGGED_RELEASE TRUE
 
 #endif
