8890500b770d4712b2456d1addefbe0b44993793
kittenchilly <mario.pikmin.zelda@gmail.com>
2024-10-10 14:26:46 -0500

    Fix P_FRIENDSHIP_EVO_THRESHOLD not checking for Gen 8 (#5503)

--- src/pokemon.c
+++ src/pokemon.c
@@ -60,7 +60,7 @@
 #include "constants/weather.h"
 #include "wild_encounter.h"
 
-#define FRIENDSHIP_EVO_THRESHOLD ((P_FRIENDSHIP_EVO_THRESHOLD >= GEN_9) ? 160 : 220)
+#define FRIENDSHIP_EVO_THRESHOLD ((P_FRIENDSHIP_EVO_THRESHOLD >= GEN_8) ? 160 : 220)
 
 struct SpeciesItem
 {
