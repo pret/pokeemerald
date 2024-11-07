b89fda79ea29142ff076eb3b012a082ee8d0a26d
GriffinR <griffin.g.richards@gmail.com>
2024-11-06 21:41:43 -0500

    Generate MAPSEC constants from JSON

--- json_data_rules.mk
+++ json_data_rules.mk
@@ -12,3 +12,7 @@ $(DATA_SRC_SUBDIR)/region_map/region_map_entries.h: $(DATA_SRC_SUBDIR)/region_ma
 	$(JSONPROC) $^ $@
 
 $(C_BUILDDIR)/region_map.o: c_dep += $(DATA_SRC_SUBDIR)/region_map/region_map_entries.h
+
+AUTO_GEN_TARGETS += include/constants/region_map_sections.h
+include/constants/region_map_sections.h: $(DATA_SRC_SUBDIR)/region_map/region_map_sections.json $(DATA_SRC_SUBDIR)/region_map/region_map_sections.constants.json.txt
+	$(JSONPROC) $^ $@
