1fe81e0bb2acf2d6401b8f1d2382f63c288c4e7b
Eduardo Quezada <eduardo602002@gmail.com>
2025-05-17 14:54:50 -0400

    Document cCURSOR_POSITION

--- include/constants/battle_script_commands.h
+++ include/constants/battle_script_commands.h
@@ -43,6 +43,7 @@
 #define MSG_DISPLAY             7
 #define BATTLE_COMMUNICATION_ENTRIES_COUNT  8
 
+#define cCURSOR_POSITION     (gBattleCommunication + CURSOR_POSITION)
 #define cEFFECT_CHOOSER      (gBattleCommunication + MOVE_EFFECT_BYTE)
 #define cMULTISTRING_CHOOSER (gBattleCommunication + MULTISTRING_CHOOSER)
 #define cMISS_TYPE           (gBattleCommunication + MISS_TYPE)
